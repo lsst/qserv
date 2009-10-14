96abdead46f4ce249cb953d835ca8154a5ab7f6a
danielw <danielw@git.lsstcorp.org>
2009-10-14 00:30:53 +0000

    Remove redundant create table statements from partitioning.

--- examples/partition.py
+++ examples/partition.py
@@ -1,17 +1,21 @@
 import MySQLdb as sql
 
+
 class TinySql:
     def __init__(self):
         self.connection = sql.connect()
         self.cursor = self.connection.cursor()
+        self.parentDbName = "LSSTx"
+
     def execUnary(self, q):
         self.cursor.execute(q)
         return self.cursor.fetchall()[0][0]
 
     def getPartitions(self, field, count, tablename):
+        fullName = self.parentDbName + "." + tablename
         # Find out ranges 
-        maxv = self.execUnary("SELECT max(%s) FROM %s;" % (field, tablename))
-        minv = self.execUnary("SELECT min(%s) FROM %s;" % (field, tablename))
+        maxv = self.execUnary("SELECT max(%s) FROM %s;" % (field, fullName))
+        minv = self.execUnary("SELECT min(%s) FROM %s;" % (field, fullName))
 
         # Uniformly split range
         block = (maxv - minv) / count
@@ -20,32 +24,39 @@ class TinySql:
         parts += [maxv+1]
         return parts
 
-    def insertRows(self, spec, srcTable, targetPrefix):
-        # Unwrap
-        (chunkId, subchunkId, ramin, ramax, declmin, declmax) = spec
-        tblName = "%s_%d" % (targetPrefix, chunkId)
-        tsql = "CREATE TABLE IF NOT EXISTS %s LIKE %s;" % (tblName, srcTable)
+    def defineChunkTable(self, chunkId, srcTable, targetTablePrefix):
+        tblName = "%s.%s_%d" % (self.parentDbName,
+                                targetTablePrefix, chunkId)
+        srcTableName = "%s.%s" % (self.parentDbName, srcTable)
+        tsql = "CREATE TABLE IF NOT EXISTS %s LIKE %s;" % (tblName,
+                                                           srcTableName)
         csql = "ALTER TABLE %s ADD COLUMN subchunkId INT;" % tblName
         print "Inserting into table:", tsql
         self.cursor.execute(tsql)
         self.cursor.fetchall() # Ignore output
-        print "Altering table:", csql
         try:
             self.cursor.execute(csql)  # May fail
             self.cursor.fetchall()
         except:
             pass ## silence Alter errors.
+        srcName = srcTableName
+        destName = tblName
+        return (srcName, destName)
+        
+    def insertRows(self, spec, srcName, destName):
+        (chunkId, subchunkId, ramin, ramax, declmin, declmax) = spec
+
         insqltemp = "INSERT INTO %s SELECT *,%d FROM %s where ra between %s and %s and decl between %s and %s;" 
-        insql = insqltemp % (tblName, subchunkId, srcTable, ramin, ramax, declmin, declmax)
-        print "asdfasdfasdfasdf"
+        insql = insqltemp % (destName, subchunkId, srcName,
+                             ramin, ramax, declmin, declmax)
         try:
-            print insql
+            #print insql
             self.cursor.execute(insql)
             self.cursor.fetchall() ## drop results
         except:
             pass # fail silently now.
     def makePartMap(self, tuples):
-        tblName = "test.partmap1"
+        tblName = "test.partmap3"
         csql = """CREATE TABLE IF NOT EXISTS %s(
         chunkId INT, 
         subchunkId INT, 
@@ -59,7 +70,7 @@ class TinySql:
         for t in tuples:
             tstr = ",".join([str(v) for v in list(t)])
             isql = "INSERT INTO %s VALUES (%s);" % (tblName, tstr)
-            print "Executing ",isql
+            #print "Executing ",isql
             createSql.append(isql)
             self.cursor.execute(isql)
             self.cursor.fetchall() # Drop result
@@ -79,7 +90,7 @@ def split():
         decl_chunks = 4
         decl_sub = 3
         
-    inputtable = "LSST.cat"
+    inputtable = "cat"
     s = TinySql()
     
     declParts = s.getPartitions("decl", decl_chunks * decl_sub, inputtable)
@@ -91,17 +102,17 @@ def split():
     for rap in range(ra_chunks):
         for declp in range(decl_chunks):
             chunkId = 1 + (rap * decl_chunks) + declp
+            tblNames = s.defineChunkTable(chunkId, inputtable, "Object")
             for ras in range(ra_sub):
                 for decls in range(decl_sub):
                     subChunkId = (ras * decl_sub) + decls
-                    print subChunkId
                     partMapTuple = (chunkId, subChunkId, 
                                     raParts[ras + (rap*ra_sub)],
                                     raParts[1+ ras + (rap*ra_sub)],
                                     declParts[decls + (declp*decl_sub)],
                                     declParts[1+ decls + (declp*decl_sub)])
                     partTuples.append(partMapTuple)
-                    s.insertRows(partMapTuple, inputtable, "LSST.Object")
+                    s.insertRows(partMapTuple, tblNames[0], tblNames[1])
     csql = s.makePartMap(partTuples)
     filename = "partmapsql.sql"
     print "writing sql for partmap to ", filename
