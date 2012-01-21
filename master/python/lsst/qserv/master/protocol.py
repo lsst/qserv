c18ddd1792334e6fe810218b51ac54c551990ebf
Daniel L Wang <danielw@slac.stanford.edu>
2012-01-20 18:30:48 -0800

    master: Add protocol handling skeleton code.

---
+++ master/python/lsst/qserv/master/protocol.py
@@ -0,0 +1,51 @@
+# 
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+# protocol module for lsst.qserv.master
+# Manages wire-protocol-related operations for qserv.
+import worker_pb2
+
+class TaskMsgFactory:
+    def __init__(self, session, db):
+        msg = worker_pb2.TaskMsg()
+        msg.session = session
+        msg.db = db
+        self.msg = msg
+
+    def newChunk(self, resulttable, chunkid):
+        msg = self.msg
+        msg.chunkid = chunkid
+        self.resulttable = resulttable
+        frag = msg.fragment.add()
+        self.frag = frag
+
+    def fillFragment(self, query, subchunks):
+        self.frag.query = query
+        self.frag.resulttable = self.resulttable
+        if subchunks:
+            for s in subchunks:
+                self.frag.subchunk.add(s)
+        pass
+
+    def getBytes(self):
+        return self.msg.SerializeToString()
+
