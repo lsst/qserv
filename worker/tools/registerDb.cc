b86ef25ed3aec01472394516ac163b3c92d3f3c5
Jacek Becla <becla@slac.stanford.edu>
2012-03-15 14:44:48 -0700

    moved the getopt code into registerDb

--- worker/tools/registerDb.cc
+++ worker/tools/registerDb.cc
@@ -10,32 +10,50 @@ namespace qWorker = lsst::qserv::worker;
 
 using lsst::qserv::SqlConfig;
 
+void
+printHelp() {
+    std::cout
+        << "\nUsage:\n\n"
+        << "   registerDb -r -d <dbName> -t <tables>\n"
+        << "   registerDb -g -a -b <baseDir>\n"
+        << "   registerDb -g -d <dbName> -b <baseDir>\n"
+        << "   registerDb -h\n"
+        << "\nWhere:\n\n"
+        << "  -r           - register database in qserv metadata\n"
+        << "  -g           - generate export paths\n"
+        << "  -a           - for all databases registered in qserv metadata\n"
+        << "  -d <dbName>  - database name\n"
+        << "  -t <tables>  - comma-separated list of partitioned tables\n"
+        << "  -b <baseDir> - base directory\n"
+        << "  -h           - prints help and exits\n"
+        << endl;
+}
 
-int main(int, char**) {
-// get these through program arguments
-    std::string const workerId = "theId";
-    std::string dbName = "rplante_PT1_2_u_pt12prod_im3000_qserv";
-    std::string partitionedTables = "Object, Source, ForcedSource";
-    std::string baseDir = "/u1/qserv/xrootd-run";
-    
-    SqlConfig sc;
-    sc.hostname = "";
-    sc.username = "adm_becla";
-    sc.password = "";
-    sc.dbName = "";
-    sc.port = 0;
-    sc.socket = qWorker::getConfig().getString("mysqlSocket").c_str();
-    
+bool
+registerDb(std::string const& workerId,
+           std::string const& dbName,
+           std::string const& pTables) {
+    cout << "Registering db: " << dbName << ", partTables: " << pTables << endl;
+    lsst::qserv::SqlConnection sqlConn(sc);
+    lsst::qserv::SqlErrorObject errObj;
+    lsst::qserv::worker::Metadata m(workerId);
+    if ( !m.registerQservedDb(dbName, pTables, sqlConn, errObj) ) {
+        std::cerr << "Failed to register the db. " 
+                  << errObj.printErrMsg() << std::endl;
+        return errObj.errNo();
+    }
+    return 0;
+}
+
+bool
+generateExportPathsForDb(std::string const& workerId,
+                         std::string const& dbName, 
+                         std::string const& baseDir) {
     lsst::qserv::SqlConnection sqlConn(sc);
     lsst::qserv::SqlErrorObject errObj;
 
     lsst::qserv::worker::Metadata m(workerId);
     
-    if ( !m.registerQservedDb(dbName, partitionedTables, sqlConn, errObj) ) {
-        std::cerr << "Failed to register the db. " 
-                  << errObj.printErrMsg() << std::endl;
-        return errObj.errNo();
-    }
     std::vector<std::string> exportPaths;
     if ( ! m.generateExportPathsForDb(baseDir, dbName, partitionedTables,
                                       sqlConn, errObj, exportPaths) ) {
@@ -54,5 +72,122 @@ int main(int, char**) {
                   << errObj.printErrMsg() << std::endl;
         return errObj.errNo();
     }
+    return 0;
+}
+
+bool
+generateExportPaths(std::string const& workerId,
+                    std::string const& baseDir) {
+    lsst::qserv::SqlConnection sqlConn(sc);
+    lsst::qserv::SqlErrorObject errObj;
+
+    lsst::qserv::worker::Metadata m(workerId);
+    
+    std::vector<std::string> exportPaths;
+    if ( ! m.generateExportPaths(baseDir, sqlConn, errObj, exportPaths) ) {
+        std::cerr << "Failed to generate export directories. " 
+                  << errObj.printErrMsg() << std::endl;
+        return errObj.errNo();
+    }
+    lsst::qserv::worker::QservPathStructure p;
+    if ( !p.insert(exportPaths) ) {
+        std::cerr << "Failed to insert export paths. "
+                  << errObj.printErrMsg() << std::endl;
+        return errObj.errNo();
+    }
+    if ( !p.persist() ) {
+        std::cerr << "Failed to persist export paths. " 
+                  << errObj.printErrMsg() << std::endl;
+        return errObj.errNo();
+    }
+    return 0;
+}
+
+
+int 
+main(int, char**) {
+    // TODO: get this from xrootd
+    std::string const workerId = "theId";
+
+    // TODO: decide how to get these:
+    SqlConfig sc;
+    sc.hostname = "";
+    sc.username = "adm_becla";
+    sc.password = "";
+    sc.dbName = "";
+    sc.port = 0;
+    sc.socket = qWorker::getConfig().getString("mysqlSocket").c_str();
+
+    std::string dbName;
+    std::string pTables;
+    std::string baseDir;
+
+    bool flag_regDb = false;
+    bool flag_genEp = false;
+    bool flag_allDb = false;
+
+    int c;
+    opterr = 0;
+    while ((c = getopt (argc, argv, "rgd:t:ab:h")) != -1) {
+        switch (c) {
+        case 'r': { flag_regDb = true; break;}
+        case 'g': { flag_genEp = true; break;}
+        case 'a': { flag_allDb = true; break;}
+        case 'd': { dbName  = optarg; break;} 
+        case 't': { pTables = optarg; break;}
+        case 'b': { baseDir = optarg; break;}
+        case 'h': { printHelp(); return 0;}
+        case '?':
+            if (optopt=='r'||optopt=='d'||
+                optopt=='t'||optopt== 'b') {
+                cerr << "Option -" << optopt << " requires an argument." << endl;
+            } else if (isprint (optopt)) {
+                cerr << "Unknown option `-" << char(optopt) << "'" << endl;
+            } else {
+                cerr << "Unknown option character " << char(optopt) << endl;
+            }
+            return -1;
+        default: {
+            return -2;
+        }
+        }
+    }
+    if ( flag_regDb ) {
+        if ( dbName.empty() ) {
+            cerr << "database name not specified "
+                 << "(must use -d <dbName> with -r option)" << endl;
+            return -3;
+        } else if ( pTables.empty() ) {
+            cerr << "partitioned tables not specified "
+                 << "(must use -t <tables> with -r option)" << endl;
+            return -4;
+        }
+        return registerDb(workerId, dbName, pTables);        
+    } else if ( flag_genEp ) {
+        if ( baseDir.empty() ) {
+            cerr << "base dir not specified "
+                 << "(must use -b <baseDir> with -g option)" << endl;
+            return -5;
+        }       
+        if ( !dbName.empty() ) {
+            cout << "Generating export paths for database: "
+                 << dbName << ", baseDir is: " << baseDir << endl;
+            return generateExportPaths(workerId, dbName, baseDir);
+        } else if ( flag_allDb ) {
+            cout << "generating export paths for all databases "
+                 << "registered in the qserv metadata, baseDir is: " 
+                 << baseDir << endl;
+            return generateAllExportPaths(workerId, baseDir);
+        } else {
+            cerr << "\nDo you want to generate export paths for one "
+                 << "database, or all? (hint: use -d <dbName or -a flag)" 
+                 << endl;
+            printHelp();
+            return -6;
+        }
+        // ...
+        return 0;
+    }
+    printHelp();
     return 0;
 }
