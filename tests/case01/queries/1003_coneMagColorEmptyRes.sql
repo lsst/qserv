86203b8e969e55f1a20eff0bf3bc291c439966e8
Jacek Becla <becla@slac.stanford.edu>
2012-04-17 17:10:08 -0700

    fixed negative ra ranges

--- tests/case01/queries/1003_coneMagColorEmptyRes.sql
+++ tests/case01/queries/1003_coneMagColorEmptyRes.sql
@@ -6,9 +6,9 @@
 
 SELECT COUNT(*)
 FROM   Object
-WHERE  ra_PS BETWEEN -10 AND 4  -- noQserv
+WHERE  ra_PS BETWEEN 0 AND 4  -- noQserv
  AND   decl_PS BETWEEN -6 AND -5 -- noQserv
--- withQserv WHERE qserv_areaspec_box(-10, -6, 4, -5)
+-- withQserv WHERE qserv_areaspec_box(0, -6, 4, -5)
    AND scisql_fluxToAbMag(zFlux_PS) BETWEEN 20 AND 24
    AND scisql_fluxToAbMag(gFlux_PS)-scisql_fluxToAbMag(rFlux_PS) BETWEEN 0.1 AND 0.2
    AND scisql_fluxToAbMag(iFlux_PS)-scisql_fluxToAbMag(zFlux_PS) BETWEEN 0.1 AND 0.2
--- tests/case01/queries/1004_varObjects.sql
+++ tests/case01/queries/1004_varObjects.sql
@@ -3,7 +3,7 @@
 
 SELECT objectId
 FROM   Object
-WHERE  ra_PS BETWEEN -1 AND 3   -- noQserv
+WHERE  ra_PS BETWEEN 0 AND 3   -- noQserv
   AND  decl_PS BETWEEN 0 AND 10 -- noQserv
--- withQserv WHERE qserv_areaspec_box(-1, 0, 3, 10)
+-- withQserv WHERE qserv_areaspec_box(0, 0, 3, 10)
 --   AND variability > 0.8
