2d89be11046d68a57d51d5f5e702ce703c05fc23
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-01-18 19:39:07 +0100

    Cleanup admin shell scripts

--- admin/qserv-connect-mysql-proxy.sh
+++
@@ -1,3 +0,0 @@
-#mysql --host clrlsst-dbmaster.in2p3.fr --port 4040 --user qsmaster LSST 2>&1 > qserv-test.out < qserv-test.sql&
-source $QSERV_SRC/qserv-install-params.sh
-mysql --host 127.0.0.1 --port $QSERV_MYSQL_PROXY_PORT --user qsmaster LSST 
