1a19fc23bb6e20889f21274513f61a1206350e53
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-04-09 16:51:58 +0200

    creation of qservw_workerid database with associated acces rights

---
+++ admin/custom/tmp/qservw_workerid.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS qservw_worker;
+CREATE DATABASE qservw_worker;
+GRANT ALL ON qservw_worker.* TO 'qsmaster'@'localhost';
+-- FLUSH PRIVILEGES;
+CREATE TABLE qservw_worker.Dbs (
+  `db` char(200) NOT NULL
+);
+INSERT INTO qservw_worker.Dbs VALUES('LSST'); 
+GRANT SELECT ON LSST.* TO ''@'%';
+FLUSH PRIVILEGES;
