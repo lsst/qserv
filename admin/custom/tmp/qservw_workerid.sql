7a638414569966d3b30d621c926d05b4e08d38fd
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-04-10 16:06:29 +0200

    access right on LSST db will be managed at its creation

--- admin/custom/tmp/qservw_workerid.sql
+++ admin/custom/tmp/qservw_workerid.sql
@@ -1,10 +1,8 @@
 DROP DATABASE IF EXISTS qservw_worker;
 CREATE DATABASE qservw_worker;
 GRANT ALL ON qservw_worker.* TO 'qsmaster'@'localhost';
--- FLUSH PRIVILEGES;
+FLUSH PRIVILEGES;
 CREATE TABLE qservw_worker.Dbs (
   `db` char(200) NOT NULL
 );
 INSERT INTO qservw_worker.Dbs VALUES('LSST'); 
-GRANT SELECT ON LSST.* TO ''@'%';
-FLUSH PRIVILEGES;
