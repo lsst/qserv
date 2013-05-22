ba26b4859f4d328afb5219e30c6c9428da333cdd
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-05-22 16:44:33 +0200

    minor bugfixe solved in #2774

--- admin/custom/tmp/install/scisql.sh
+++ admin/custom/tmp/install/scisql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # TODO manage scisql version in templating system
-%(QSERV_BASE_DIR)s/bin/mysqld_safe --defaults-file=$install_dir/etc/my.cnf &
+%(QSERV_BASE_DIR)s/bin/mysqld_safe --defaults-file=%(QSERV_BASE_DIR)s/etc/my.cnf &
 cd %(QSERV_BASE_DIR)s/build &&
 tar jxvf scisql-0.3.tar.bz2 &&
 cd scisql-0.3 &&
