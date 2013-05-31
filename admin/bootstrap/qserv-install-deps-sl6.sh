0676b82d4dd5975bd838de677161929e05b58894
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-05-31 11:16:34 +0200

    adding SL6 dependencies

--- admin/bootstrap/qserv-install-deps-sl6.sh
+++ admin/bootstrap/qserv-install-deps-sl6.sh
@@ -13,10 +13,10 @@ yum install gcc-c++ git zlib-devel
 yum install python-devel
 
 # mysql
-yum install ncurses-devel
+yum install ncurses-devel glibc-devel
 
 # qserv
-yum install boost-devel openssl-devel antlr swig
+yum install boost-devel openssl-devel antlr swig java-1.6.0-openjdk
 
 # lua
 yum install readline-devel
