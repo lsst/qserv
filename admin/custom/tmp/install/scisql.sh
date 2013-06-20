1209acbaf7147f619afc4e8aeab3b24fa1ffaea1
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-06-20 11:12:08 +0200

    Stopping MySQL after SciSQL install

--- admin/custom/tmp/install/scisql.sh
+++ admin/custom/tmp/install/scisql.sh
@@ -3,10 +3,12 @@
 PATH=%(QSERV_BASE_DIR)s/bin:${PATH}
 SCISQL_VERSION=scisql-0.3.2
 
-%(QSERV_BASE_DIR)s/bin/mysqld_safe --defaults-file=%(QSERV_BASE_DIR)s/etc/my.cnf &
+mysqld_safe --defaults-file=%(QSERV_BASE_DIR)s/etc/my.cnf &
+
 cd %(QSERV_BASE_DIR)s/build &&
 tar jxvf ${SCISQL_VERSION}.tar.bz2 &&
 cd ${SCISQL_VERSION} &&
-./configure --prefix %(QSERV_BASE_DIR)s --mysql-user=root --mysql-password=%(MYSQLD_PASS)s --mysql-socket=%(MYSQLD_SOCK)s  &&
+./configure --prefix %(QSERV_BASE_DIR)s --mysql-user=root --mysql-password='%(MYSQLD_PASS)s' --mysql-socket=%(MYSQLD_SOCK)s  &&
 make &&
-make install
+make install &&
+mysqladmin -S %(MYSQLD_SOCK)s shutdown -u root -p'%(MYSQLD_PASS)s'
