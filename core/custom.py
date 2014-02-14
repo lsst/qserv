b496f3549ab6499a72e30827cad156a063908b3a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-02-14 23:41:53 +0100

    Header and lib path have to be provided one by one in custom.py

--- core/custom.py
+++ core/custom.py
@@ -6,14 +6,13 @@ MYSQL_DIR=os.getenv("MYSQL_DIR")
 PROTOBUF_DIR=os.getenv("PROTOBUF_DIR")
 PYTHONPATH=os.getenv("PYTHON_PATH")
 
-INC=[]
-INC.append(os.path.join(XROOTD_DIR, "include","xrootd"))
-INC.append(os.path.join(MYSQL_DIR,"include"))
 
-LIB=[]
-LIB.append(os.path.join(MYSQL_DIR,"lib","mysql"))
-LIB.append(os.path.join(XROOTD_DIR,"lib64"))
-LIB.append(os.path.join(PROTOBUF_DIR,"lib"))
+XROOTD_INC = os.path.join(XROOTD_DIR, "include","xrootd")
+MYSQL_INC = os.path.join(MYSQL_DIR,"include")
+
+MYSQL_LIB = os.path.join(MYSQL_DIR,"lib","mysql")
+XROOTD_LIB = os.path.join(XROOTD_DIR,"lib64")
+PROTOBUF_LIB = os.path.join(PROTOBUF_DIR,"lib")
 
 PROTOC=os.path.join(PROTOBUF_DIR,"bin","protoc")
 MYSQL_ROOT=MYSQL_DIR
