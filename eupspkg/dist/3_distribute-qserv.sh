71f3b7099f058cec933a30831dcac168e4e6d057
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-03-22 02:38:15 +0100

    Adding messages to distribution scripts

--- eupspkg/dist/3_distribute-qserv.sh
+++ eupspkg/dist/3_distribute-qserv.sh
@@ -18,8 +18,9 @@ QSERV_REPO_PATH=${DEPS_DIR}/qserv
 
 rm -f ${LOCAL_PKGROOT}/tables/qserv-${VERSION}.table &&
 
-# retrieve package
-git archive --remote=${QSERV_REPO} --format=tar --prefix=qserv/ ${QSERV_BRANCH} | gzip > ${QSERV_REPO_PATH}/upstream/qserv-${VERSION}.tar.gz &&
+echo "Retrieving Qserv archive"
+git archive --remote=${QSERV_REPO} --format=tar --prefix=qserv/ ${QSERV_BRANCH} | gzip > ${QSERV_REPO_PATH}/upstream/qserv-${VERSION}.tar.gz ||
+(echo "Unable to download qserv archive"; exit 1)
 
 cd ${QSERV_REPO_PATH} &&
 
@@ -29,18 +30,19 @@ git commit -m "Packaging qserv-${VERSION}" &&
 git tag | head -1 | xargs git tag -f &&
 git push origin master -f --tags &&
 
-# distribute
+echo "Retrieving Qserv tests dataset"
+git archive --remote=${DATA_REPO} --format=tar --prefix=testdata/ ${DATA_BRANCH} | gzip > ${LOCAL_PKGROOT}/tarballs/testdata-${VERSION}.tar.gz || 
+(echo "Unable to download tests dataset"; exit 1)
+
+echo "Distributing Qserv"
 cd - &&
+eups distrib install git --repository="http://lsst-web.ncsa.illinois.edu/~mjuric/pkgs" &&
 setup git &&
-eups_dist qserv ${VERSION} &&
-
-# prepare tests
-git archive --remote=${DATA_REPO} --format=tar --prefix=testdata/ ${DATA_BRANCH} | gzip > ${LOCAL_PKGROOT}/tarballs/testdata-${VERSION}.tar.gz
-
-
-# scisql
+eups_dist qserv ${VERSION} ||
+(echo "Unable to distribute Qserv"; exit 1)
+ 
+echo "Downloading scisql"
 SCISQL_ARCHIVE=scisql-0.3.2.tar.bz2
-
 if [ ! -f ${LOCAL_PKGROOT}/tarballs/${SCISQL_ARCHIVE} ]; then
     mkdir -p ${LOCAL_PKGROOT}/tarballs
     wget https://launchpad.net/scisql/trunk/0.3.2/+download/scisql-0.3.2.tar.bz2 --directory-prefix=${LOCAL_PKGROOT}/tarballs
