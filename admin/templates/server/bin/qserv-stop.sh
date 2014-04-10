0592c7fa22ec3ff702d4b387eb5ebb058f5e0302
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-04-10 16:40:17 +0200

    FIXES :
    - on init.d status() function
    - QSW_RESULTPATH is now created in xrootd startup script

--- admin/templates/server/bin/qserv-stop.sh
+++ admin/templates/server/bin/qserv-stop.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 QSERV_DIR=%(QSERV_DIR)s
+QSERV_RUN_DIR=%(QSERV_RUN_DIR)s
 . ${QSERV_DIR}/bin/env.sh
 
 services_rev=`echo ${SERVICES} | tr ' ' '\n' | tac`
@@ -8,6 +9,6 @@ for service in $services_rev; do
     ${QSERV_DIR}/etc/init.d/$service stop
 done
 # still usefull ?
-echo "Cleaning  ${QSERV_RUN}/xrootd-run/result/"
-rm -f ${QSERV_RUN}/xrootd-run/result/*
+echo "Cleaning  ${QSERV_RUN_DIR}/xrootd-run/result/"
+rm -f ${QSERV_RUN_DIR}/xrootd-run/result/*
 
