5658a05af69c3b66e02905f3a6d714c5d4afc959
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-04-10 09:59:51 +0200

    Comment : cleaning xrootd dirs when stopping Qserv

--- admin/templates/server/bin/qserv-stop.sh
+++ admin/templates/server/bin/qserv-stop.sh
@@ -8,5 +8,6 @@ for service in $services_rev; do
     ${QSERV_DIR}/etc/init.d/$service stop
 done
 # still usefull ?
+echo "Cleaning  ${QSERV_RUN}/xrootd-run/result/"
 rm -f ${QSERV_RUN}/xrootd-run/result/*
 
