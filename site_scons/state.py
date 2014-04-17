lsst/qserv

commit 63c51c29abfee5bc5573cc23413f8867f3b9d490

Fixes to cope with scons mods: wrong python path for swig lib, intolerance of empty pythonpath

site_scons/state.py
@@ -109,10 +109,10 @@ def _initVariables(src_dir):
     opts.Update(env)
 
     print "DEBUG " + str(opts)
-    print "DEBUG " + env.Dump() 
+    print "DEBUG " + env.Dump()
 
     opts.AddVariables(
-            (PathVariable('python_prefix', 'qserv install directory for python modules', os.path.join(env['prefix'], "lib", "python"), PathVariable.PathIsDirCreate))
+            (PathVariable('python_prefix', 'qserv install directory for python modules', os.path.join("lib", "python"), PathVariable.PathIsDirCreate))
             )
     opts.Update(env)
 

core/modules/SConscript
@@ -91,6 +91,7 @@ shProducts = { "xrdfs" : {'mods' : """proto xrdfs
                                'SHLIBPREFIX' : '',
                                'distDir' : os.path.join(env['python_prefix'],"lsst","qserv","master")}
                }
+
 # modules used in shProducts
 modList = [m for m in chain(*map(lambda d: d['mods'],
                                 shProducts.values()))]

site_scons/runner.py
@@ -38,7 +38,8 @@ $*
 
 def makeRunner(target=None, source=None, env=None):
     libpath = filter(None, env["LIBPATH"])
-    ppEnv = env.get("python_prefix","") + ":" + env.get("PYTHONPATH","") 
+    ppEnv = ":".join(filter(lambda x:x, [env.get("python_prefix",""),
+                                         env.get("PYTHONPATH","")]))
     fname = str(target[0])
     s = scriptTemplate % {
         "ldpath" : ":".join(map(str, libpath)), #commaReplace(env["LIBPATH"]),

commit 3c8a2a4e5f3674e504ab3f86e99317f0173d80b1

BUGFIX : incorrect install prefix management introduced in 63c51c29abfee5bc5573cc23413f8867f3b9d490

site_scons/state.py
@@ -108,11 +108,11 @@ def _initVariables(src_dir):
             )
     opts.Update(env)
 
-    print "DEBUG " + str(opts)
-    print "DEBUG " + env.Dump()
+    # print "DEBUG " + str(opts)
+    # print "DEBUG " + env.Dump() 
 
     opts.AddVariables(
-            (PathVariable('python_prefix', 'qserv install directory for python modules', os.path.join("lib", "python"), PathVariable.PathIsDirCreate))
+            (PathVariable('python_prefix', 'qserv install directory for python modules', os.path.join(env['prefix'], "lib", "python"), PathVariable.PathIsDirCreate))
             )
     opts.Update(env)
 