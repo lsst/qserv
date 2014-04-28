64af8b124ef57d5165edd488009a539d1b28312c
Jacek Becla <becla@slac.stanford.edu>
2014-04-28 14:25:23 -0700

    applied ubuntu patch (crypto lib added)

--- core/ubuntu.patch/applyIt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-(cd modules/proto;  patch SConscript.test < ../../ubuntu.patch/proto_SConscript.test)
-(cd modules/sql;    patch SConscript.test < ../../ubuntu.patch/sql_SConscript.test)
-(cd modules/wdb;    patch SConscript.test < ../../ubuntu.patch/wdb_SConscript.test)
-(cd modules/wsched; patch SConscript.test < ../../ubuntu.patch/wsched_SConscript.test)
-(cd modules/xrdfs;  patch SConscript.test < ../../ubuntu.patch/xrdfs_SConscript.test)
-(cd modules/xrdoss; patch SConscript.test < ../../ubuntu.patch/xrdoss_SConscript.test)
