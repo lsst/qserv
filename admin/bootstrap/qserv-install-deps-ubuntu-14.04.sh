d7b0f68948962d88a3bb94f83064032771d1ef4e
Jacek Becla <becla@slac.stanford.edu>
2014-06-12 13:02:18 -0700

    added missing dependency for ubuntu 14.04

--- admin/bootstrap/qserv-install-deps-ubuntu-14.04.sh
+++ admin/bootstrap/qserv-install-deps-ubuntu-14.04.sh
@@ -3,5 +3,8 @@
 # for Ubuntu wheezy/sid :
 apt-get install bison cmake curl doxygen-latex flex g++ git latex2html libboost-all-dev libcurl4-openssl-dev libglib2.0-dev libreadline-dev ncurses-dev  openjdk-7-jdk openssl python-dev python-numpy scons swig zlib1g-dev
 
+# eups
+apt-get install patch bzip2 bzip2-devel libbz2-dev
+
 # xrootd 
 apt-get install libpthread-workqueue-dev
