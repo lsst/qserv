398a6e43630a6aa0fa2f81c9270856fad2cf803b
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-10-22 19:43:34 +0200

    Separate paths management for Qserv services
    - init.d/path.cfg.sh loads paths in environment using eups or
      meta-config file,
    - source_sysconfig() loads parameters for Qserv services
      from etc/sysconfig/service-name.

---
+++ admin/templates/configuration/etc/init.d/path.cfg.sh
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Handling paths, using eups or meta-config file
+# Author: Fabrice Jammes <fabrice.jammes@in2p3.fr> (09.17.2013)
+#-------------------------------------------------------------------------------
+
+# Set up a default search path.
+if [ -n ${EUPS_DIR} ]; then
+    QSERV_ENV="(eups environment)"
+else
+    QSERV_ENV="(meta-config environment)"
+
+    # all
+    export PATH={{PATH}}
+
+    # qserv-czar
+    export PYTHONPATH={{PYTHONPATH}}
+
+    # qserv-czar, xrootd
+    export LD_LIBRARY_PATH={{LD_LIBRARY_PATH}}
+
+    # mysql-proxy
+    QSERV_DIR={{QSERV_DIR}}
+    LUA_DIR={{LUA_DIR}}
+
+    # xrootd
+    XROOTD_DIR={{XROOTD_DIR}}
+fi
