c05166111c104b7495010c0f629c874038e7e2cc
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-11-05 15:18:28 -0800

    Fix SQL scripts for worker and czar configuration
    SQL scripts launched by configuration tool for czar
    and worker didn't create the right tables at the
    right place.

--- admin/templates/configuration/tmp/configure/sql/qserv-worker.sql
+++ admin/templates/configuration/tmp/configure/sql/qserv-worker.sql
@@ -1,8 +1,18 @@
+-- Subchunk
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE ON `Subchunks\_%`.* TO 'qsmaster'@'localhost' WITH GRANT OPTION;
+
+-- Used by xrootd Qserv plugin to publish LSST databases
 DROP DATABASE IF EXISTS qservw_worker;
 CREATE DATABASE qservw_worker;
 GRANT ALL ON qservw_worker.* TO 'qsmaster'@'localhost';
-FLUSH PRIVILEGES;
 CREATE TABLE qservw_worker.Dbs (
   `db` char(200) NOT NULL
 );
+
+-- Data table
+-- Has to be created by the dataloader in the long term
+CREATE DATABASE IF NOT EXISTS LSST;
+GRANT ALL ON LSST.* TO 'qsmaster'@'localhost';
+GRANT EXECUTE ON `LSST`.* TO 'qsmaster'@'localhost';
 INSERT INTO qservw_worker.Dbs VALUES('LSST'); 
+
