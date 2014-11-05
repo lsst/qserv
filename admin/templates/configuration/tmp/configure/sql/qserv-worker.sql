2b43b58b0a5e8eaba3763ec33ea61e85c3702b4d
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-11-05 15:18:28 -0800

    Sharpen SQL scripts for worker and czar configuration
    SQL scripts launched by configuration tool for czar
    and worker now create the right tables at the
    right place, with more accurate permissions.

--- admin/templates/configuration/tmp/configure/sql/qserv-worker.sql
+++ admin/templates/configuration/tmp/configure/sql/qserv-worker.sql
@@ -1,8 +1,24 @@
+-- Used by xrootd Qserv plugin:
+
+--   to publish LSST databases
 DROP DATABASE IF EXISTS qservw_worker;
 CREATE DATABASE qservw_worker;
-GRANT ALL ON qservw_worker.* TO 'qsmaster'@'localhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON qservw_worker.* TO 'qsmaster'@'localhost';
 CREATE TABLE qservw_worker.Dbs (
   `db` char(200) NOT NULL
 );
-INSERT INTO qservw_worker.Dbs VALUES('LSST'); 
+
+CREATE DATABASE IF NOT EXISTS qservScratch;
+GRANT ALL ON qservScratch.* TO 'qsmaster'@'localhost';
+
+GRANT SELECT ON mysql.* TO 'qsmaster'@'localhost';
+GRANT ALL ON `q\_%`.* TO 'qsmaster'@'localhost';
+
+-- Subchunks databases
+GRANT ALL ON `Subchunks\_%`.* TO 'qsmaster'@'localhost';
+
+-- Database for business (i.e. LSST) data
+-- Has to be created by the dataloader in the long term
+CREATE DATABASE IF NOT EXISTS LSST;
+GRANT ALL ON LSST.* TO 'qsmaster'@'localhost';
+INSERT INTO qservw_worker.Dbs VALUES('LSST');
