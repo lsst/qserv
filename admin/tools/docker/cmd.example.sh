38f83c768f32aaeb0edbc24749cf5e3ab63ba917
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-11-14 23:10:54 +0000

    Provide clear Docker use cases

---
+++ admin/tools/docker/cmd.example.sh
@@ -0,0 +1,26 @@
+# These commands require Docker on your machine
+# and that you belong to docker group
+
+# Download and run a docker image containing qserv 2014_10
+docker run --hostname="qserv-host" --interactive=true --tty "fjammes/qserv:2014_10" /bin/bash
+
+# Below, we assume your Docker username is jdoe
+# Create it here: https://hub.docker.com
+
+# Create an image from a container
+# Show all containers and choose one 
+docker ps -all
+CONTAINER_ID=f98249856123
+# Commit your current container state in image jdoe/qserv:dm-1234
+docker commit --message="DM-1234" --author="John Doe" ${CONTAINER_ID} jdoe/qserv:dm-1234
+
+# Build an image using a Dockerfile
+docker build --tag="jdoe/qserv:2014_10" - < Dockerfile
+
+# Upload image jdoe/qserv:dm-1234 to your Docker repository
+docker push jdoe/qserv:dm-1234
+
+# Clean old local images
+docker images | grep 'hours ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
+# Clean old local containers 
+docker ps -a | grep 'hours ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm
