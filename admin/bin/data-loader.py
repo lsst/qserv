1232c503da87f5e0e4f013495393913f24196457
Andy Salnikov <salnikov@slac.stanford.edu>
2014-11-25 13:30:19 -0600

    Initial implementation of the new data loading script.
    New loading procedure is implemented as a small bunch of python modules:
      configParser.py - parses "new-style" partitioner configuration files
      partConfig.py   - extracts relevant parameters from partitioner config
      dataLoader.py   - the whole procedure for partitioning data, loading
                        it into database and updating CSS
    
    In addition to the modules that can be imported and used independently
    there is a new script data-loader.py which accepts a bunch of command
    line options and arguments and calls dataLoder.py to do actual data
    loading.
    
    See also DM-621 for discussion.

---
+++ admin/bin/data-loader.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+
+# LSST Data Management System
+# Copyright 2014 AURA/LSST.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+
+"""
+User-friendly data loading script for Qserv partitioned data.
+
+This is a driver script for (currently single-node) data loading process.
+Script is loading data for one table at a time and must be called with
+these input arguments:
+  - database/table name
+  - table schema file (SQL statement CREATE TABLE ...)
+  - one or more input data files (input for partitioner)
+  - partitioner configuration
+
+Script performs these tasks:
+  - optionally call partitioner to produce per-chunk data files
+  - create database if needed and all chunk tables in it
+  - load data into every chunk table
+  - update CSS data for database and table
+
+Script can be run in two modes:
+  1. Normal partitioning, where partitioned tables have their data split
+     into chunks and each chunk is loaded into separate table
+  2. Non-partitioned, all table data stored in one table, this is useful
+     for testing and doing comparison of qserv to direct mysql queries
+
+Currently not supported is duplication mode when data from one sky segment
+is replicated multiple times to other segments to cover whole sky. This
+option will be added later.
+
+@author  Andy Salnikov, SLAC
+
+"""
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+import argparse
+import logging
+import MySQLdb as mysql
+import warnings
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+from lsst.qserv.admin.dataLoader import DataLoader
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+class Loader(object):
+    """
+    Application class for loader application
+    """
+
+
+    def __init__(self):
+        """
+        Constructor parse all arguments and prepares for execution.
+        """
+
+        # define all command-line arguments
+        parser = argparse.ArgumentParser(description='Single-node data loading script for Qserv.')
+
+        parser.add_argument('-v', '--verbose', dest='verbose', default=[], action='append_const',
+                            const=None, help='More verbose output, can use several times.')
+
+        group = parser.add_argument_group('Partitioning options',
+                                          'Options defining how partitioning is performed')
+        group.add_argument('-f', '--config', dest='configFiles', default=[], action='append',
+                           required=True, metavar='PATH',
+                           help='Partitioner configuration file, required, more than one acceptable.')
+        group.add_argument('-d', '--chunks-dir', dest='chunksDir', metavar='PATH',
+                           default="./loader_chunks", help='Directory where to store chunk data, must '
+                           'have enough space to keep all data. If option --skip-partition is specified, '
+                           'then directory must exist and have existing data in it. Otherwise directory '
+                           'must be empty or do not exist. def: %(default)s.')
+        group.add_argument('-k', '--keep-chunks', dest='keepChunks', action='store_true', default=False,
+                           help='If specified then chunks will not be deleted after loading.')
+        group.add_argument('-s', '--skip-partition', dest='skipPart', action='store_true', default=False,
+                           help='If specified then skip partitioning, chunks must exist already '
+                           '(from previous run with -k option).')
+        group.add_argument('-1', '--one-table', dest='oneTable', action='store_true', default=False,
+                           help='If specified then load whole dataset into one table, even if it is '
+                           'partitioned. This is useful for testing quries against mysql directly.')
+
+        group = parser.add_argument_group('CSS options', 'Options controlling CSS metadata')
+        group.add_argument('-c', '--css-conn', dest='cssConn', default='localhost:12181',
+                           help='Connection string for zookeeper, def: %(default)s.')
+        group.add_argument('-r', '--css-remove', dest='cssClear', default=False, action='store_true',
+                           help='Remove CSS table info if it already exists.')
+
+        group = parser.add_argument_group('Database options', 'Options for database connection')
+        group.add_argument('-H', '--host', dest='mysqlHost', default='localhost', metavar='HOST',
+                           help='Host name for mysql server, def: %(default)s.')
+        group.add_argument('-u', '--user', dest='user', default=None,
+                           help='User name to use when connecting to server.')
+        group.add_argument('-p', '--password', dest='password', default=None,
+                           help='Password to use when connecting to server.')
+        group.add_argument('-P', '--port', dest='mysqlPort', default=3306, metavar='PORT_NUMBER',
+                           help='Port number to use for connection, def: %(default)s.')
+        group.add_argument('-S', '--socket', dest='mysqlSocket', default=None, metavar='PATH',
+                           help='The socket file to use for connection.')
+
+        parser.add_argument('database',
+                            help='Database name, Expected to exist and have correct permissions.')
+        parser.add_argument('table', help='Table name, must not exist.')
+        parser.add_argument('schema',
+                            help='Table schema file (should contain CREATE [TABLE|VIEW] ... statement).')
+        parser.add_argument('data', nargs='*',
+                            help='Input data files (CSV or anything that partitioner accepts). '
+                            'Input can be empty, e.g. in case of defining SQL view instead of '
+                            'regular table.')
+
+        # suppress some warnings from mysql
+        warnings.filterwarnings('ignore', category=mysql.Warning)
+
+        # parse all arguments
+        self.args = parser.parse_args()
+
+        verbosity = len(self.args.verbose)
+        levels = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+        logging.basicConfig(level=levels.get(verbosity, logging.DEBUG))
+
+        # connect to mysql server
+        mysqlConn = self._dbConnect()
+
+        # instantiate loader
+        self.loader = DataLoader(self.args.configFiles,
+                                 mysqlConn,
+                                 chunksDir=self.args.chunksDir,
+                                 keepChunks=self.args.keepChunks,
+                                 skipPart=self.args.skipPart,
+                                 oneTable=self.args.oneTable,
+                                 cssConn=self.args.cssConn,
+                                 cssClear=self.args.cssClear)
+
+    def run(self):
+        """
+        Do actual loading based on parameters defined in constructor.
+        This will throw exception if anything goes wrong.
+        """
+        self.loader.load(self.args.database, self.args.table, self.args.schema, self.args.data)
+        return 0
+
+
+    def _dbConnect(self):
+        """
+        Connect to mysql server, returns connection or throws exception.
+        """
+        kws = dict(local_infile=1)
+        if self.args.mysqlHost:
+            kws['host'] = self.args.mysqlHost
+        if self.args.user:
+            kws['user'] = self.args.user
+        if self.args.password:
+            kws['passwd'] = self.args.password
+        if self.args.mysqlPort:
+            kws['port'] = self.args.mysqlPort
+        if self.args.mysqlSocket:
+            kws['unix_socket'] = self.args.mysqlSocket
+
+        try:
+            return mysql.Connection(**kws)
+        except Exception as exc:
+            logging.critical('Failed to connect to mysql database: %s', exc)
+            raise
+
+
+if __name__ == "__main__":
+    try:
+        loader = Loader()
+        sys.exit(loader.run())
+    except Exception as exc:
+        logging.critical('Exception occured: %s', exc, exc_info=True)
+        sys.exit(1)
---
+++ admin/python/lsst/qserv/admin/configParser.py
@@ -0,0 +1,244 @@
+# LSST Data Management System
+# Copyright 2014 AURA/LSST.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+
+"""
+Module defining ConfigParser class and related methods.
+
+ConfigParser class is used to parse configuration files in the format
+defined by partitioner. For details check code in partitioner/CmdLineUtils.
+Most of the code is stolen from partitioner C++ code and adopted for Python.
+
+@author  Andy Salnikov, SLAC
+"""
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+class ConfigParser(object):
+    """
+    Instances of ConfigParser class are used to parse configuration from
+    a single file.
+    """
+
+    escapes = {'b': '\b', 'f': '\f', 'n': '\n', 'r': '\r', 't': '\t'}
+
+    def __init__(self, source, keySeparator='.'):
+        """
+        @param source:   file (or file-like) object which contains data to parse
+        @param keySeparator: Separator character for key components
+        """
+        self._sep = keySeparator
+        self._data = source.read()
+        self._cur = 0
+        self._end = len(self._data)
+
+    def _skipWhitespace(self):
+        """
+        Skips all whitespace characters in an input
+        """
+        while self._cur < self._end:
+            c = self._data[self._cur]
+            if c not in '\t\n\r ':
+                break
+            self._cur += 1
+
+    def _skipLine(self):
+        """
+        Skips until after new-line character
+        """
+        while self._cur < self._end:
+            c = self._data[self._cur]
+            if c in '\r\n':
+                break
+            self._cur += 1
+
+    def _join(self, keys):
+        """
+        Join all key components into a single path, return a string.
+
+        @param keys:   list of key components
+        @return   string conststing of componenets joined with separator character
+        """
+        # remove leading/trailing separators from each component
+        kres = [k.strip(self._sep) for k in keys if k]
+        return self._sep.join(kres)
+
+    def _parseValue(self):
+        """
+        get next unquoted value from a file
+        """
+        val = ""
+        while self._cur < self._end:
+            c = self._data[self._cur]
+            if c in "\t\n\r #,:=()[]{}":
+                return val
+            elif ord(c) < 0x20:
+                raise ValueError("Unquoted values must not "
+                                 "contain control characters.")
+            val += c
+            self._cur += 1
+
+        return val
+
+    def _parseUnicodeEscape(self):
+        """
+        Extracts value of the unicode value as UTF-8 encoded string
+        """
+        # Extract 1-4 hexadecimal digits to build a Unicode
+        # code-point in the Basic Multilingual Plane.
+        val = ""
+        end = min(self._end, self._cur + 4)
+        while self._cur < end:
+            c = self._data[self._cur]
+            if c in '0123456789ABCDEFabcdef':
+                val += c
+            else:
+                break
+            self._cur += 1
+
+        if not val:
+            raise ValueError("Invalid unicode escape in quoted value")
+
+        # code point
+        cp = int(val, 16)
+
+        # UTF-8 encode the code-point.
+        val = unichr(cp).encode('utf_8')
+
+        return val
+
+    def _parseQuotedValue(self, quote):
+        """
+        Extracts value of a quoted string.
+        """
+        val = ""
+        while True:
+            if self._cur >= self._end:
+                raise ValueError("Unmatched quote character.")
+
+            c = self._data[self._cur]
+            self._cur += 1
+            if c == quote:
+                break
+            elif c == '\\':
+                # Handle escape sequence.
+                if self._cur >= self._end:
+                    raise ValueError("Unmatched quote character.")
+
+                c = self._data[self._cur]
+                self._cur += 1
+                if c == 'u':
+                    c = self._parseUnicodeEscape()
+                else:
+                    c = self.escapes.get(c, c)
+
+            val += c
+
+        return val
+
+    def parse(self):
+        """
+        Parse the whole thing, return a list of (key, value) tuples, where key
+        is a full otion path name.
+        """
+        parsed = []
+        keys = []
+        groups = [(0, '')]    # list of pairs (level, open_brace)
+        lvl = 0
+        while True:
+
+            self._skipWhitespace()
+            if self._cur >= self._end:
+                break
+
+            c = self._data[self._cur]
+            s = ""
+            if c == '#':
+                self._cur += 1
+                self._skipLine()
+                continue
+            elif c == ',':
+                self._cur += 1
+                continue
+            elif c in '([{':
+                self._cur += 1
+                groups.append((lvl, c))
+                continue
+            elif c in ')]}':
+                self._cur += 1
+                p = groups[-1]
+                del groups[-1]
+                if p[1] == '(' and c != ')':
+                    raise ValueError("Unmatched (.")
+                elif p[1] == '[' and c != ']':
+                    raise ValueError("Unmatched [.")
+                elif p[1] == '{' and c != '}':
+                    raise ValueError("Unmatched {.")
+                elif p[1] == '':
+                    raise ValueError("Unmatched ), ], or }.")
+                while lvl > groups[-1][0]:
+                    del keys[-1]
+                    lvl -= 1
+                continue
+            elif c in "\"\'":
+                self._cur += 1
+                s = self._parseQuotedValue(c)
+            else:
+                s = self._parseValue()
+
+            self._skipWhitespace()
+            c = self._data[self._cur] if self._cur < self._end else ','
+            if c in ':=':
+                self._cur += 1
+                keys.append(s)
+                lvl += 1
+                continue
+
+            if not keys:
+                key = s
+                val = None
+            else:
+                key = self._join(keys)
+                val = s
+
+            while lvl > groups[-1][0]:
+                del keys[-1]
+                lvl -= 1
+
+            parsed.append((key, val))
+
+        if keys or lvl != 0 or len(groups) != 1:
+            raise ValueError("Missing value for key, "
+                                "or unmatched (, [ or {.")
+
+        return parsed
