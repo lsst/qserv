8ff89c73524668bcba15bcc1dc7f8788ffaa5a7c
Daniel L. Wang <danielw@slac.stanford.edu>
2014-11-25 18:22:40 -0800

    WIP emptychunks

---
+++ core/modules/css/EmptyChunks.cc
@@ -0,0 +1,132 @@
+// -*- LSST-C++ -*-
+/*
+ * LSST Data Management System
+ * Copyright 2014 AURA/LSST.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+#include "css/EmptyChunks.h"
+
+// System headers
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+#include <functional>
+
+// Third-party headers
+#include "boost/shared_ptr.hpp"
+#include "boost/make_shared.hpp"
+
+// Local headers
+#include "global/ConfigError.h"
+
+using lsst::qserv::ConfigError;
+using lsst::qserv::IntSet;
+
+namespace {
+// Workaround lack of copy_if before C++11
+template<class InputIt, class OutputIt, class UnaryPredicate>
+OutputIt copy_if(InputIt first, InputIt last,
+                 OutputIt d_first, UnaryPredicate pred)
+{
+    return std::remove_copy_if(first, last, d_first, std::not1(pred));
+}
+
+std::string makeDefaultFilename() {
+    return std::string("emptyChunks.txt");
+}
+
+bool isSafe(std::string::value_type const& c) {
+    std::locale loc;
+    if(std::isalnum(c)) { // Not sure that using the default locale is safe.
+        return true;
+    }
+    switch(c) { // Special cases. '_' is the only one right now.
+    case '_':
+        return true;
+    default:
+        return false;
+    }
+}
+struct isSafePred {
+    inline bool operator()(std::string::value_type const& c) const {
+        return isSafe(c);
+    }
+    typedef std::string::value_type argument_type;
+};
+
+std::string sanitizeName(std::string const& name) {
+    std::string out;
+#if 0
+    copy_if(
+        name.begin(), name.end(),
+        std::insert_iterator<std::string>(out, out.begin()),
+        isSafePred());
+#else
+    std::remove_copy_if(name.begin(), name.end(),
+                        std::insert_iterator<std::string>(out, out.begin()),
+                        std::not1(isSafePred()));
+
+#endif
+    return out;
+}
+
+std::string makeFilename(std::string const& db) {
+    return "empty_" + sanitizeName(db) + ".txt";
+}
+
+void populate(IntSet& s, std::string const& db) {
+    std::ifstream rawStream(makeFilename(db).c_str());
+    if(!rawStream.good()) { // On error, try using default filename
+        rawStream.close();
+        rawStream.open(makeDefaultFilename().c_str());
+    }
+    if(!rawStream.good()) {
+        throw ConfigError("No such empty chunks file: " + makeFilename(db)
+                          + " or " + makeDefaultFilename());
+    }
+    std::istream_iterator<int> chunkStream(rawStream);
+    std::istream_iterator<int> eos;
+    std::copy(chunkStream, eos, std::insert_iterator<IntSet>(s, s.begin()));
+}
+} // anonymous namespace
+
+namespace lsst {
+namespace qserv {
+namespace css {
+
+boost::shared_ptr<IntSet const> EmptyChunks::getEmpty(std::string const& db) {
+    boost::lock_guard<boost::mutex> lock(_setsMutex);
+    IntSetMap::const_iterator i = _sets.find(db);
+    if(i != _sets.end()) {
+        IntSetConstPtr readOnly = i->second;
+        return readOnly;
+    }
+    IntSetPtr newSet = boost::make_shared<IntSet>();
+    _sets.insert(IntSetMap::value_type(db, newSet));
+    populate(*newSet, db); // Populate reference
+    return IntSetConstPtr(newSet);
+}
+
+bool EmptyChunks::isEmpty(std::string const& db, int chunk) {
+    IntSetConstPtr s = getEmpty(db);
+    return s->end() == s->find(chunk);
+}
+
+}}} // namespace lsst::qserv::css
---
+++ core/modules/css/EmptyChunks.h
@@ -0,0 +1,83 @@
+// -*- LSST-C++ -*-
+/*
+ * LSST Data Management System
+ * Copyright 2014 AURA/LSST.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+/**
+  * @file
+  *
+  * @brief Empty-chunks tracker. Reads an on-disk file from cwd, but
+  * should ideally query (and cache) table state.
+  *
+  * @Author Daniel L. Wang, SLAC
+  */
+
+#ifndef LSST_QSERV_CSS_EMPTYCHUNKS_H
+#define LSST_QSERV_CSS_EMPTYCHUNKS_H
+
+// System headers
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// Third-party headers
+#include "boost/shared_ptr.hpp"
+#include "boost/thread/mutex.hpp"
+
+// Local headers
+#include "global/intTypes.h"
+// #include "css/KvInterface.h"
+// #include "css/MatchTableParams.h"
+// #include "css/StripingParams.h"
+
+namespace lsst {
+namespace qserv {
+namespace css {
+
+/// High-level empty-chunk-tracking class. Tracks empty chunks
+/// per-database. In the future, we will likely migrate to a
+/// per-partitioning-group scheme, at which point, we will re-think
+/// the db-based dispatch as well (user tables in the partitioning
+/// group may be extremely sparse).
+class EmptyChunks {
+public:
+    EmptyChunks() {}
+
+    // accessors
+
+    /// @return set of empty chunks for this db
+    boost::shared_ptr<IntSet const> getEmpty(std::string const& db);
+
+    /// @return true if db/chunk is empty
+    bool isEmpty(std::string const& db, int chunk);
+
+private:
+    typedef boost::shared_ptr<IntSet> IntSetPtr;
+    typedef boost::shared_ptr<IntSet const> IntSetConstPtr;
+    typedef std::map<std::string, IntSetPtr> IntSetMap;
+
+    IntSetMap _sets;
+    boost::mutex _setsMutex;
+};
+
+}}} // namespace lsst::qserv::css
+
+#endif // LSST_QSERV_CSS_EMPTYCHUNKS_H
