b8065c79f552cbdae253547469fcb3b42185d01e
Mario Juric <mjuric@lsst.org>
2014-12-14 01:40:36 -0800

    bugfix: add include file for protobuf 2.6 compatibility

--- core/modules/proto/testProtocol.cc
+++ core/modules/proto/testProtocol.cc
@@ -30,6 +30,7 @@
 
 // Third-party headers
 #include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+#include <google/protobuf/io/coded_stream.h>
 
 // Qserv headers
 #include "proto/ProtoHeaderWrap.h"
