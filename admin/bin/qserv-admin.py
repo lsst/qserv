0ac05290bd83f7ae01ea7f4df6403f824a16cfb3
Daniel L. Wang <danielw@slac.stanford.edu>
2014-12-17 18:16:11 -0800

    qserv-admin improvements for argument validation and defaulting
    This is the bulk of the work for DM-370, which is to improve
    qserv-admin defaulting and validation.
    Add force-default for storageClass in qserv-admin.py
    Add dir table and child table parameter checking to qserv-admin.py
    Add more parameter validation to qserv-admin and qservAdmin
    Add code for pulling default overlap for qservAdmin.py

--- admin/bin/qserv-admin.py
+++ admin/bin/qserv-admin.py
@@ -45,6 +45,7 @@ import sys
 # local imports
 from lsst.qserv.css.kvInterface import KvException
 from lsst.qserv.admin.qservAdmin import QservAdmin
+from lsst.qserv.admin.qservAdmin import validateMatchTableOpts
 from lsst.qserv.admin.qservAdminException import QservAdminException
 
 ####################################################################################
@@ -52,6 +53,45 @@ class CommandParser(object):
     """
     Parse commands and calls appropriate function from qservAdmin.
     """
+
+    requiredOpts = {
+        "createDb" : [ # Db opts
+            "storageClass",
+            "partitioning",
+            "partitioningStrategy"
+            ],
+        "createDbSphBox" : [
+            "nStripes",
+            "nSubStripes",
+            "overlap"],
+        "createTable" : [
+            "tableName",
+            "partitioning",
+            "schema", # "schemaFile" can be read into "schema"
+            "clusteredIndex",
+            "match",
+            "isView"],
+        "createTableSphBox" : [
+            "dirTable",
+            # "overlap" # overlap should inherit from db if unspecified.
+            ],
+        "createTableDir" : [
+            "dirColName",
+            "latColName",
+            "lonColName"
+            ],
+        "createChildTable" : [
+            "dirColName",
+            ],
+        "createChildTableExtra" : [
+            "latColName",
+            "lonColName"],
+        "createTableMatch" : [
+            "dirTable1",
+            "dirColName1",
+            "dirTable2",
+            "dirColName2"],
+        }
 
     def __init__(self, connInfo):
         """
@@ -286,54 +326,85 @@ class CommandParser(object):
                 xx[option] = config.get(section, option)
         return xx
 
+    def _checkExist(self, opts, required):
+        for r in required:
+            if r not in opts:
+                raise QservAdminException(QservAdminException.MISSING_PARAM, r)
+
     def _processDbOptions(self, opts):
         """
         Validate options used by createDb, add default values for missing
         parameters.
         """
-        if not opts.has_key("clusteredIndex"):
-            self._logger.info(
-                "param 'clusteredIndex' not found, will use default: ''")
-            opts["clusteredIndex"] = ''
-        if not opts.has_key("partitioning"):
-            self._logger.info(
-                "param 'partitioning' not found, will use default: 0")
-            opts["partitioning"] = "0"
+        self._setDefault(opts, "partitioning", "0")
+        self._setDefault(opts, "clusteredIndex", "NULL")
+        self._forceOption(opts, "storageClass", "L2")
         # these are required options for createDb
-        _crDbOpts = { 
-            "db_info": ("storageClass", 
-                        "partitioning", 
-                        "partitioningStrategy")}
-        _crDbPSOpts = {
-            "sphBox": ("nStripes", 
-                       "nSubStripes", 
-                       "overlap")}
+        self._checkExist(opts, CommandParser.requiredOpts["createDb"])
+        if opts["partitioning"] != "0":
+            if opts["partitioningStrategy"].lower() == "sphBox".lower():
+                self._checkExist(opts,
+                                 CommandParser.requiredOpts["createDbSphBox"])
+            else:
+                raise QservAdminException(QservAdminException.WRONG_PARAM,
+                                          opts["partitioningStrategy"])
         return opts
+
+    def _setDefault(self, opts, key, defaultValue):
+        if not opts.has_key(key):
+            self._logger.info(
+                "param '" + key + "' not found, will use default: " + str(defaultValue))
+            opts[key] = defaultValue
+
+    def _forceOption(self, opts, key, defaultValue):
+        if not opts.has_key(key):
+            self._logger.info(
+                "param '" + key + "' not found, will use default: " + str(defaultValue))
+            opts[key] = defaultValue
+        elif opts[key] != defaultValue:
+            raise QservAdminException(QservAdminException.WRONG_PARAM,
+                                      opts[key])
 
     def _processTbOptions(self, opts):
         """
         Validate options used by createTable, add default values for missing
         parameters.
         """
-        if not opts.has_key("clusteredIndex"):
-            self._logger.info(
-                "param 'clusteredIndex' not found, will use default: ''")
-            opts["clusteredIndex"] = "NULL"
-        if not opts.has_key("match"):
-            self._logger.info("param 'match' not found, will use default: 0")
-            opts["match"] = "0"
+        self._setDefault(opts, "partitioning", "0")
+        self._setDefault(opts, "clusteredIndex", "NULL")
+        self._setDefault(opts, "match", "0")
+        self._setDefault(opts, "isView", "0")
+        if opts.has_key("schemaFile"):
+            if opts.has_key("schema"):
+                self._logger.info("Both schema and schemaFile specified. " +
+                                  "Ignoring schemaFile")
+            else: # Create schema field from file.
+                self._logger.info("Importing schema from " + opts["schemaFile"])
+                opts["schema"] = open(opts["schemaFile"]).read()
+                # Note: Consider stripping comments here.
+        elif not opts.has_key("schema"):
+            raise QservAdminException(QservAdminException.MISSING_PARAM,
+                                      "Missing 'schema' or 'schemaFile'")
         # these are required options for createTable
-        _crTbOpts = {
-            "table_info":("tableName",
-                          "partitioning",
-                          "schemaFile",
-                          "clusteredIndex",
-                          "match",
-                          "isView")}
-        _crTbPSOpts = {
-            "sphBox":("overlap",
-                      "lonColName", 
-                      "latColName")}
+        self._checkExist(opts, CommandParser.requiredOpts["createTable"])
+        if opts["partitioning"] != "0":
+            self._processTbPartitionOpts(opts)
+        return opts
+
+    def _processTbPartitionOpts(self, opts):
+        # only sphBox allowed
+        self._checkExist(opts,
+                         CommandParser.requiredOpts["createTableSphBox"])
+        if opts.get("match", "0") != "0":
+            pass # Defer to qservAdmin to check opts
+        else:
+            # Fill-in dirTable
+            if not opts.get("dirTable", None):
+                opts["dirTable"] = opts["tableName"]
+            if opts["tableName"] == opts["dirTable"]:
+                self._checkExist(opts, CommandParser.requiredOpts["createTableDir"])
+            else: # child table
+                self._checkExist(opts, CommandParser.requiredOpts["createChildTable"])
         return opts
 
 
