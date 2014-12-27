e23963a5e44dc0122e1b986d8881ace2fbf252ec
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-12-26 23:00:35 -0800

    Add logging tool to import configuration from yaml

--- admin/python/lsst/qserv/admin/logger.py
+++ admin/python/lsst/qserv/admin/logger.py
@@ -1,23 +1,44 @@
 import logging
-import os 
+import os
+import yaml
 
-# Used to parse cli logging values 
+# Used to parse cli logging values
 verbose_dict = {
-    'DEBUG'     : logging.DEBUG,
-    'INFO'      : logging.INFO,
-    'WARNING'   : logging.WARNING,
-    'ERROR'     : logging.ERROR,
-    'FATAL'   : logging.FATAL,
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARNING': logging.WARNING,
+    'ERROR': logging.ERROR,
+    'FATAL': logging.FATAL,
 }
+
+
+def setup_logging(default_path='logging.yaml',
+                  default_level=logging.INFO,
+                  env_key='LOG_CFG'):
+    """
+    Setup logging configuration from yaml file
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
 
 def init_default_logger(log_file_prefix, level=logging.DEBUG, log_path="."):
     if level == logging.DEBUG:
-        format = '%(asctime)s {%(pathname)s:%(lineno)d} %(levelname)s %(message)s'
+        _format = '%(asctime)s {%(pathname)s:%(lineno)d} %(levelname)s %(message)s'
     else:
-        format = '%(asctime)s %(levelname)s %(message)s'
-    add_console_logger(level, format)
+        _format = '%(asctime)s %(levelname)s %(message)s'
+    add_console_logger(level, _format)
     logger = add_file_logger(log_file_prefix, level, log_path, format)
     return logger
+
 
 def add_console_logger(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s'):
     logger = logging.getLogger()
@@ -29,13 +50,14 @@ def add_console_logger(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(
 
     return logger
 
+
 def add_file_logger(log_file_prefix, level=logging.DEBUG, log_path=".", format='%(asctime)s %(levelname)s %(message)s'):
 
     logger = logging.getLogger()
     formatter = logging.Formatter(format)
     # this level can be reduce for each handler
     logger.setLevel(level)
-    logfile = os.path.join(log_path,log_file_prefix+'.log')
+    logfile = os.path.join(log_path, log_file_prefix + '.log')
     file_handler = logging.FileHandler(logfile)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
