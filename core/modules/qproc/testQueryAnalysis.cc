6d974ea224629ad4c88633376c1418d779a3c2c1
Daniel L. Wang <danielw@slac.stanford.edu>
2015-01-16 17:24:03 -0800

    Add test case for nested expressions in WHERE

--- core/modules/qproc/testQueryAnalysis.cc
+++ core/modules/qproc/testQueryAnalysis.cc
@@ -119,8 +119,10 @@ boost::shared_ptr<QuerySession> testAndCompare(QuerySession::Test& t,
                                                std::string const& expected) {
 
     boost::shared_ptr<QuerySession> qs = testStmt3(t, stmt);
-    std::string actual = computeFirst(*qs);
-    BOOST_CHECK_EQUAL(actual, expected);
+    if(qs->getError().empty()) {
+        std::string actual = computeFirst(*qs);
+        BOOST_CHECK_EQUAL(actual, expected);
+    }
     return qs;
 }
 
@@ -806,6 +808,7 @@ BOOST_AUTO_TEST_CASE(dm646) {
 }
 
 BOOST_AUTO_TEST_CASE(dm681) {
+    // Stricter sql_stmt grammer rules: reject trailing garbage
     std::string stmt = "SELECT foo FROM Filter f limit 5";
     std::string stmt2 = "SELECT foo FROM Filter f limit 5;";
     std::string stmt3 = "SELECT foo FROM Filter f limit 5;; ";
@@ -820,7 +823,26 @@ BOOST_AUTO_TEST_CASE(dm681) {
     boost::shared_ptr<QuerySession> qs;
     qs = testStmt3(qsTest, stmt, expectedErr);
     qs = testStmt3(qsTest, stmt2, expectedErr);
+}
 
+BOOST_AUTO_TEST_CASE(FuncExprPred) {
+    // DM-1784: Nested ValueExpr in function calls.
+    std::string stmt = "SELECT  o1.objectId "
+        "FROM Object o1 "
+        "WHERE ABS( (scisql_fluxToAbMag(o1.gFlux_PS)-scisql_fluxToAbMag(o1.rFlux_PS)) -              (scisql_fluxToAbMag(o1.gFlux_PS)-scisql_fluxToAbMag(o1.rFlux_PS)) ) < 1;";
+    std::string expected = "SELECT o1.objectId FROM LSST.Object_100 AS o1 WHERE ABS((scisql_fluxToAbMag(o1.gFlux_PS)-scisql_fluxToAbMag(o1.rFlux_PS))-(scisql_fluxToAbMag(o1.gFlux_PS)-scisql_fluxToAbMag(o1.rFlux_PS)))<1";
+    testAndCompare(qsTest, stmt, expected);
+    stmt = "SELECT  o1.objectId, o2.objectId "
+        "FROM Object o1, Object o2 "
+        "WHERE scisql_angSep(o1.ra_Test, o1.decl_Test, o2.ra_Test, o2.decl_Test) < 0.00001 "
+        "AND o1.objectId <> o2.objectId AND "
+        "ABS( (scisql_fluxToAbMag(o1.gFlux_PS)-scisql_fluxToAbMag(o1.rFlux_PS)) -              (scisql_fluxToAbMag(o2.gFlux_PS)-scisql_fluxToAbMag(o2.rFlux_PS)) ) < 1;";
+    expected = "SELECT o1.objectId,o2.objectId "
+        "FROM Subchunks_LSST_100.Object_100_100000 AS o1,Subchunks_LSST_100.Object_100_100000 AS o2 "
+        "WHERE scisql_angSep(o1.ra_Test,o1.decl_Test,o2.ra_Test,o2.decl_Test)<0.00001 "
+        "AND o1.objectId<>o2.objectId AND "
+        "ABS((scisql_fluxToAbMag(o1.gFlux_PS)-scisql_fluxToAbMag(o1.rFlux_PS))-(scisql_fluxToAbMag(o2.gFlux_PS)-scisql_fluxToAbMag(o2.rFlux_PS)))<1";
+    testAndCompare(qsTest, stmt, expected);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
