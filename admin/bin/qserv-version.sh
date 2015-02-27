1219c28e3412c7142313e1f1ecd577a9e086e6d8
Jacek Becla <becla@slac.stanford.edu>
2015-02-27 17:15:02 -0600

    Switch to a new release.

--- admin/bin/qserv-version.sh
+++ admin/bin/qserv-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # LSST Data Management System
-# Copyright 2013-2014 LSST Corporation.
+# Copyright 2013-2015 LSST Corporation.
 #
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -25,5 +25,5 @@
 # Used to determine if Qserv run dir is compliant with Qserv version
 
 # @author  Fabrice Jammes, IN2P3
-QSERV_VERSION="2015_02"
+QSERV_VERSION="2015_03"
 echo "${QSERV_VERSION}"
--- doc/source/toplevel.rst
+++ doc/source/toplevel.rst
@@ -6,5 +6,5 @@ Qserv documentation
 Available versions
 ******************
 
-* Last release `2015_01 <2015_01>`_
-* Under development `2015_02 <2015_02>`_
+* Last release `2015_02 <2015_02>`_
+* Under development `2015_03 <2015_03>`_
