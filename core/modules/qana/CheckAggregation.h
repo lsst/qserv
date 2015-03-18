2f8cc9aea857bf3ea80ce8a68979744ce66b9918
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-03-18 14:55:19 -0700

    Remove trailing whitespaces

--- core/modules/qana/CheckAggregation.h
+++ core/modules/qana/CheckAggregation.h
@@ -1,41 +1,41 @@
-// -*- LSST-C++ -*-                                                                                                                                                                      
-/*                                                                                                                                                                                       
- * LSST Data Management System                                                                                                                                                           
- * Copyright 2014-2015 AURA/LSST.                                                                                                                                                        
- *                                                                                                                                                                                       
- * This product includes software developed by the                                                                                                                                       
- * LSST Project (http://www.lsst.org/).                                                                                                                                                  
- *                                                                                                                                                                                       
- * This program is free software: you can redistribute it and/or modify                                                                                                                  
- * it under the terms of the GNU General Public License as published by                                                                                                                  
- * the Free Software Foundation, either version 3 of the License, or                                                                                                                     
- * (at your option) any later version.                                                                                                                                                   
- *                                                                                                                                                                                       
- * This program is distributed in the hope that it will be useful,                                                                                                                       
- * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                                                                        
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                                                                                         
- * GNU General Public License for more details.                                                                                                                                          
- *                                                                                                                                                                                       
- * You should have received a copy of the LSST License Statement and                                                                                                                     
- * the GNU General Public License along with this program.  If not,                                                                                                                      
- * see <http://www.lsstcorp.org/LegalNotices/>.                                                                                                                                          
+// -*- LSST-C++ -*-
+/*
+ * LSST Data Management System
+ * Copyright 2014-2015 AURA/LSST.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
  */
-/**                                                                                                                                                                                      
- * @file                                                                                                                                                                                 
- *                                                                                                                                                                                       
+/**
+ * @file
+ *
  * @ingroup qana
- *                                                                                                                                                                                       
+ *
  * @brief Functor class to check if a FactorOp
  *        is related to a SQL aggregation function
- *                                                                                                                                                                                       
- * @author Fabrice Jammes, IN2P3/SLAC                                                                                                                                                    
+ *
+ * @author Fabrice Jammes, IN2P3/SLAC
  */
 
-// System headers                                                                                                                                                                        
+// System headers
 
 // Third-party headers
 
-// Qserv headers 
+// Qserv headers
 #include "query/ValueExpr.h"
 #include "query/ValueFactor.h"
 
