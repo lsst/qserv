055e1f5d25bba3e9f8e4b01c9a44873dfcf12769
Andy Salnikov <salnikov@slac.stanford.edu>
2015-03-19 13:19:13 -0500

    Initial implementation of the worker manager service (DM-2176)
    This commit includes database-related AP (everything under /dbs) and it
    is not complete. Data loading piece needs more work to understand how to
    do it in a most efficient manner. All other database operations are
    implemented. There is no test yet, will be provided in later commits.
    
    Remaining things to be done: integrate with qserv-configure, add xrootd
    API and process management API.

---
+++ core/modules/wmgr/bin/qservWmgr.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+# LSST Data Management System
+# Copyright 2015 AURA/LSST.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+
+"""
+Application which provides worker management service (HTTP-based).
+
+@author  Andy Salnikov, SLAC
+"""
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import argparse
+import logging
+import sys
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+from flask import Flask
+from lsst.qserv.wmgr.config import Config
+from lsst.qserv.wmgr import dbMgr
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Single-node data loading script for Qserv.')
+
+    parser.add_argument('-v', '--verbose', dest='verbose', default=[], action='append_const',
+                        const=None, help='More verbose output, can use several times.')
+    parser.add_argument('-c', '--config', dest='configFile', default=None, metavar='PATH',
+                        help='Read configuration from provided file. By default configuration'
+                        ' is loaded from a file specified with env. variable WMGRCONFIG.')
+    args = parser.parse_args()
+
+    # configure logging
+    verbosity = len(args.verbose)
+    levels = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+    level = levels.get(verbosity, logging.DEBUG)
+    fmt = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    logging.basicConfig(level=level, format=fmt)
+
+    # instanciate and configure app
+    app = Flask(__name__)
+    if args.configFile is not None:
+        app.config.from_pyfile(args.configFile)
+    else:
+        app.config.from_envvar('WMGRCONFIG')
+    Config.init(app)
+
+    # add few blueprints
+    app.register_blueprint(dbMgr.dbService, url_prefix='/dbs')
+
+    # run
+    app.run()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ core/modules/wmgr/python/errors.py
@@ -0,0 +1,65 @@
+#
+# LSST Data Management System
+# Copyright 2015 AURA/LSST.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+"""
+Module defining dbMgr class and related methods.
+
+@author Andy Salnikov - salnikov@slac.stanford.edu
+"""
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+from flask import json
+from werkzeug.exceptions import HTTPException
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+def errorResponse(code, errorType, message, cause=None):
+    """
+    Make JSON error response, structure follows our guidelines document.
+    """
+    excData = dict(exception=errorType, message=message)
+    if cause:
+        excData['cause'] = cause
+    response = json.jsonify(excData)
+    response.status_code = code
+    return response
+
+class ExceptionResponse(HTTPException):
+    """
+    Exception class that will be converted to error response.
+    """
+
+    def __init__(self, code, errorType, message, cause=None):
+        response = errorResponse(code, errorType, message, cause)
+        HTTPException.__init__(self, response=response)
