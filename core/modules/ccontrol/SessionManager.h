abd7b58598dcb79bfe101d1f65a74cdcd1c430ab
Jacek Becla <becla@slac.stanford.edu>
2015-05-08 11:34:51 -0700

    copyright info updated

--- core/modules/ccontrol/SessionManager.h
+++ core/modules/ccontrol/SessionManager.h
@@ -1,7 +1,7 @@
 // -*- LSST-C++ -*-
 /*
  * LSST Data Management System
- * Copyright 2009-2014 LSST Corporation.
+ * Copyright 2009-2015 LSST Corporation.
  *
  * This product includes software developed by the
  * LSST Project (http://www.lsst.org/).
--- core/modules/qdisp/MessageStore.cc
+++ core/modules/qdisp/MessageStore.cc
@@ -1,7 +1,7 @@
 // -*- LSST-C++ -*-
 /*
  * LSST Data Management System
- * Copyright 2008-2014 LSST Corporation.
+ * Copyright 2008-2015 LSST Corporation.
  *
  * This product includes software developed by the
  * LSST Project (http://www.lsst.org/).
