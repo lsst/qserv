be9176d63fd16314e5623748c98abaee170be200
Andy Salnikov <salnikov@slac.stanford.edu>
2015-11-20 16:42:43 -0600

    Fixing regex issue for gcc48 (DM-4398)
    Moved all parsing code to a separate class UserQueryType which is now
    testable and wrote unt test for that. Replaced std::regex with boost.

--- core/modules/ccontrol/testCControl.cc
+++ core/modules/ccontrol/testCControl.cc
@@ -34,6 +34,7 @@
 
 // Qserv headers
 #include "ccontrol/userQueryProxy.h"
+#include "ccontrol/UserQueryType.h"
 
 namespace test = boost::test_tools;
 using namespace lsst::qserv;
@@ -48,6 +49,59 @@ BOOST_AUTO_TEST_CASE(UserQueryException) {
     ccontrol::UserQuery_kill(session++);
     ccontrol::UserQuery_submit(session++);
     ccontrol::UserQuery_join(session++);
+}
+
+BOOST_AUTO_TEST_CASE(testUserQueryType) {
+    LOGF_INFO("testing UserQueryType class");
+
+    using lsst::qserv::ccontrol::UserQueryType;
+
+    BOOST_CHECK(UserQueryType::isSelect("SELECT 1"));
+    BOOST_CHECK(UserQueryType::isSelect("SELECT\t1"));
+    BOOST_CHECK(UserQueryType::isSelect("SELECT\n\r1"));
+
+    BOOST_CHECK(UserQueryType::isSelect("select 1"));
+    BOOST_CHECK(UserQueryType::isSelect("SeLeCt 1"));
+
+    BOOST_CHECK(not UserQueryType::isSelect("unselect X"));
+    BOOST_CHECK(not UserQueryType::isSelect("DROP SELECT;"));
+
+    struct {
+        const char* query;
+        const char* db;
+        const char* table;
+    } drop_table_ok[] = {
+        {"DROP TABLE DB.TABLE", "DB", "TABLE"},
+        {"DROP TABLE DB.TABLE;", "DB", "TABLE"},
+        {"DROP TABLE DB.TABLE ;", "DB", "TABLE"},
+        {"DROP TABLE `DB`.`TABLE` ", "DB", "TABLE"},
+        {"DROP TABLE \"DB\".\"TABLE\"", "DB", "TABLE"},
+        {"DROP TABLE TABLE", "", "TABLE"},
+        {"DROP TABLE `TABLE`", "", "TABLE"},
+        {"DROP TABLE \"TABLE\"", "", "TABLE"},
+        {"drop\ttable\nDB.TABLE ;", "DB", "TABLE"}
+    };
+
+    for (auto test: drop_table_ok) {
+        std::string db, table;
+        BOOST_CHECK(UserQueryType::isDropTable(test.query, db, table));
+        BOOST_CHECK_EQUAL(db, test.db);
+        BOOST_CHECK_EQUAL(table, test.table);
+    }
+
+    const char* drop_table_fail[] = {
+        "DROP DATABASE DB",
+        "DROP TABLE",
+        "DROP TABLE TABLE; DROP IT;",
+        "DROP TABLE 'DB'.'TABLE'",
+        "DROP TABLE db%.TABLE",
+        "UNDROP TABLE X"
+    };
+    for (auto test: drop_table_fail) {
+        std::string db, table;
+        BOOST_CHECK(not UserQueryType::isDropTable(test, db, table));
+    }
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
