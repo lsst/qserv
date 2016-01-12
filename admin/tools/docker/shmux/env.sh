05a79c99e2c3f651a71058b881be34a73adb9559
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-01-12 17:30:02 +0100

    Improve shmux scripts
    - Use ssh instead of shmux for master node
    - Simplify css file management
    - Detach docker container

--- admin/tools/docker/shmux/env.sh
+++ admin/tools/docker/shmux/env.sh
@@ -1,6 +1,9 @@
-MASTER=clrqserv00
-WORKERS=$(echo clrqserv0{1..3})
+MASTER=fjammes-qserv-1
+WORKERS=$(echo fjammes-qserv-{2..3})
 
-MASTER_IMAGE=fjammes/qserv:master_master_clrqserv00.in2p3.fr
-WORKER_IMAGE=fjammes/qserv:master_worker_clrqserv00.in2p3.fr
-#alias shmux=/usr/local/bin/shmux
+DOCKER_ORG=qserv
+VERSION=dev
+MASTER_IMAGE="$DOCKER_ORG/qserv:${VERSION}_master_$MASTER"
+WORKER_IMAGE="$DOCKER_ORG/qserv:${VERSION}_worker_$MASTER"
+
+CSS_FILE=nodes.css
