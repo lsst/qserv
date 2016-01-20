bacfb1108a0a36dd6150788601763664f19d3d04
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-01-20 17:26:18 +0100

    Add example files for deployment procedure

--- admin/tools/docker/shmux/env.example.sh
+++ admin/tools/docker/shmux/env.example.sh
@@ -1,7 +1,9 @@
-MASTER=fjammes-qserv-1
-WORKERS=$(echo fjammes-qserv-{2..3})
+MASTER=qserv00.domain.org
+WORKERS=$(echo qserv0{1..3}.domain.org)
 
 DOCKER_ORG=qserv
+# VERSION can be ticket branch but with _ instead of /
+# example: u_fjammes_DM-4295 
 VERSION=dev
 MASTER_IMAGE="$DOCKER_ORG/qserv:${VERSION}_master_$MASTER"
 WORKER_IMAGE="$DOCKER_ORG/qserv:${VERSION}_worker_$MASTER"
