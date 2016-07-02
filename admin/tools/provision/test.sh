b7c5e0c1bd7e09e15cc9b7c4f98a39402d1fb4eb
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-07-02 16:11:28 +0200

    Fix test script

--- admin/tools/provision/test.sh
+++ admin/tools/provision/test.sh
@@ -9,13 +9,14 @@
 # @author  Oualid Achbal, IN2P3
 
 set -e
+set -x
 
 # Source the cloud openrc file
 # and choose the configuration file which contains instance parameters
 
 # For NCSA
-. ./LSST-openrc.sh.example
-CONF_FILE="ncsa.conf.example"
+# . ./LSST-openrc.sh
+CONF_FILE="ncsa.conf"
 
 # For Petasky/Galactica
 # . ./petasky-openrc.sh
@@ -27,8 +28,9 @@ NB_SERVERS=3
 # Choose snapshot name and update the cloud conf file chosen
 SNAPSHOT_NAME="centos-7-qserv"
 
+SAFE_USERNAME=$(echo  "$OS_USERNAME" | sed 's/\.//g')
 # Delete the previous instances for a new test
-PREVIOUS_INSTANCE_IDS=$(openstack server list | grep "$OS_USERNAME-qserv" | cut -d'|' -f 2)
+PREVIOUS_INSTANCE_IDS=$(openstack server list | grep "$SAFE_USERNAME-qserv" | cut -d'|' -f 2)
 # Test PREVIOUS_INSTANCE_IDS 
 if [ "$PREVIOUS_INSTANCE_IDS" ]
 then
@@ -66,12 +68,15 @@ cp ssh_config ~/.ssh/config
 
 cd ../docker/deployment/parallel
 
+WORKER_LAST_ID=$(expr $NB_SERVERS - 1)
+HOSTNAME_FORMAT="${SAFE_USERNAME}-qserv-%g"
+
 # Update env.sh
-# cp env.example.sh env.sh
-sed -i -e 's/$HOSTNAME_FORMAT/$OS_USERNAME-qserv-%g/g' env.sh
+cp env.example.sh env.sh
+sed -i -e "s/HOSTNAME_FORMAT=/HOSTNAME_FORMAT=$HOSTNAME_FORMAT #/g" env.sh
 sed -i -e "s/MASTER_ID=1/MASTER_ID=0/" env.sh
 sed -i -e "s/WORKER_FIRST_ID=2/WORKER_FIRST_ID=1/" env.sh
-sed -i -e "s/WORKER_LAST_ID=3/WORKER_LAST_ID=$NB_SERVERS/" env.sh
+sed -i -e "s/WORKER_LAST_ID=3/WORKER_LAST_ID=$WORKER_LAST_ID/" env.sh
 
 # Run multinode tests
 ./run-multinode-tests.sh
