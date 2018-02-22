811024e5d914de0c1b095c32111cf7aff5dfe4e9
Nate Pease <npease@slac.stanford.edu>
2018-02-22 15:48:08 -0800

    change Listener handler to be called Adapter

--- core/modules/parser/MySqlListener.cc
+++ core/modules/parser/MySqlListener.cc
@@ -45,17 +45,16 @@ public:
     virtual void handleUidString(const std::string& string) = 0;
 };
 
-/// Listener classes
+/// Adapter classes
 
-class Listener {
+class Adapter {
 public:
-    virtual ~Listener() {}
-    typedef std::shared_ptr<Listener> Ptr;
+    virtual ~Adapter() {}
 };
 
 namespace {
 
-class RootListener : public Listener {
+class RootAdapter : public Adapter {
 public:
     query::SelectStmt::Ptr selectStatement();
 private:
@@ -63,19 +62,19 @@ private:
 };
 
 
-class DMLListener : public Listener {
+class DMLAdapter : public Adapter {
 };
 
 
-class SelectListener : public Listener {
+class SelectAdapter : public Adapter {
 };
 
 
-class SelectElementsListener : public Listener {
+class SelectElementsAdapter : public Adapter {
 };
 
 
-class SelectColumnElementListener : public Listener, public UidCBH {
+class SelectColumnElementAdapter : public Adapter, public UidCBH {
     virtual void handleUidString(const std::string& string) {
         LOGS(_log, LOG_LVL_ERROR, __FUNCTION__ << string);
     }
@@ -88,32 +87,32 @@ class SelectColumnElementListener : public Listener, public UidCBH {
 } // end namespace
 
 
-template<typename ParentListener, typename ChildListener>
-std::shared_ptr<ChildListener> MySqlListener::pushListenerStack() {
-    auto p = std::dynamic_pointer_cast<ParentListener>(_listenerStack.top());
+template<typename ParentAdapter, typename ChildAdapter>
+std::shared_ptr<ChildAdapter> MySqlListener::pushAdapterStack() {
+    auto p = std::dynamic_pointer_cast<ParentAdapter>(_adapterStack.top());
     if (nullptr == p) {
             int status;
-            LOGS(_log, LOG_LVL_ERROR, "can't acquire expected Listener " <<
-                    abi::__cxa_demangle(typeid(ParentListener).name(),0,0,&status) <<
+            LOGS(_log, LOG_LVL_ERROR, "can't acquire expected Adapter " <<
+                    abi::__cxa_demangle(typeid(ParentAdapter).name(),0,0,&status) <<
                     " from top of listenerStack."); // todo add some type names
         // might want to throw here...?
         return nullptr;
     }
-    auto childListener = std::make_shared<ChildListener>();
-    _listenerStack.push(childListener);
-    return childListener;
+    auto childAdapter = std::make_shared<ChildAdapter>();
+    _adapterStack.push(childAdapter);
+    return childAdapter;
 }
 
 
-template<typename ChildListener>
-void MySqlListener::popListenerStack() {
-    shared_ptr<Listener> listenerPtr = _listenerStack.top();
-    _listenerStack.pop();
-    shared_ptr<ChildListener> derivedPtr = dynamic_pointer_cast<ChildListener>(listenerPtr);
+template<typename ChildAdapter>
+void MySqlListener::popAdapterStack() {
+    shared_ptr<Adapter> listenerPtr = _adapterStack.top();
+    _adapterStack.pop();
+    shared_ptr<ChildAdapter> derivedPtr = dynamic_pointer_cast<ChildAdapter>(listenerPtr);
     if (nullptr == derivedPtr) {
         int status;
         LOGS(_log, LOG_LVL_ERROR, "Top of listenerStack was not of expected type. " <<
-                "Expected: " << abi::__cxa_demangle(typeid(ChildListener).name(),0,0,&status) <<
+                "Expected: " << abi::__cxa_demangle(typeid(ChildAdapter).name(),0,0,&status) <<
                 " Actual: " << abi::__cxa_demangle(typeid(listenerPtr).name(),0,0,&status) <<
                 " Are there out of order or unhandled listener exits?"); // todo add some type names
         // might want to throw here...?
@@ -121,15 +120,15 @@ void MySqlListener::popListenerStack() {
 }
 
 
-// might want to use this in popListenerStack?
-template<typename ChildListener>
-std::shared_ptr<ChildListener> MySqlListener::listenerStackTop() const {
-    shared_ptr<Listener> listenerPtr = _listenerStack.top();
-    shared_ptr<ChildListener> derivedPtr = dynamic_pointer_cast<ChildListener>(listenerPtr);
+// might want to use this in popAdapterStack?
+template<typename ChildAdapter>
+std::shared_ptr<ChildAdapter> MySqlListener::adapterStackTop() const {
+    shared_ptr<Adapter> listenerPtr = _adapterStack.top();
+    shared_ptr<ChildAdapter> derivedPtr = dynamic_pointer_cast<ChildAdapter>(listenerPtr);
     if (nullptr == derivedPtr) {
         int status;
         LOGS(_log, LOG_LVL_ERROR, "Top of listenerStack was not of expected type. " <<
-                "Expected: " << abi::__cxa_demangle(typeid(ChildListener).name(),0,0,&status) <<
+                "Expected: " << abi::__cxa_demangle(typeid(ChildAdapter).name(),0,0,&status) <<
                 " Actual: " << abi::__cxa_demangle(typeid(listenerPtr).name(),0,0,&status));
         // might want to throw here?
     }
@@ -141,69 +140,69 @@ void MySqlListener::enterRoot(MySqlParser::RootContext * ctx) {
     // since there's no parent listener on the stack for a root listener, we don't use the template push
     // function, we just push the first item onto the stack by hand like so:
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    _rootListener = std::make_shared<RootListener>();
-    _listenerStack.push(_rootListener);
+    _rootAdapter = std::make_shared<RootAdapter>();
+    _adapterStack.push(_rootAdapter);
 }
 
 
 void MySqlListener::exitRoot(MySqlParser::RootContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    popListenerStack<RootListener>();
+    popAdapterStack<RootAdapter>();
 }
 
 
 void MySqlListener::enterDmlStatement(MySqlParser::DmlStatementContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    pushListenerStack<RootListener, DMLListener>();
+    pushAdapterStack<RootAdapter, DMLAdapter>();
 }
 
 
 void MySqlListener::exitDmlStatement(MySqlParser::DmlStatementContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    popListenerStack<DMLListener>();
+    popAdapterStack<DMLAdapter>();
 }
 
 
 void MySqlListener::enterSimpleSelect(MySqlParser::SimpleSelectContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    pushListenerStack<DMLListener, SelectListener>();
+    pushAdapterStack<DMLAdapter, SelectAdapter>();
 }
 
 
 void MySqlListener::exitSimpleSelect(MySqlParser::SimpleSelectContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    popListenerStack<SelectListener>();
+    popAdapterStack<SelectAdapter>();
 }
 
 
 void MySqlListener::enterSelectElements(MySqlParser::SelectElementsContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    pushListenerStack<SelectListener, SelectElementsListener>();
+    pushAdapterStack<SelectAdapter, SelectElementsAdapter>();
 }
 
 
 void MySqlListener::exitSelectElements(MySqlParser::SelectElementsContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    popListenerStack<SelectElementsListener>();
+    popAdapterStack<SelectElementsAdapter>();
 }
 
 
 void MySqlListener::enterSelectColumnElement(MySqlParser::SelectColumnElementContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    pushListenerStack<SelectElementsListener, SelectColumnElementListener>();
+    pushAdapterStack<SelectElementsAdapter, SelectColumnElementAdapter>();
 }
 
 
 void MySqlListener::exitSelectColumnElement(MySqlParser::SelectColumnElementContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    popListenerStack<SelectColumnElementListener>();
+    popAdapterStack<SelectColumnElementAdapter>();
 }
 
 
 void MySqlListener::enterUid(MySqlParser::UidContext * ctx) {
     LOGS(_log, LOG_LVL_DEBUG, __FUNCTION__);
-    shared_ptr<UidCBH> handler = listenerStackTop<UidCBH>();
-//    shared_ptr<SelectColumnElementListener> handler = listenerStackTop<SelectColumnElementListener>();
+    shared_ptr<UidCBH> handler = adapterStackTop<UidCBH>();
+//    shared_ptr<SelectColumnElementAdapter> handler = adapterStackTop<SelectColumnElementAdapter>();
 }
 
 
