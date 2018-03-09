cc000d030188d6e93d310349612bba247e3381c7
John Gates <jgates@slac.stanford.edu>
2018-03-09 15:42:18 -0800

    Working UDP async echo server and sync client.

--- core/modules/loader/udpServer.cc
+++ core/modules/loader/udpServer.cc
@@ -24,35 +24,34 @@ public:
         socket_.async_receive_from(
                 boost::asio::buffer(data_, max_length), sender_endpoint_,
                 boost::bind(&server::handle_receive_from, this,
-                        boost::asio::placeholders::error,
-                        boost::asio::placeholders::bytes_transferred));
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
     }
 
     void handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd) {
         if (!error && bytes_recvd > 0) {
             std::string str(data_, bytes_recvd);
-            std::cout << "received(" << bytes_recvd << "):" << str << std::endl;
-            socket_.async_send_to(
-                    boost::asio::buffer(data_, bytes_recvd), sender_endpoint_,
-                    boost::bind(&server::handle_send_to, this,
-                            boost::asio::placeholders::error,
-                            boost::asio::placeholders::bytes_transferred));
+            std::cout << "received(" << bytes_recvd << "):" << str
+                      <<", error code: " << error << ", from endpoint " << sender_endpoint_ << std::endl;
+            socket_.async_send_to(boost::asio::buffer(data_, bytes_recvd), sender_endpoint_,
+                                  boost::bind(&server::handle_send_to, this,
+                                              boost::asio::placeholders::error,
+                                              boost::asio::placeholders::bytes_transferred));
         } else {
             std::cout << "received error or empty bytes=" << bytes_recvd << std::endl;
-            socket_.async_receive_from(
-                    boost::asio::buffer(data_, max_length), sender_endpoint_,
-                    boost::bind(&server::handle_receive_from, this,
-                            boost::asio::placeholders::error,
-                            boost::asio::placeholders::bytes_transferred));
+            socket_.async_receive_from(boost::asio::buffer(data_, max_length), sender_endpoint_,
+                                       boost::bind(&server::handle_receive_from, this,
+                                                   boost::asio::placeholders::error,
+                                                   boost::asio::placeholders::bytes_transferred));
         }
     }
 
     void handle_send_to(const boost::system::error_code& error, size_t bytes_sent) {
-        socket_.async_receive_from(
-                boost::asio::buffer(data_, max_length), sender_endpoint_,
-                boost::bind(&server::handle_receive_from, this,
-                        boost::asio::placeholders::error,
-                        boost::asio::placeholders::bytes_transferred));
+        std::cout << "  handle_send_to bytes_sent=" << bytes_sent << std::endl;
+        socket_.async_receive_from(boost::asio::buffer(data_, max_length), sender_endpoint_,
+                                   boost::bind(&server::handle_receive_from, this,
+                                               boost::asio::placeholders::error,
+                                               boost::asio::placeholders::bytes_transferred));
     }
 
 private:
