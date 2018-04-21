290c9751237dd939e1c0bca293ca0cf7df26b080
Nate Pease <npease@slac.stanford.edu>
2018-04-20 18:36:51 -0700

    rename ValueFactor::_tableStar to constVal

--- core/modules/qana/RelationGraph.cc
+++ core/modules/qana/RelationGraph.cc
@@ -269,8 +269,8 @@ double getNumericConst(ValueExprPtr const& ve) {
         return std::numeric_limits<double>::quiet_NaN();
     }
     char *e = nullptr;
-    double a = std::strtod(vf->getTableStar().c_str(), &e);
-    if (e == vf->getTableStar().c_str()) {
+    double a = std::strtod(vf->getConstVal().c_str(), &e);
+    if (e == vf->getConstVal().c_str()) {
         // conversion error - non-numeric constant
         return std::numeric_limits<double>::quiet_NaN();
     }
