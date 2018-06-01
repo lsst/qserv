c2f4453c560b144cc7380da73c8ef2e951d10bf5
John Gates <jgates@slac.stanford.edu>
2018-06-01 14:14:00 -0700

    Added a timer and made minor alterations to queues.

--- core/modules/qdisp/QdispPool.h
+++ core/modules/qdisp/QdispPool.h
@@ -127,7 +127,7 @@ public:
         _prQueue->addPriQueue(0, 1, 90);  // Highest priority - interactive queries
         _prQueue->addPriQueue(1, 1, 90);  // Outgoing shared scan queries.
         _prQueue->addPriQueue(2, 6, 25);  // FAST queries (Object table)
-        _prQueue->addPriQueue(3, 6, 25);  // MEDIUM queries (Source table)
+        _prQueue->addPriQueue(3, 7, 50);  // MEDIUM queries (Source table)
         _prQueue->addPriQueue(4, 6, 25);  // SLOW queries (Object Extra table)
         _prQueue->addPriQueue(5, 6, 12);  // FAST large results
         _prQueue->addPriQueue(6, 6, 12);  // MEDIUM large results
@@ -170,7 +170,7 @@ public:
 private:
     /// The default priority queue is meant for pool control commands.
     PriorityQueue::Ptr _prQueue = std::make_shared<PriorityQueue>(100, 1, 5); // default (lowest) priority.
-    util::ThreadPool::Ptr _pool{util::ThreadPool::newThreadPool(80, _prQueue)};
+    util::ThreadPool::Ptr _pool{util::ThreadPool::newThreadPool(100, _prQueue)};
 };
 
 
--- core/modules/qdisp/QueryRequest.cc
+++ core/modules/qdisp/QueryRequest.cc
@@ -46,6 +46,7 @@
 #include "qdisp/JobStatus.h"
 #include "qdisp/ResponseHandler.h"
 #include "util/common.h"
+#include "util/Timer.h" // &&&
 
 namespace {
 LOG_LOGGER _log = LOG_GET("lsst.qserv.qdisp.QueryRequest");
@@ -66,8 +67,10 @@ public:
 
     void action(util::CmdData *data) override {
         // If everything is ok, call GetResponseData to have XrdSsi ask the worker for the data.
+        util::Timer tWaiting; // &&&
+        util::Timer tTotal; // &&&
         {
-            util::InstanceCount _ic{"1-act-AskForResponseDataCmd"};
+            tTotal.start();
             auto jq = _jQuery.lock();
             auto qr = _qRequest.lock();
             if (jq == nullptr || qr == nullptr) {
@@ -85,6 +88,7 @@ public:
             }
             std::vector<char>& buffer = jq->getDescription()->respHandler()->nextBuffer();
             LOGS(_log, LOG_LVL_DEBUG, _idStr << " AskForResp GetResponseData size=" << buffer.size());
+            tWaiting.start();
             qr->GetResponseData(&buffer[0], buffer.size());
         }
 
@@ -95,6 +99,7 @@ public:
             std::unique_lock<std::mutex> uLock(_mtx);
             // TODO: make timed wait, check for wedged, if weak pointers dead, log and give up.
             _cv.wait(uLock, [this](){ return _state != State::STARTED0; });
+            tWaiting.stop();
             // _mtx is locked at this point.
             LOGS(_log, LOG_LVL_DEBUG, _idStr << " AskForResp should be DATAREADY1 " << (int)_state);
             if (_state == State::DONE2) {
@@ -112,7 +117,6 @@ public:
         // If more data needs to be sent, _processData will make a new AskForResponseDataCmd
         // object and queue it.
         {
-            util::InstanceCount _ic{"3-act-AskForResponseDataCmd&&&"};
             auto jq = _jQuery.lock();
             auto qr = _qRequest.lock();
             if (jq == nullptr || qr == nullptr) {
@@ -124,9 +128,12 @@ public:
             qr->_processData(jq, _blen, _last);
             LOGS(_log, LOG_LVL_DEBUG, _idStr << " &&& AskForResp processing end" << _blen);
             // _processData will have created another AskForResponseDataCmd object if needed.
+            tTotal.stop();
         }
         _setState(State::DONE2);
         LOGS(_log, LOG_LVL_DEBUG, _idStr << " Ask data is done.");
+        LOGS(_log, LOG_LVL_DEBUG, _idStr << " &&& AskForResp time wait=" << tWaiting.getElapsed() <<
+                " total=" << tTotal.getElapsed());
     }
 
     void notifyDataSuccess(int blen, bool last) {
