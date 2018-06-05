9cd3d6f4535b343ed190ba4820ae1583b5b9a916
John Gates <jgates@slac.stanford.edu>
2018-06-05 09:27:16 -0700

    Increased number of threads.

--- core/modules/qdisp/QdispPool.h
+++ core/modules/qdisp/QdispPool.h
@@ -125,13 +125,13 @@ public:
         // slow queries at the same time a burden on the system.
         // TODO Set up thread pool size and queues in configuration.
         _prQueue->addPriQueue(0, 1, 90);  // Highest priority - interactive queries
-        _prQueue->addPriQueue(1, 1, 90);  // Outgoing shared scan queries.
-        _prQueue->addPriQueue(2, 6, 25);  // FAST queries (Object table)
-        _prQueue->addPriQueue(3, 7, 50);  // MEDIUM queries (Source table)
-        _prQueue->addPriQueue(4, 6, 25);  // SLOW queries (Object Extra table)
-        _prQueue->addPriQueue(5, 6, 12);  // FAST large results
-        _prQueue->addPriQueue(6, 6, 12);  // MEDIUM large results
-        _prQueue->addPriQueue(7, 6, 12);  // Everything else (slow things)
+        _prQueue->addPriQueue(1, 1, 150);  // Outgoing shared scan queries.
+        _prQueue->addPriQueue(2, 6, 100);  // FAST queries (Object table)
+        _prQueue->addPriQueue(3, 7, 100);  // MEDIUM queries (Source table)
+        _prQueue->addPriQueue(4, 6, 100);  // SLOW queries (Object Extra table)
+        _prQueue->addPriQueue(5, 6, 20);  // FAST large results
+        _prQueue->addPriQueue(6, 6, 20);  // MEDIUM large results
+        _prQueue->addPriQueue(7, 6, 20);  // Everything else (slow things)
         // default priority is the lowest priority.
     }
 
@@ -170,7 +170,7 @@ public:
 private:
     /// The default priority queue is meant for pool control commands.
     PriorityQueue::Ptr _prQueue = std::make_shared<PriorityQueue>(100, 1, 5); // default (lowest) priority.
-    util::ThreadPool::Ptr _pool{util::ThreadPool::newThreadPool(100, _prQueue)};
+    util::ThreadPool::Ptr _pool{util::ThreadPool::newThreadPool(500, _prQueue)};
 };
 
 
