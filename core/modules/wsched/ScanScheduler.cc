f727260b8292999f678cbad87ab588f00a4a0292
John Gates <jgates@slac.stanford.edu>
2018-06-14 12:02:41 -0700

    Fixed a logic error that could unlock memman handles too soon.

--- core/modules/wsched/ScanScheduler.cc
+++ core/modules/wsched/ScanScheduler.cc
@@ -163,8 +163,8 @@ bool ScanScheduler::_ready() {
     bool useFlexibleLock = (_inFlight < 1);
     auto rdy = _taskQueue->ready(useFlexibleLock); // Only returns true if MemMan grants resources.
     bool logMemStats = false;
-    // If ready failed, holding onto this is unlikely to help, otherwise the new Task now has its own handle.
-    if (_memManHandleToUnlock != memman::MemMan::HandleType::INVALID) {
+    // If ready failed, holding on to this is unlikely to help, otherwise the new Task now has its own handle.
+    if (!rdy && _memManHandleToUnlock != memman::MemMan::HandleType::INVALID) {
         LOGS(_log, LOG_LVL_DEBUG, "ScanScheduler::_ready unlocking handle=" << _memManHandleToUnlock);
         _memMan->unlock(_memManHandleToUnlock);
         _memManHandleToUnlock = memman::MemMan::HandleType::INVALID;
