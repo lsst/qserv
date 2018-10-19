29294b898a4bf846e988708bba1da58cbce7b075
Nathan Pease <npease@slac.stanford.edu>
2018-10-19 08:51:48 -0700

    fixup make JoinSpec ctor pass by const &

--- core/modules/query/JoinSpec.h
+++ core/modules/query/JoinSpec.h
@@ -62,14 +62,14 @@ class JoinSpec {
 public:
     typedef std::shared_ptr<JoinSpec> Ptr;
 
-    JoinSpec(std::shared_ptr<BoolTerm> onTerm)
+    JoinSpec(std::shared_ptr<BoolTerm> const& onTerm)
         : _onTerm(onTerm) {}
 
     /// FIXME: not supporting join by multiple columns now
-    JoinSpec(std::shared_ptr<ColumnRef> ref)
+    JoinSpec(std::shared_ptr<ColumnRef> const& ref)
         : _usingColumn(ref) {}
 
-    JoinSpec(std::shared_ptr<ColumnRef> ref, std::shared_ptr<BoolTerm> onTerm)
+    JoinSpec(std::shared_ptr<ColumnRef> ref, std::shared_ptr<BoolTerm> const& onTerm)
         : _usingColumn(ref), _onTerm(onTerm) {}
 
     std::shared_ptr<ColumnRef> getUsing() { return _usingColumn; }
