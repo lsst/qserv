c2c7028b167c73ace38b5652738bc249b85e8a42
Nathan Pease <npease@slac.stanford.edu>
2019-06-24 17:01:14 -0700

    fix 2-to-3 smig script
    fix versions in comment, add version number update

--- core/modules/qmeta/schema/migrate-2-to-3.sql
+++ core/modules/qmeta/schema/migrate-2-to-3.sql
@@ -1,5 +1,9 @@
 --
--- Migration script from version 1 to version 2 of QMeta database:
---   - QInfo table adds resultQuery column
+-- Migration script from version 2 to version 3 of QMeta database:
+--
 
+-- QInfo table adds resultQuery column, migration script expecets this table to exist.
 ALTER TABLE `QInfo` ADD COLUMN (`resultQuery` TEXT);
+
+-- Update record for schema version, migration script expects this record to exist.
+UPDATE `QMetadata` SET `value` = '3' WHERE `metakey` = 'version';
