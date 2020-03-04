b6005bbf130d9f2b537a40c1c8f2c36b21975bff
Igor Gaponenko <gapon@slac.stanford.edu>
2020-03-04 12:23:54 -0800

    Refactored the performance test to support threads and flow-control.

--- core/modules/wpublish/qserv-worker-perf-chunks.cc
+++ core/modules/wpublish/qserv-worker-perf-chunks.cc
@@ -1,8 +1,39 @@
+/*
+ * LSST Data Management System
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+/**
+ * This application tests the performance of the XRootD/SSI protocol
+ * using Qserv workers as servers. The application also supports
+ * the multi-threaded option for initiating requests.
+ */
+
 // System header
+#include <condition_variable>
 #include <iostream>
 #include <iomanip>
+#include <list>
+#include <mutex>
 #include <stdexcept>
 #include <string>
+#include <thread>
 #include <vector>
 
 // Third party headers
@@ -24,6 +55,7 @@ namespace util     = lsst::qserv::util;
 namespace wpublish = lsst::qserv::wpublish;
 
 using namespace std;
+using RequestT = wpublish::TestEchoQservRequest;
 
 namespace {
 
@@ -35,7 +67,90 @@ string value;
 string serviceProviderLocation;
 unsigned int numResources;
 bool resourceFirst;
-unsigned int cancelAfterMs;
+unsigned int numThreads;
+unsigned int flowControlLimit;
+bool silent;
+int xrootdCBThreadsMax;
+int xrootdCBThreadsInit;
+
+
+/**
+ * The synchronize counter, which is used to limit the number of the "in-flight"
+ * requests if the flow-control is enabled. The later is enabled if a value of
+ * the contructor's parameter 'maxRequestsAllowed' is not equal to 0.
+ */
+class Counter {
+public:
+    Counter() = delete;
+    Counter(Counter const&) = delete;
+    Counter& operator=(Counter const&) = delete;
+    ~Counter() = default;
+
+    explicit Counter(unsigned int maxRequestsAllowed)
+        :   _maxRequestsAllowed(maxRequestsAllowed) {
+    }
+
+    void inc() {
+        if (_maxRequestsAllowed == 0) {
+            ++_counter;
+        } else {
+            unique_lock<mutex> lock(_mtx);
+            _cv.wait(lock, [&]() {
+                return _counter < _maxRequestsAllowed;
+            });
+            ++_counter;
+        }
+    }
+
+    void dec() {
+        if (_maxRequestsAllowed == 0) {
+            --_counter;
+        } else {
+            unique_lock<mutex> lock(_mtx);
+            --_counter;
+            lock.unlock();
+            _cv.notify_one();
+        }
+    }
+    
+    unsigned int counter() const {
+        return _counter;
+    }
+
+private:
+    unsigned int _maxRequestsAllowed;
+    atomic<unsigned int> _counter{0};
+    std::mutex _mtx;
+    std::condition_variable _cv;
+};
+
+
+RequestT::Ptr makeRequest(
+        Counter& numRequestsInFlight,
+        XrdSsiService* serviceProvider,
+        string const& resourcePath) {
+    auto request = RequestT::create(
+        value,
+        [&numRequestsInFlight](RequestT::Status status,
+                               string const& error,
+                               string const& sent,
+                               string const& received) {
+            if (not silent) {
+                if (status != RequestT::Status::SUCCESS) {
+                    cout << "status: " << RequestT::status2str(status) << ", error: " << error << endl;
+                } else {
+                    cout << "value sent: '" << sent << "', received: '" << received << "'" << endl;
+                }
+            }
+            numRequestsInFlight.dec();
+        }
+    );
+    numRequestsInFlight.inc();
+    XrdSsiResource resource(resourcePath);
+    serviceProvider->ProcessRequest(*request, resource);
+    return request;
+}
+
 
 int test() {
 
@@ -46,11 +161,14 @@ int test() {
         return 1;
     }
 
+    // Configure threads at the XRootD/SSI client
+    XrdSsiProviderClient->SetCBThreads(xrootdCBThreadsMax, xrootdCBThreadsInit);
+
     // Connect to a service provider
     XrdSsiErrInfo errInfo;
-    auto serviceProvider =
-        XrdSsiProviderClient->GetService(errInfo,
-                                         serviceProviderLocation);
+    XrdSsiService* serviceProvider = XrdSsiProviderClient->GetService(
+        errInfo, serviceProviderLocation
+    );
     if (nullptr == serviceProvider) {
         cerr << "failed to contact service provider at: " << serviceProviderLocation
              << ", error: " << errInfo.Get() << endl;
@@ -58,89 +176,64 @@ int test() {
     }
     cout << "connected to service provider at: " << serviceProviderLocation << endl;
 
-
-    // Store request pointers here to prevent them deleted too early
-    vector<wpublish::TestEchoQservRequest::Ptr> requests;
-
-    atomic<unsigned int> finished(0);
-
+    // Build a complete list of jobs (resource path names) to be processed
+    // in the specified order. Note that the actual (run time) ordering may
+    // be different if running this test in the multi-threaded mode.
+    vector<string> jobs;
     if (resourceFirst) {
         for (unsigned int j = 0; j < numResources; ++j) {
-            string const& resourcePath = resources[j];
-
+            auto&& resourcePath = resources[j];
             for (unsigned int i = 0; i < numRequests; ++i) {
-
-                auto request = wpublish::TestEchoQservRequest::create(
-                    value,
-                    [&finished] (wpublish::TestEchoQservRequest::Status status,
-                                 string const& error,
-                                 string const& sent,
-                                 string const& received) {
-
-                        if (status != wpublish::TestEchoQservRequest::Status::SUCCESS) {
-                            cout << "status: " << wpublish::TestEchoQservRequest::status2str(status) << "\n"
-                                 << "error:  " << error << endl;
-                        } else {
-                            cout << "value sent:     " << sent     << "\n"
-                                 << "value received: " << received << endl;
-                        }
-                        finished--;
-                    });
-                requests.push_back(request);
-
-                // Submit the request
-                finished++;
-                XrdSsiResource resource(resourcePath);
-                serviceProvider->ProcessRequest(*request, resource);
+                jobs.push_back(resourcePath);
             }
         }
 
     } else {
-
         for (unsigned int i = 0; i < numRequests; ++i) {
-
             for (unsigned int j = 0; j < numResources; ++j) {
-                string const& resourcePath = resources[j];
-                auto request = wpublish::TestEchoQservRequest::create(
-                    value,
-                    [&finished] (wpublish::TestEchoQservRequest::Status status,
-                                 string const& error,
-                                 string const& sent,
-                                 string const& received) {
-
-                        if (status != wpublish::TestEchoQservRequest::Status::SUCCESS) {
-                            cout << "status: " << wpublish::TestEchoQservRequest::status2str(status) << "\n"
-                                 << "error:  " << error << endl;
-                        } else {
-                            cout << "value sent:     " << sent     << "\n"
-                                 << "value received: " << received << endl;
-                        }
-                        finished--;
-                    });
-                requests.push_back(request);
-
-                // Submit the request
-                finished++;
-                XrdSsiResource resource(resourcePath);
-                serviceProvider->ProcessRequest(*request, resource);
+                auto&& resourcePath = resources[j];
+                jobs.push_back(resourcePath);
             }
         }
     }
-    if (cancelAfterMs == 0) {
-        // Block while at least one request is in progress
-        util::BlockPost blockPost(1000, 2000);
-        while (finished) {
-            blockPost.wait(200);
-        }
-    } else {
-        // Request cancellation timeout is used to test the correctness of
-        // the XRootD/SSI implementation under heavy loads.
-        util::BlockPost blockPost(cancelAfterMs, cancelAfterMs+1);
-        blockPost.wait();
-        for (auto&& request: requests) {
-            bool const cancel = true;
-            request->Finished(cancel);
-        }
+
+    // Make allocation of jobs to threads using the 'round-robin' method
+    vector<vector<string>> thread2jobs(numThreads);
+    for (unsigned int i = 0; i < jobs.size(); ++i) {
+        auto const threadIdx = i % numThreads;
+        auto&& resourcePath = jobs[i];
+        thread2jobs[threadIdx].push_back(resourcePath);
+    }
+
+    Counter numRequestsInFlight(flowControlLimit);
+
+    // Launch threads
+    vector<unique_ptr<thread>> threads;
+    for (unsigned int tIdx = 0; tIdx < numThreads; ++tIdx) {
+
+        threads.emplace_back(new thread(
+            [&numRequestsInFlight](vector<string> const& jobs,
+                                   XrdSsiService* serviceProvider) {
+                // Launch requests
+                vector<RequestT::Ptr> requests;
+                for (auto&& resourcePath: jobs) {
+                    requests.push_back(makeRequest(numRequestsInFlight, serviceProvider, resourcePath));
+                }
+    
+                // Block while at least one request is in progress
+                util::BlockPost blockPost(1000, 2000);
+                while (numRequestsInFlight.counter() != 0) {
+                    blockPost.wait(200);
+                }
+            },
+            thread2jobs[tIdx],
+            serviceProvider
+        ));
+    }
+
+    // Wait before all threads will finish to avoid crashing the application
+    for (auto&& t: threads) {
+        t->join();
     }
     return 0;
 }
@@ -171,9 +264,13 @@ int main(int argc, const char* const argv[]) {
             "  --service=<provider>    - location of a service provider (default: 'localhost:1094')\n"
             "  --num-resources=<value> - the number of resources (default: 1, range: 1..*)\n"
             "  --resource-first        - iterate over resources, then over requests\n"
-            "  --cancel-after=<milliseconds> \n"
-            "                          - the number of milliseconds to wait before cancelling\n"
-            "                            all requests (default 0 means no cancellation)\n"
+            "  --num-threads=<value>   - the number of parallel threads (default: 1, range: 1..*)\n"
+            "  --flow-control=<limit>  - if the value is not 0 then it will turn on the flow control\n"
+            "                            for the requests processing. In this case a value of the parameter\n"
+            "                            puts a cap on the maximum number of requests being processed at each\n"
+            "                            moment of time (default: 0, range: 0..*)\n"
+            "  --silent                - do not report any status or error messages, including\n"
+            "                            the ones sent via the LSST Logger API.\n"
             "\n"
             "Parameters:\n"
             "  <resources-file-name>  - a file with resource paths (one resource per line)\n"
@@ -187,7 +284,11 @@ int main(int argc, const char* const argv[]) {
         ::serviceProviderLocation = parser.option<string>("service", "localhost:1094");
         ::numResources            = parser.option<unsigned int>("num-resources", 1);
         ::resourceFirst           = parser.flag("resource-first");
-        ::cancelAfterMs           = parser.option<unsigned int>("cancel-after", 0);
+        ::numThreads              = parser.option<unsigned int>("num-threads", 1);
+        ::flowControlLimit        = parser.option<unsigned int>("flow-control", 0);
+        ::silent                  = parser.flag("silent");
+        ::xrootdCBThreadsMax      = parser.option<int>("xrootd-cdb-threads-max", 0);
+        ::xrootdCBThreadsInit     = parser.option<int>("xrootd-cdb-threads-init", 0);
 
     } catch (exception const& ex) {
         return 1;
