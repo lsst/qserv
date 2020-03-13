f9f093be139aa5e830371f6a99e9ee739ae5794d
Igor Gaponenko <gapon@slac.stanford.edu>
2020-03-13 01:19:26 -0700

    Extended the test application to study the performance (I/O bandwidth, etc.) of the XRootD/SSI protocol.

--- core/modules/wpublish/qserv-worker-perf-chunks.cc
+++ core/modules/wpublish/qserv-worker-perf-chunks.cc
@@ -27,14 +27,17 @@
 
 // System header
 #include <condition_variable>
+#include <functional>
 #include <iostream>
 #include <iomanip>
-#include <list>
+#include <map>
+#include <memory>
 #include <mutex>
 #include <stdexcept>
 #include <string>
 #include <thread>
 #include <vector>
+#include <utility>
 
 // Third party headers
 #include "XrdSsi/XrdSsiProvider.hh"
@@ -55,6 +58,8 @@ namespace util     = lsst::qserv::util;
 namespace wpublish = lsst::qserv::wpublish;
 
 using namespace std;
+using namespace std::placeholders;
+
 using RequestT = wpublish::TestEchoQservRequest;
 
 namespace {
@@ -63,7 +68,8 @@ namespace {
 
 string fileName;
 unsigned int numRequests;
-string value;
+char value;
+unsigned int valueSize;
 string serviceProviderLocation;
 unsigned int numResources;
 bool resourceFirst;
@@ -72,23 +78,26 @@ unsigned int flowControlLimit;
 bool silent;
 int xrootdCBThreadsMax;
 int xrootdCBThreadsInit;
+bool memoryCleanup;
 
 
 /**
  * The synchronize counter, which is used to limit the number of the "in-flight"
  * requests if the flow-control is enabled. The later is enabled if a value of
- * the contructor's parameter 'maxRequestsAllowed' is not equal to 0.
+ * the constructor's parameter 'maxRequestsAllowed' is not equal to 0.
  */
 class Counter {
 public:
+    typedef shared_ptr<Counter> Ptr;
+
+    static Ptr create(unsigned int maxRequestsAllowed) {
+        return Ptr(new Counter(maxRequestsAllowed));
+    }
+
     Counter() = delete;
     Counter(Counter const&) = delete;
     Counter& operator=(Counter const&) = delete;
     ~Counter() = default;
-
-    explicit Counter(unsigned int maxRequestsAllowed)
-        :   _maxRequestsAllowed(maxRequestsAllowed) {
-    }
 
     void inc() {
         if (_maxRequestsAllowed == 0) {
@@ -118,6 +127,10 @@ public:
     }
 
 private:
+    explicit Counter(unsigned int maxRequestsAllowed)
+        :   _maxRequestsAllowed(maxRequestsAllowed) {
+    }
+
     unsigned int _maxRequestsAllowed;
     atomic<unsigned int> _counter{0};
     std::mutex _mtx;
@@ -125,34 +138,72 @@ private:
 };
 
 
-RequestT::Ptr makeRequest(
-        Counter& numRequestsInFlight,
-        XrdSsiService* serviceProvider,
-        string const& resourcePath) {
-    auto request = RequestT::create(
-        value,
-        [&numRequestsInFlight](RequestT::Status status,
-                               string const& error,
-                               string const& sent,
-                               string const& received) {
-            if (not silent) {
-                if (status != RequestT::Status::SUCCESS) {
-                    cout << "status: " << RequestT::status2str(status) << ", error: " << error << endl;
-                } else {
-                    cout << "value sent: '" << sent << "', received: '" << received << "'" << endl;
-                }
-            }
-            numRequestsInFlight.dec();
-        }
-    );
-    numRequestsInFlight.inc();
-    XrdSsiResource resource(resourcePath);
-    serviceProvider->ProcessRequest(*request, resource);
-    return request;
-}
+class RequestManager: public enable_shared_from_this<RequestManager> {
+public:
+    typedef shared_ptr<RequestManager> Ptr;
+    typedef function<void(RequestT::Status, string const&, string const&, string const&)> OnFinish;
+
+    static Ptr create(Counter::Ptr const& numRequestsInFlight,
+                      XrdSsiService* serviceProvider,
+                      string const& resourcePath,
+                      string const& payload,
+                      OnFinish const& onFinish,
+                      bool memoryCleanup) {
+        return Ptr(new RequestManager(
+            numRequestsInFlight, serviceProvider, resourcePath, payload, onFinish, memoryCleanup
+        ));
+    }
+
+    RequestManager(RequestManager const&) = delete;
+    RequestManager& operator=(RequestManager const&) = delete;
+
+    void start() {
+        _ptr = RequestT::create(
+            _payload,
+            bind(&RequestManager::_finished, shared_from_this(), _1, _2, _3, _4)
+        );
+        _numRequestsInFlight->inc();
+        XrdSsiResource resource(_resourcePath);
+        _serviceProvider->ProcessRequest(*_ptr, resource);
+    }
+    
+private:
+    void _finished(RequestT::Status status,
+                   string const& error,
+                   string const& sent,
+                   string const& received) {
+        _onFinish(status, error, sent, received);
+        _numRequestsInFlight->dec();
+        if (_memoryCleanup) _ptr = nullptr;
+    }
+
+    RequestManager(Counter::Ptr const& numRequestsInFlight,
+                   XrdSsiService* serviceProvider,
+                   string const& resourcePath,
+                   string const& payload,
+                   OnFinish const& onFinish,
+                   bool memoryCleanup)
+        :   _numRequestsInFlight(numRequestsInFlight),
+            _serviceProvider(serviceProvider),
+            _resourcePath(resourcePath),
+            _payload(payload),
+            _onFinish(onFinish),
+            _memoryCleanup(memoryCleanup) {
+    }
+
+    Counter::Ptr const _numRequestsInFlight;
+    XrdSsiService* _serviceProvider;
+    string const _resourcePath;
+    string const _payload;
+    OnFinish const _onFinish;
+    bool const _memoryCleanup;
+    RequestT::Ptr _ptr;
+};
 
 
 int test() {
+
+    string const payload = string(valueSize, value);
 
     vector<string> const resources = util::File::getLines(fileName, true);
     if (not numResources or (resources.size() < numResources)) {
@@ -205,24 +256,48 @@ int test() {
         thread2jobs[threadIdx].push_back(resourcePath);
     }
 
-    Counter numRequestsInFlight(flowControlLimit);
+    Counter::Ptr const numRequestsInFlight = Counter::create(flowControlLimit);
+    bool memoryCleanup = ::memoryCleanup;
 
     // Launch threads
     vector<unique_ptr<thread>> threads;
     for (unsigned int tIdx = 0; tIdx < numThreads; ++tIdx) {
 
         threads.emplace_back(new thread(
-            [&numRequestsInFlight](vector<string> const& jobs,
-                                   XrdSsiService* serviceProvider) {
-                // Launch requests
-                vector<RequestT::Ptr> requests;
+            [&numRequestsInFlight,&payload,memoryCleanup](
+                    vector<string> const& jobs,
+                    XrdSsiService* serviceProvider) {
+
+                // Launch requests and keep them in the collection until they finish
+                // to prevent request being deleted while there are unhandled callbacks
+                // on request completion.
+                vector<RequestManager::Ptr> requests;
+
                 for (auto&& resourcePath: jobs) {
-                    requests.push_back(makeRequest(numRequestsInFlight, serviceProvider, resourcePath));
+                    auto const ptr = RequestManager::create(
+                        numRequestsInFlight,
+                        serviceProvider,
+                        resourcePath,
+                        payload,
+                        [](RequestT::Status status, string const& error, string const& sent,
+                           string const& received) {
+                            if (not silent) {
+                                if (status != RequestT::Status::SUCCESS) {
+                                    cout << "status: " << RequestT::status2str(status) << ", error: " << error << endl;
+                                } else {
+                                    cout << "value sent: '" << sent << "', received: '" << received << "'" << endl;
+                                }
+                            }
+                        },
+                        memoryCleanup
+                    );
+                    requests.push_back(ptr);
+                    ptr->start();
                 }
     
                 // Block while at least one request is in progress
                 util::BlockPost blockPost(1000, 2000);
-                while (numRequestsInFlight.counter() != 0) {
+                while (numRequestsInFlight->counter() != 0) {
                     blockPost.wait(200);
                 }
             },
@@ -253,44 +328,52 @@ int main(int argc, const char* const argv[]) {
             argc,
             argv,
             "\n"
-            "Usage:\n"
-            "  <resources-file-name> <num-requests> <value>\n"
-            "  [--service=<provider>]\n"
-            "  [--num-resources=<value>]\n"
-            "  [--resources-first]\b"
-            "  [--cancel-after=<milliseconds>]\n"
-            "\n"
-            "Flags an options:\n"
-            "  --service=<provider>    - location of a service provider (default: 'localhost:1094')\n"
-            "  --num-resources=<value> - the number of resources (default: 1, range: 1..*)\n"
-            "  --resource-first        - iterate over resources, then over requests\n"
-            "  --num-threads=<value>   - the number of parallel threads (default: 1, range: 1..*)\n"
-            "  --flow-control=<limit>  - if the value is not 0 then it will turn on the flow control\n"
-            "                            for the requests processing. In this case a value of the parameter\n"
-            "                            puts a cap on the maximum number of requests being processed at each\n"
-            "                            moment of time (default: 0, range: 0..*)\n"
-            "  --silent                - do not report any status or error messages, including\n"
-            "                            the ones sent via the LSST Logger API.\n"
+            "General syntax:\n"
+            "  <resources-file-name> <num-requests> [flags, options...]\n"
             "\n"
             "Parameters:\n"
-            "  <resources-file-name>  - a file with resource paths (one resource per line)\n"
-            "  <num-requests>         - number of requests per resource\n"
-            "  <value>                - arbitrary string\n");
+            "  <resources-file-name>     - A file with resource paths (one resource per line).\n"
+            "  <num-requests>            - The number of requests per resource.\n"
+            "\n"
+            "Flags and options:\n"
+            "  --service=<provider>      - A location of a service provider (default: 'localhost:1094').\n"
+            "  --num-resources=<value>   - The number of resources (default: 1, range: 1..*).\n"
+            "  --resource-first          - Iterate over resources, then over requests.\n"
+            "  --num-threads=<value>     - The number of parallel threads (default: 1, range: 1..*).\n"
+            "  --value=<value>           - An arbitrary string to be used in composing messages sent\n"
+            "                              to the services (default: 'a').\n"
+            "  --value-size=<value>      - The number of times to replicate the <value> when composing\n"
+            "                              payloads of requests sent to the worker services (default: 1).\n"
+            "  --flow-control=<limit>    - If the value is not 0 then it will turn on the flow control\n"
+            "                              for the requests processing. In this case a value of the parameter\n"
+            "                              puts a cap on the maximum number of requests being processed at each\n"
+            "                              moment of time (default: 0, range: 0..*).\n"
+            "  --silent                  - Do not report any status or error messages, including\n"
+            "                              the ones sent via the LSST Logger API.\n"
+            "  --xrootd-cdb-threads-max  - The configuration parameter of the XRootD/SSI.\n"
+            "  --xrootd-cdb-threads-init - The configuration parameter of the XRootD/SSI.\n"
+            "  --memory-cleanup          - Automatically delete XRootD/SSI requests as they finish\n"
+            "                              in order to clean the memory allocated by the requests.\n");
 
         ::fileName    = parser.parameter<string>(1);
         ::numRequests = parser.parameter<unsigned int>(2);
-        ::value       = parser.parameter<string>(3);
 
         ::serviceProviderLocation = parser.option<string>("service", "localhost:1094");
         ::numResources            = parser.option<unsigned int>("num-resources", 1);
         ::resourceFirst           = parser.flag("resource-first");
         ::numThreads              = parser.option<unsigned int>("num-threads", 1);
+        ::value                   = parser.option<char>("value", 'a');
+        ::valueSize               = parser.option<unsigned int>("value-size", 1);
         ::flowControlLimit        = parser.option<unsigned int>("flow-control", 0);
         ::silent                  = parser.flag("silent");
         ::xrootdCBThreadsMax      = parser.option<int>("xrootd-cdb-threads-max", 0);
         ::xrootdCBThreadsInit     = parser.option<int>("xrootd-cdb-threads-init", 0);
+        ::memoryCleanup           = parser.flag("memory-cleanup");
+
+        if (::valueSize == 0) throw invalid_argument("a value of option <value-size> can't be 0");
 
     } catch (exception const& ex) {
+        cerr << "error: " << ex.what() << endl;
         return 1;
     }
     return ::test();
