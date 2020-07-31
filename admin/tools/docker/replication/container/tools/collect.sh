2d0779319616bb2ded01c90ae0d6399c7d1a0e67
Igor Gaponenko <gapon@slac.stanford.edu>
2020-07-30 20:13:27 -0700

    Updated build scripts for the Replication tools container

--- admin/tools/docker/replication/container/tools/collect.sh
+++ admin/tools/docker/replication/container/tools/collect.sh
@@ -8,20 +8,22 @@ if [ -z "$SOURCE" ]; then
     exit 1 
 fi
 
-source /stack/loadLSST.bash
+source /qserv/stack/loadLSST.bash
 setup -t qserv-dev qserv_distrib
 
 cd $SOURCE
-setup -t qserv-dev -r .
+setup -r .
 
 build=tmp/replication/container/build
 
 mkdir -p "${build}/lib" && rm -rf "${build}/lib/*"
 mkdir -p "${build}/bin" && rm -rf "${build}/bin/*"
 
-bins="$(ls bin/qserv-replica-* bin/qserv-worker-* $PARTITION_DIR/bin/sph-*)"
+bins="$(ls bin/qserv-replica-* bin/qserv-worker-*)"
 cp ${bins} "${build}/bin/"
 
-libs="$(for l in $(ldd ${bins} | grep '=>' | grep '/' | awk '{print $3}'); do if [[ $l != /lib* && $l != /usr/* ]]; then echo $l; fi; done | sort -u)"
+libs="$(for l in $(ldd ${bins} | grep '=>' | grep ${SOURCE}/lib | awk '{print $3}'); do if [[ $l != /lib* && $l != /usr/* ]]; then echo $l; fi; done | sort -u)"
 cp ${libs} "${build}/lib/"
-cp ${CONDA_PREFIX}/lib/libjemalloc.so "${build}/lib/"
+
+cp admin/tools/docker/replication/container/tools/lsst "${build}/bin/"
+chmod +x "${build}/bin/lsst"
