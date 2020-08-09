387f0a667d36a00f768e25f63625b7910228363b
Andy Salnikov <salnikov@slac.stanford.edu>
2020-08-08 20:22:20 -0500

    Add slurm support

--- admin/python/lsst/qserv/testing/main.py
+++ admin/python/lsst/qserv/testing/main.py
@@ -5,8 +5,9 @@ setup process to define an entry point).
 """
 
 import argparse
-import logging
 import functools
+import logging
+import os
 
 import MySQLdb
 
@@ -18,9 +19,12 @@ from .runner_mgr import RunnerManager
 _LOG = logging.getLogger(__name__)
 
 
-def _logConfig(level):
+def _logConfig(level, slot):
     levels = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
-    simple_format = "%(asctime)s %(levelname)7s %(name)s -- %(message)s"
+    if slot is None:
+        simple_format = "%(asctime)s %(levelname)7s %(name)s -- %(message)s"
+    else:
+        simple_format = f"%(asctime)s %(levelname)7s %(name)s [slot:{slot}] -- %(message)s"
     level = levels.get(level, logging.DEBUG)
     logging.basicConfig(format=simple_format, level=level)
 
@@ -54,6 +58,8 @@ def main():
                         " --num-slots and --slot must be specified together.")
     agroup.add_argument("-t", "--time-limit", type=int, default=None, metavar="SECONDS",
                         help="Run for maximum number of seconds.")
+    agroup.add_argument("--slurm", action="store_true", default=False,
+                        help="Obtain slot information from slurm.")
 
     agroup = parser.add_argument_group("Monitoring options")
     agroup.add_argument("-m", "--monitor", choices=["log", "influxdb-file"],
@@ -61,10 +67,13 @@ def main():
     agroup.add_argument("-r", "--monitor-rollover", type=int, default=3600, metavar="SECONDS",
                         help="Number of seconds between rollovers for influxdb-file monitor,"
                         " default: %(default)s")
-    agroup.add_argument("--influxdb-file-name", default="qserv-kraken-mon-%T.dat", metavar="PATH",
-                        help="File name for influxdb-file monitor, default: %(default)s.")
+    agroup.add_argument("--influxdb-file-name", default="qserv-kraken-mon-%S-%T.dat", metavar="PATH",
+                        help="File name template for influxdb-file monitor, default: %(default)s.")
     agroup.add_argument("--influxdb-db", default="qserv_kraken", metavar="DATABASE",
                         help="InfluxDB database name, default: %(default)s.")
+
+    parser.add_argument("--dump-config", action="store_true", default=False,
+                        help="Dump resulting configuration.")
 
     parser.add_argument("config", nargs="+", type=argparse.FileType(),
                         help="Configuration file name, at least one is required.")
@@ -73,14 +82,30 @@ def main():
     if (args.num_slots, args.slot).count(None) == 1:
         parser.error("options --num-slots and --slot must be specified together")
 
-    _logConfig(args.verbose)
+
+    if args.slurm:
+        if args.num_slots is not None or args.slot is not None:
+            parser.error("cannot use --slurm together with --num-slots or --slot")
+            return 2
+        num_slots = os.environ.get("SLURM_NTASKS")
+        slot = os.environ.get("SLURM_NODEID")
+        if num_slots is None or slot is None:
+            parser.error("cannot determine slurm configuration, envvar is not set")
+            return 2
+        num_slots = int(num_slots)
+        if num_slots > 1:
+            args.num_slots = num_slots
+            args.slot = int(slot)
+
+    _logConfig(args.verbose, args.slot)
 
     # build/split config
     cfg = Config.from_yaml(args.config)
     if args.num_slots is not None:
         cfg = cfg.split(args.num_slots, args.slot)
-    print("Configuration for this process (n_slots={args.num_slots}, slot={args.slot}):")
-    print(cfg.to_yaml())
+    if args.dump_config:
+        print("Configuration for this process (n_slots={args.num_slots}, slot={args.slot}):")
+        print(cfg.to_yaml())
 
     # connection factory
     if args.dummy_db:
@@ -100,13 +125,15 @@ def main():
     if args.monitor == "log":
         monitor = LogMonitor(logging.getLogger("metrics"))
     elif args.monitor == "influxdb-file":
+        slot = "" if args.slot is None else str(args.slot)
+        fname = args.influxdb_file_name.replace("%S", slot)
         monitor = InfluxDBFileMonitor(
-            args.influxdb_file_name,
+            fname,
             periodSec=args.monitor_rollover,
             dbname=args.influxdb_db
         )
     # add a tag for slot number
-    if args.slot is not None:
+    if monitor is not None and args.slot is not None:
         monitor = AddTagsMonitor(monitor, tags={"slot": args.slot})
 
     mgr = RunnerManager(cfg, connFactory, args.slot,
