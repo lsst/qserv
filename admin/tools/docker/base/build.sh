7d8281154a3ea7a5aede6fb6410144a36dd0da53
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-04-06 08:39:24 +0200

    Add support for qserv-operator and k8s
    - Install mariadb client in Qserv base image
    - Add example build script for lite-qserv images
    - Set python cmd to python3 in lite-run-base image

---
+++ admin/tools/docker/base/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DIR="$(cd "$(dirname "$0")"; pwd -P)"
+
+TAG="$(git -C "$DIR" describe --dirty --always)"
+
+docker build  --target=lite-build --tag=qserv/lite-build:$TAG "$DIR"
+docker build  --tag=qserv/lite-run-base:$TAG "$DIR"
---
+++ admin/tools/docker/ci/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DIR="$(cd "$(dirname "$0")"; pwd -P)"
+
+TAG="$(git -C "$DIR" describe --dirty --always)"
+BASE_IMG_TAG="$TAG"
+IMG="qserv/lite-qserv:$TAG"
+docker build --build-arg TAG="$BASE_IMG_TAG" --tag="$IMG" "$DIR"
