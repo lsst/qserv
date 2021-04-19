a1d0cc279fd55a5fc1c467015c94ee3858aef84e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-04-19 19:23:26 +0200

    upd

--- admin/tools/docker/base/build.sh
+++ admin/tools/docker/base/build.sh
@@ -6,5 +6,5 @@ DIR="$(cd "$(dirname "$0")"; pwd -P)"
 
 TAG="$(git -C "$DIR" describe --dirty --always)"
 
-docker build  --target=lite-build --tag=qserv/lite-build:$TAG .
-docker build  --tag=qserv/lite-run-base:$TAG .
+docker build  --target=lite-build --tag=qserv/lite-build:$TAG "$DIR"
+docker build  --tag=qserv/lite-run-base:$TAG "$DIR"
--- admin/tools/docker/ci/build.sh
+++ admin/tools/docker/ci/build.sh
@@ -7,4 +7,4 @@ DIR="$(cd "$(dirname "$0")"; pwd -P)"
 TAG="$(git -C "$DIR" describe --dirty --always)"
 BASE_IMG_TAG="$TAG"
 IMG="qserv/lite-qserv:$TAG"
-docker build --build-arg TAG="$BASE_IMG_TAG" --tag="$IMG" .
+docker build --build-arg TAG="$BASE_IMG_TAG" --tag="$IMG" "$DIR"
