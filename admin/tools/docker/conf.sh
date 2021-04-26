85baa99bba5975f344b52e30df11a4c47b312590
Fritz Mueller <fritzm@slac.stanford.edu>
2021-04-26 12:00:53 -0700

    Update to deps_20210426_1747

--- admin/tools/docker/conf.sh
+++ admin/tools/docker/conf.sh
@@ -4,7 +4,7 @@ DEPS_TAG_PREFIX="deps"
 
 # Default version of Qserv dependencies image
 # Useful for travis-ci and local build
-DEPS_TAG_DEFAULT="deps_20210421_0122"
+DEPS_TAG_DEFAULT="deps_20210426_1747"
 
 # This file contains tag of latest locally built image
 # useful for lsst-dm-ci
