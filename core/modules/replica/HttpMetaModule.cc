1eb335b845317a5ff5cf44df2a04c13aec2fffdf
Igor Gaponenko <gapon@slac.stanford.edu>
2021-07-25 04:19:30 +0000

    Incremented version of the Replication/Ingest system's API

--- core/modules/replica/HttpMetaModule.cc
+++ core/modules/replica/HttpMetaModule.cc
@@ -35,7 +35,7 @@ namespace lsst {
 namespace qserv {
 namespace replica {
 
-unsigned int const HttpMetaModule::version = 3;
+unsigned int const HttpMetaModule::version = 4;
 
 
 void HttpMetaModule::process(Controller::Ptr const& controller,
