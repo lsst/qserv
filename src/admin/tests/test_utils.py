092554a711af19c5aff2c94ac9e95c2fe9193f72
Nathan Pease <npease@slac.stanford.edu>
2021-11-01 17:52:13 -0500

    fix boilerplate

--- src/admin/tests/test_utils.py
+++ src/admin/tests/test_utils.py
@@ -1,4 +1,4 @@
-# This file is part of daf_butler.
+# This file is part of qserv.
 #
 # Developed for the LSST Data Management System.
 # This product includes software developed by the LSST Project
