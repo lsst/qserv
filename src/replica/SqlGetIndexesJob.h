49b50507b293f49cb1961edbdf89087d967075c6
Igor Gaponenko <gapon@slac.stanford.edu>
2022-11-11 20:25:32 +0000

    Extended the index retrieval job to report index status in a specialized object
    The previous version of the class SqlGetIndexesJob didn't have a convenient
    representation for the statuses of indexes found at worker tables. The new
    version adds an adaptor that translates the standard result object
    of the base class SqlJob into a special representation. The new
    representation also has the JSON interface to simplify using the result
    by the REST services, for storing it in the database, or caching in memory.

--- src/replica/SqlGetIndexesJob.h
+++ src/replica/SqlGetIndexesJob.h
@@ -25,16 +25,93 @@
 #include <cstdint>
 #include <functional>
 #include <list>
-#include <set>
+#include <map>
 #include <string>
 #include <tuple>
+#include <vector>
 
 // Qserv headers
 #include "replica/Common.h"
 #include "replica/SqlJob.h"
 
+// Third party headers
+#include "nlohmann/json.hpp"
+
 // This header declarations
 namespace lsst::qserv::replica {
+
+/**
+ * Class SqlIndexes represents postprocessed results of the index retrieval jobs (class SqlGetIndexesJob).
+ * The object could be produced only in case of the successful completion of the jobs.
+ */
+class SqlIndexes {
+public:
+    // Parameters defining a context of the job.
+    std::string database;
+    std::string table;
+    bool overlap;
+
+    /**
+     * The class Column describes columns of an index.
+     */
+    class Column {
+    public:
+        std::string name;          ///< the column name
+        unsigned int seq = 0;      ///< the column sequence number in the index, starting with 1
+        unsigned int subPart = 0;  ///< the number of indexed characters if partially indexed, or 0 otherwise
+
+        /// The enum Collation describes how the column is sorted in the index.
+        enum class Collation : int {
+            ASC = 0,    ///< for ascending order
+            DESC,       ///< for descending order
+            NOT_SORTED  ///< for undetermined order
+        };
+        Collation collation = Collation::NOT_SORTED;  ///< the collation of the columns
+
+        static std::string collation2string(Collation collation);
+    };
+
+    /**
+     * The class Index describes the index itself.
+     */
+    class Index {
+    public:
+        // The index definition
+        std::string name;           ///< the key name
+        bool unique = false;        ///< index uniqueness
+        std::string type;           ///< index type
+        std::string comment;        ///< the optional comment
+        std::list<Column> columns;  ///< definitions for contributing columns
+
+        /// Compare index definitions of the current and other index.
+        /// @note The 'comment' attribute of the definition will be excluded from the comparison.
+        /// @param other Other index to compare the current one with.
+        /// @return 'true' if the definitions are the same.
+        bool equalIndexDef(Index const& other) const;
+
+        /// The enum Status describes the status of the index
+        enum class Status : int {
+            COMPLETE = 0,  ///< all replicas of chunks/tables have the same index
+            INCOMPLETE,    ///< the index is missing in some replicas
+            INCONSISTENT   ///< index definition is different at some replicas
+        };
+        Status status = Status::INCONSISTENT;  ///< the status of the index
+
+        static std::string status2string(Status status);
+
+        size_t numReplicasTotal = 0;  ///< the total number of replicas probed by the job
+        size_t numReplicas = 0;       ///< the number of replicas where the index was found
+    };
+    std::list<Index> indexes;  ///< indexes defined on the table
+
+    /// @return  JSON representation of the object
+    nlohmann::json toJson() const;
+};
+
+inline bool operator==(SqlIndexes::Column const& lhs, SqlIndexes::Column const& rhs) {
+    return (lhs.name == rhs.name) && (lhs.seq == rhs.seq) && (lhs.subPart == rhs.subPart) &&
+           (lhs.collation == rhs.collation);
+}
 
 /**
  * Class SqlGetIndexesJob represents a tool which will broadcast batches of
@@ -96,6 +173,15 @@ public:
 
     std::list<std::pair<std::string, std::string>> extendedPersistentState() const final;
 
+    /**
+     * Return info on the indexes.
+     * @note This method is meant to be used only for succesfully completed jobs.
+     * @return definitions of indexes
+     * @throw std::logic_error If the method is called wguile the job is still running,
+     *  or if it's failed.
+     */
+    SqlIndexes indexes() const;
+
 protected:
     void notify(util::Lock const& lock) final;
 
@@ -117,10 +203,11 @@ private:
 
     CallbackType _onFinish;  /// @note is reset when the job finishes
 
-    /// A registry of workers to mark those for which request has been sent.
+    /// A collection of tables to be processed by workers. The collection
+    /// is also needed to mark those for which request has been sent.
     /// The registry prevents duplicate requests because exactly one
     /// such request is permitted to be sent to each worker.
-    std::set<std::string> _workers;
+    std::map<std::string, std::vector<std::string>> _workers2tables;
 };
 
 }  // namespace lsst::qserv::replica
