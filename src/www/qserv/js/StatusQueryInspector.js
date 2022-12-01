4cda38273114a17f992cc06fe9693349c15e2e5e
Igor Gaponenko <gapon@slac.stanford.edu>
2022-12-01 05:10:29 +0000

    Web Dashboard: expanded implementation of the query info page
    The new version of the page shows intermediate queries
    srored in the query metadata table QInfo.  A few bugs have been
    fixed. Added support for toggling query view (compact vs expanded.
    and copying quwery text to the clipboard.)

--- src/www/qserv/js/StatusQueryInspector.js
+++ src/www/qserv/js/StatusQueryInspector.js
@@ -3,15 +3,13 @@ define([
     'webfwk/Fwk',
     'webfwk/FwkApplication',
     'qserv/Common',
-    'underscore',
-    'modules/sql-formatter.min'],
+    'underscore'],
 
 function(CSSLoader,
          Fwk,
          FwkApplication,
          Common,
-         _,
-         sqlFormatter) {
+         _) {
 
     CSSLoader.load('qserv/css/StatusQueryInspector.css');
 
@@ -22,6 +20,12 @@ function(CSSLoader,
 
         constructor(name) {
             super(name);
+            // The last query info is cached here as it's needed for handling events on
+            // toggling query views.
+            this._info = undefined;
+            // Store 'true' to allow persistent state of the query display for
+            // the expanded queries between updates.
+            this._expanded = {'query': false, 'qTemplate': false, 'qMerge': false, 'resultQuery': false};
         }
 
         /**
@@ -75,6 +79,8 @@ function(CSSLoader,
             }
             if (this._initialized) return;
             this._initialized = true;
+            const queryCopyTitle = "Click to copy the query text to the clipboard.";
+            const queryToggleTitle = "Click to toggle query formatting.";
             let html = `
 <div class="form-row" id="fwk-status-query-controls">
   <div class="form-group col-md-1">
@@ -101,29 +107,18 @@ function(CSSLoader,
 <div class="row" id="fwk-status-query-info">
   <div class="col col-md-2">
     <table class="table table-sm table-hover">
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr>
+      </thead>
       <tbody>
         <tr>
           <th style="text-align:left" scope="row">Status</th>
           <td style="text-align:left" id="status"><pre></pre></td>
         </tr>
-        <tr>
-          <th style="text-align:left" scope="row">Chunks</th>
-          <td style="text-align:left"><pre id="chunkCount"></pre></td>
-        </tr>
-        <tr>
-          <th style="text-align:left" scope="row">Result Rows</th>
-          <td style="text-align:left"><pre id="resultRows"></pre></td>
-        </tr>
-        <tr>
-          <th style="text-align:left" scope="row">Result Bytes</th>
-          <td style="text-align:left"><pre id="resultBytes"></pre></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <div class="col col-md-3">
-    <table class="table table-sm table-hover">
-      <tbody>
         <tr>
           <th style="text-align:left" scope="row">Type</th>
           <td style="text-align:left"><pre id="qType"></pre></td>
@@ -136,15 +131,78 @@ function(CSSLoader,
           <th style="text-align:left" scope="row">Completed</th>
           <td style="text-align:left"><pre id="completed"></pre></td>
         </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="col col-md-2">
+    <table class="table table-sm table-hover">
+      <thead>
         <tr>
-          <th style="text-align:left" scope="row">Returned</th>
-          <td style="text-align:left"><pre id="returned"></pre></td>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th style="text-align:left" scope="row">Chunks</th>
+          <td style="text-align:left"><pre id="chunkCount"></pre></td>
+        </tr>
+        <tr>
+          <th style="text-align:left" scope="row">Collected Bytes</th>
+          <td style="text-align:left"><pre id="collectedBytes"></pre></td>
+        </tr>
+        <tr>
+          <th style="text-align:left" scope="row">Collected Rows</th>
+          <td style="text-align:left"><pre id="collectedRows"></pre></td>
+        </tr>
+        <tr>
+          <th style="text-align:left" scope="row">Final Rows</th>
+          <td style="text-align:left"><pre id="finalRows"></pre></td>
         </tr>
       </tbody>
     </table>
   </div>
-  <div class="col col-md-7">
-    <textarea style="" disabled id="query"></textarea>
+  <div class="col col-md-8">
+    <table class="table table-sm table-hover">
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          <th><i class="bi bi-clipboard-fill"></i></th>
+          <th>&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th style="text-align:left" scope="row">User</th>
+          <td style="text-align:center; padding-top:0; padding-bottom:0">
+            <button class="btn btn-outline-dark btn-sm copy-query" title="${queryCopyTitle}" target="query"></button>
+          </td>
+          <td style="text-align:left"><pre class="query_toggler" title="${queryToggleTitle}" id="query"></pre></td>
+        </tr>
+        <tr>
+          <th style="text-align:left" scope="row">Template</th>
+          <td style="text-align:center; padding-top:0; padding-bottom:0">
+            <button class="btn btn-outline-dark btn-sm copy-query" title="${queryCopyTitle}" target="qTemplate"></button>
+          </td>
+          <td style="text-align:left"><pre class="query_toggler" title="${queryToggleTitle}" id="qTemplate"></pre></td>
+        </tr>
+        <tr>
+          <th style="text-align:left" scope="row">Merge</th>
+          <td style="text-align:center; padding-top:0; padding-bottom:0">
+            <button class="btn btn-outline-dark btn-sm copy-query" title="${queryCopyTitle}" target="qMerge"></button>
+          </td>
+          <td style="text-align:left"><pre class="query_toggler" title="${queryToggleTitle}" id="qMerge"></pre></td>
+        </tr>
+        <tr>
+          <th style="text-align:left" scope="row">Result</th>
+          <td style="text-align:center; padding-top:0; padding-bottom:0">
+            <button class="btn btn-outline-dark btn-sm copy-query" title="${queryCopyTitle}" target="resultQuery"></button>
+          </td>
+          <td style="text-align:left"><pre class="query_toggler" title="${queryToggleTitle}" id="resultQuery"></pre></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </div>
 <div class="row">
@@ -184,11 +242,14 @@ function(CSSLoader,
         }
         _get_query_id() { return this._form_control('input', 'query-id').val(); }
         _set_query_id(query_id) { this._form_control('input', 'query-id').val(query_id); }
-        _query_info(attr) {
+        _query_status() {
             if (this._query_info_obj === undefined) {
                 this._query_info_obj = this.fwk_app_container.find('div#fwk-status-query-info');
             }
-            return this._query_info_obj.find("#" + attr);
+            return this._query_info_obj;
+        }
+        _query_info(attr) {
+            return this._query_status().find("#" + attr);
         }
         _set_query_info_state(attr, val) {
             this._query_info(attr).html(val);
@@ -239,27 +300,35 @@ function(CSSLoader,
             this._set_query_info_state("status", "");
             this._set_query_info("qType", "");
             this._set_query_info("chunkCount", "");
-            this._set_query_info("resultRows", "");
-            this._set_query_info("resultBytes", "");
+            this._set_query_info("collectedBytes", "");
+            this._set_query_info("collectedRows", "");
+            this._set_query_info("finalRows", "");
             this._set_query_info("submitted", "");
             this._set_query_info("completed", "");
             this._set_query_info("returned",  "");
             this._set_query_info("query", "");
+            this._set_query_info("qTemplate", "");
+            this._set_query_info("qMerge", "");
+            this._set_query_info("resultQuery", "");
             this._status().html('<span style="color:maroon">' + msg + '</span>');
             this._status().removeClass('updating');
             this._loading = false;
         }
         _display(info) {
-            let sqlFormatterConfig = {"language":"mysql","uppercase:":true,"indent":"  "};
+            this._info = info;
             this._set_query_info_state("status", `<pre class="${this._status2class(info.status)}">${info.status}</pre>`);
             this._set_query_info("qType", info.qType);
             this._set_query_info("chunkCount", info.chunkCount);
-            this._set_query_info("resultRows", info.resultRows);
-            this._set_query_info("resultBytes", info.resultBytes);
+            this._set_query_info("collectedBytes", info.collectedBytes);
+            this._set_query_info("collectedRows", info.collectedRows);
+            this._set_query_info("finalRows", info.finalRows);
             this._set_query_info("submitted", info.submitted ? (new Date(info.submitted)).toLocalTimeString('iso') : "");
             this._set_query_info("completed", info.completed ? (new Date(info.completed)).toLocalTimeString('iso') : "");
             this._set_query_info("returned", info.returned ? (new Date(info.returned)).toLocalTimeString('iso') : "");
-            this._set_query_info("query", info.query.length < 80 ? info.query : sqlFormatter.format(info.query, sqlFormatterConfig));
+            this._set_query_info("query", Common.query2text(info.query, this._expanded["query"]));
+            this._set_query_info("qTemplate", Common.query2text(info.qTemplate, this._expanded["qTemplate"]));
+            this._set_query_info("qMerge", Common.query2text(info.qMerge, this._expanded["qMerge"]));
+            this._set_query_info("resultQuery", Common.query2text(info.resultQuery, this._expanded["resultQuery"]));
             let html = '';
             for (let i in info.messages) {
                 let msg = info.messages[i];
@@ -273,6 +342,33 @@ function(CSSLoader,
   <td class="left-aligned" id="${i}">Loading...</td>
 </tr>`;
             }
+
+            // Set up display toggler for all 4 query displays.
+            let that = this;
+            let toggleQueryDisplay = function(e) {
+                let pre = $(e.currentTarget);
+                let id = pre.attr("id");
+                that._expanded[id] = !that._expanded[id];
+                pre.text(Common.query2text(that._info[id], that._expanded[id]));
+            };
+            this._query_info("query").click(toggleQueryDisplay);
+            this._query_info("qTemplate").click(toggleQueryDisplay);
+            this._query_info("qMerge").click(toggleQueryDisplay);
+            this._query_info("resultQuery").click(toggleQueryDisplay);
+
+            // Set up event handler to copy queries to the clipboard
+            let copyQueryToClipboard = function(e) {
+                let button = $(e.currentTarget);
+                let target = button.attr("target");
+                let query = that._info[target];
+                navigator.clipboard.writeText(query,
+                    () => {},
+                    () => { alert("Failed to write the query to the clipboard. Please copy the text manually: " + query); }
+                );
+            };
+            this._query_status().find("button.copy-query").click(copyQueryToClipboard);
+
+            // Display MySQL warnings (if any)
             let tbody = this._table_messages().children('tbody');
             tbody.html(html);
             // Messages are set via DOM to avoid failures that may arrise during
