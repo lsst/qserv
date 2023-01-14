86525f94560563dc8bf54934276507ff065b0a0c
Igor Gaponenko <gapon@slac.stanford.edu>
2023-01-13 18:37:26 -0800

    Web Dashboard: improved implementation of the Qserv query queues page

--- src/www/qserv/js/QservWorkerQueries.js
+++ src/www/qserv/js/QservWorkerQueries.js
@@ -15,50 +15,61 @@ function(CSSLoader,
 
     class QservWorkerQueries extends FwkApplication {
 
-
-        /// @returns the default update interval for the page
-        static update_ival_sec() { return 2; }
-
         constructor(name) {
             super(name);
+            this._queryId2Expanded = {};    // Store 'true' to allow persistent state for the expanded
+                                            // queries between updates.
+            this._id2query = {};            // Store query text for each identifier. The dictionary gets
+                                            // updated at each refresh of the page.
         }
-
-        /// @see FwkApplication.fwk_app_on_show
         fwk_app_on_show() {
             console.log('show: ' + this.fwk_app_name);
             this.fwk_app_on_update();
         }
-
-        /// @see FwkApplication.fwk_app_on_hide
         fwk_app_on_hide() {
             console.log('hide: ' + this.fwk_app_name);
         }
-
-        /// @see FwkApplication.fwk_app_on_update
         fwk_app_on_update() {
             if (this.fwk_app_visible) {
+                this._init();
                 if (this._prev_update_sec === undefined) {
                     this._prev_update_sec = 0;
                 }
                 let now_sec = Fwk.now().sec;
-                if (now_sec - this._prev_update_sec > QservWorkerQueries.update_ival_sec()) {
+                if (now_sec - this._prev_update_sec > this._update_interval_sec()) {
                     this._prev_update_sec = now_sec;
                     this._init();
                     this._load();
                 }
             }
         }
-
-        /**
-         * The first time initialization of the page's layout
-         */
         _init() {
             if (this._initialized === undefined) this._initialized = false;
             if (this._initialized) return;
             this._initialized = true;
-            this._prevTimestamp = 0;
-
             let html = `
+<div class="row" id="fwk-qserv-queries-controls">
+  <div class="col">
+    <div class="form-row">
+      <div class="form-group col-md-1">
+        <label for="update-interval"><i class="bi bi-arrow-repeat"></i> interval:</label>
+        <select id="update-interval" class="form-control form-control-selector">
+          <option value="5">5 sec</option>
+          <option value="10" selected>10 sec</option>
+          <option value="20">20 sec</option>
+          <option value="30">30 sec</option>
+          <option value="60">1 min</option>
+          <option value="120">2 min</option>
+          <option value="300">5 min</option>
+        </select>
+      </div>
+      <div class="form-group col-md-1">
+        <label for="reset-queries-form">&nbsp;</label>
+        <button id="reset-queries-form" class="btn btn-primary form-control">Reset</button>
+      </div>
+    </div>
+  </div>
+</div>
 <div class="row">
   <div class="col">
     <table class="table table-sm table-hover table-bordered" id="fwk-qserv-queries">
@@ -68,6 +79,8 @@ function(CSSLoader,
           <th>scheduler</th>
           <th>#tasks</th>
           <th>qid</th>
+          <th style="text-align:center;"><i class="bi bi-clipboard-fill"></i></th>
+          <th class="sticky" style="text-align:center;"><i class="bi bi-info-circle-fill"></i></th>
           <th>query</th>
         </tr>
       </thead>
@@ -76,10 +89,28 @@ function(CSSLoader,
     </table>
   </div>
 </div>`;
-            this.fwk_app_container.html(html);
+            let cont = this.fwk_app_container.html(html);
+            cont.find(".form-control-selector").change(() => {
+                this._load();
+            });
+            cont.find("button#reset-queries-form").click(() => {
+                this._set_update_interval_sec(10);
+                this._load();
+            });
         }
-        
-        /// @returns JQuery table object displaying XRootD/SSI resources on each worker
+        _form_control(elem_type, id) {
+            if (this._form_control_obj === undefined) this._form_control_obj = {};
+            if (!_.has(this._form_control_obj, id)) {
+                this._form_control_obj[id] = this.fwk_app_container.find(elem_type + '#' + id);
+            }
+            return this._form_control_obj[id];
+        }
+        _update_interval_sec() { return this._form_control('select', 'update-interval').val(); }
+        _set_update_interval_sec(val) { this._form_control('select', 'update-interval').val(val); }
+
+        /**
+         * Table for displaying queries that are being processed/scheduled at workers.
+         */
         _table() {
             if (this._table_obj === undefined) {
                 this._table_obj = this.fwk_app_container.find('table#fwk-qserv-queries');
@@ -88,7 +119,7 @@ function(CSSLoader,
         }
 
         /**
-         * Load data from a web servie then render it to the application's page.
+         * Load data from a web service then render it to the application's page.
          */
         _load() {
             if (this._loading === undefined) this._loading = false;
@@ -114,14 +145,17 @@ function(CSSLoader,
                 }
             );
         }
-        
-        static arrayOfPairs2str(a) {
-            return _.reduce(a, function(s, pair){ return s + '<pre>' + pair[0] + ' (' + pair[1] + ')</pre> '; }, '');
 
-        }
-
+        /**
+         * Display the queries
+         */
         _display(data) {
+            const queryCopyTitle = "Click to copy the query text to the clipboard.";
+            const queryInspectTitle = "Click to see detailed info (progress, messages, etc.) on the query.";
+            const queryToggleTitle = "Click to toggle query formatting.";
+            const queryStyle = "color:#4d4dff;";
             let html = '';
+            this._id2query = {};
             for (let worker in data) {
                 if (!data[worker].success) {
                     html += `
@@ -145,13 +179,21 @@ function(CSSLoader,
                         for (let j in scheduler.query_id_to_count) {
                             numQueries++;
                             numSchedulerQueries++;
-                            let queryId  = scheduler.query_id_to_count[j][0];
-                            let numTasks = scheduler.query_id_to_count[j][1];
+                            const queryId  = scheduler.query_id_to_count[j][0];
+                            const numTasks = scheduler.query_id_to_count[j][1];
+                            this._id2query[queryId] = queries[queryId].query;
+                            const expanded = (queryId in this._queryId2Expanded) && this._queryId2Expanded[queryId];
                             htmlSchedulerQueries += `
-<tr>
+<tr id="${queryId}">
   <td><pre>${numTasks}</pre></td>
   <td><pre>${queryId}</pre></td>
-  <td><pre>${queries[queryId].query}<pre></td>
+  <td style="text-align:center; padding-top:0; padding-bottom:0">
+    <button class="btn btn-outline-dark btn-sm copy-query" style="height:20px; margin:0px;" title="${queryCopyTitle}"></button>
+  </td>
+  <td style="text-align:right; padding-top:0; padding-bottom:0">
+    <button class="btn btn-outline-info btn-sm inspect-query" style="height:20px; margin:0px;" title="${queryInspectTitle}"></button>
+  </td>
+  <td class="query_toggler" title="${queryToggleTitle}"><pre class="query" style="${queryStyle}">` + this._query2text(queryId, expanded) + `<pre></td>
 </tr>`;
                         }
                         if (numSchedulerQueries > 0) {
@@ -168,9 +210,38 @@ function(CSSLoader,
 </tr>` + htmlQueries;
                 }
             }
-            this._table().children('tbody').html(html);
+            let that = this;
+            let copyQueryToClipboard = function(e) {
+                let button = $(e.currentTarget);
+                let queryId = button.parent().parent().attr("id");
+                let query = that._id2query[queryId];
+                navigator.clipboard.writeText(query,
+                    () => {},
+                    () => { alert("Failed to write the query to the clipboard. Please copy the text manually: " + query); }
+                );
+            };
+            let displayQuery  = function(e) {
+                let button = $(e.currentTarget);
+                let queryId = button.parent().parent().attr("id");
+                Fwk.find("Status", "Query Inspector").set_query_id(queryId);
+                Fwk.show("Status", "Query Inspector");
+            };
+            let toggleQueryDisplay = function(e) {
+                let td = $(e.currentTarget);
+                let pre = td.find("pre.query");
+                const queryId = td.parent().attr("id");
+                const expanded = !((queryId in that._queryId2Expanded) && that._queryId2Expanded[queryId]);
+                pre.text(that._query2text(queryId, expanded));
+                that._queryId2Expanded[queryId] = expanded;
+            };
+            let tbodyQueries = this._table().children('tbody').html(html);
+            tbodyQueries.find("button.copy-query").click(copyQueryToClipboard);
+            tbodyQueries.find("button.inspect-query").click(displayQuery);
+            tbodyQueries.find("td.query_toggler").click(toggleQueryDisplay);
         }
-
+        _query2text(queryId, expanded) {
+            return Common.query2text(this._id2query[queryId], expanded);
+        }
     }
     return QservWorkerQueries;
 });
