ded087ad9886a6c4b1b875970bd65504b0ddfe55
John Gates <jgates@slac.stanford.edu>
2024-03-15 15:49:51 -0700

    Rebased from main.

--- src/cconfig/CzarConfig.cc
+++ src/cconfig/CzarConfig.cc
@@ -82,55 +82,6 @@ std::shared_ptr<CzarConfig> CzarConfig::instance() {
 }
 
 CzarConfig::CzarConfig(util::ConfigStore const& configStore, std::string const& czarName)
-#if 0 //&&&
-        : _czarName(czarName),
-          _mySqlResultConfig(configStore.get("resultdb.user", "qsmaster"),
-                             configStore.getRequired("resultdb.passwd"),
-                             configStore.getRequired("resultdb.host"), configStore.getInt("resultdb.port"),
-                             configStore.getRequired("resultdb.unix_socket"),
-                             configStore.get("resultdb.db", "qservResult")),
-          _maxTableSizeMB(configStore.getInt("resultdb.maxtablesize_mb", 5001)),
-          _maxSqlConnectionAttempts(configStore.getInt("resultdb.maxsqlconnectionattempts", 10)),
-          _resultEngine(configStore.get("resultdb.engine", "myisam")),
-          _resultMaxConnections(configStore.getInt("resultdb.maxconnections", 40)),
-          _resultMaxHttpConnections(configStore.getInt("resultdb.maxhttpconnections", 8192)),
-          _oldestResultKeptDays(configStore.getInt("resultdb.oldestResultKeptDays", 30)),
-          _cssConfigMap(configStore.getSectionConfigMap("css")),
-          _mySqlQmetaConfig(configStore.get("qmeta.user", "qsmaster"), configStore.get("qmeta.passwd"),
-                            configStore.get("qmeta.host"), configStore.getInt("qmeta.port", 3306),
-                            configStore.get("qmeta.unix_socket"), configStore.get("qmeta.db", "qservMeta")),
-          _mySqlQstatusDataConfig(
-                  configStore.get("qstatus.user", "qsmaster"), configStore.get("qstatus.passwd"),
-                  configStore.get("qstatus.host"), configStore.getInt("qstatus.port", 3306),
-                  configStore.get("qstatus.unix_socket"), configStore.get("qstatus.db", "qservStatusData")),
-          _xrootdFrontendUrl(configStore.get("frontend.xrootd", "localhost:1094")),
-          _interactiveChunkLimit(configStore.getInt("tuning.interactiveChunkLimit", 10)),
-          _xrootdCBThreadsMax(configStore.getInt("tuning.xrootdCBThreadsMax", 500)),
-          _xrootdCBThreadsInit(configStore.getInt("tuning.xrootdCBThreadsInit", 50)),
-          _xrootdSpread(configStore.getInt("tuning.xrootdSpread", 4)),
-          _qMetaSecsBetweenChunkCompletionUpdates(
-                  configStore.getInt("tuning.qMetaSecsBetweenChunkCompletionUpdates", 60)),
-          _maxMsgSourceStore(configStore.getInt("qmeta.maxMsgSourceStore", 3)),
-          _queryDistributionTestVer(configStore.getInt("tuning.queryDistributionTestVer", 0)),
-          _qdispPoolSize(configStore.getInt("qdisppool.poolSize", 1000)),
-          _qdispMaxPriority(configStore.getInt("qdisppool.largestPriority", 2)),
-          _qdispVectRunSizes(configStore.get("qdisppool.vectRunSizes", "50:50:50:50")),
-          _qdispVectMinRunningSizes(configStore.get("qdisppool.vectMinRunningSizes", "0:1:3:3")),
-          _notifyWorkersOnQueryFinish(configStore.getInt("tuning.notifyWorkersOnQueryFinish", 1)),
-          _notifyWorkersOnCzarRestart(configStore.getInt("tuning.notifyWorkersOnCzarRestart", 1)),
-          _czarStatsUpdateIvalSec(configStore.getInt("tuning.czarStatsUpdateIvalSec", 1)),
-          _czarStatsRetainPeriodSec(configStore.getInt("tuning.czarStatsRetainPeriodSec", 24 * 3600)),
-          _replicationInstanceId(configStore.get("replication.instance_id", "")),
-          _replicationAuthKey(configStore.get("replication.auth_key", "")),
-          _replicationAdminAuthKey(configStore.get("replication.admin_auth_key", "")),
-          _replicationRegistryHost(configStore.get("replication.registry_host", "")),
-          _replicationRegistryPort(configStore.getInt("replication.registry_port", 0)),
-          _replicationRegistryHearbeatIvalSec(
-                  configStore.getInt("replication.registry_heartbeat_ival_sec", 1)),
-          _replicationHttpPort(configStore.getInt("replication.http_port", 0)),
-          _replicationNumHttpThreads(configStore.getInt("replication.num_http_threads", 2)) {
-    if (_replicationRegistryHost.empty()) {
-#endif //&&&
         : _czarName(czarName) {
     _configValMap.readConfigStore(configStore);
 
@@ -170,64 +121,7 @@ CzarConfig::CzarConfig(util::ConfigStore const& configStore, std::string const& 
     // Note that actual collection may contain parameters not mentioned in
     // the input configuration.
     nlohmann::json& actualJsonConfig = _jsonConfig["actual"];
-#if 0 //&&&
-    actualJsonConfig["resultdb"] =
-            nlohmann::json::object({{"user", _mySqlResultConfig.username},
-                                    {"passwd", "xxxxx"},
-                                    {"host", _mySqlResultConfig.hostname},
-                                    {"port", std::to_string(_mySqlResultConfig.port)},
-                                    {"unix_socket", _mySqlResultConfig.socket},
-                                    {"db", _mySqlResultConfig.dbName},
-                                    {"maxtablesize_mb", std::to_string(_maxTableSizeMB)},
-                                    {"maxsqlconnectionattempts", std::to_string(_maxSqlConnectionAttempts)},
-                                    {"engine", _resultEngine},
-                                    {"maxconnections", std::to_string(_resultMaxConnections)},
-                                    {"maxhttpconnections", std::to_string(_resultMaxHttpConnections)},
-                                    {"oldestResultKeptDays", std::to_string(_oldestResultKeptDays)}});
-    actualJsonConfig["css"] = _cssConfigMap;
-    actualJsonConfig["qmeta"] =
-            nlohmann::json::object({{"user", _mySqlQmetaConfig.username},
-                                    {"passwd", "xxxxx"},
-                                    {"host", _mySqlQmetaConfig.hostname},
-                                    {"port", std::to_string(_mySqlQmetaConfig.port)},
-                                    {"unix_socket", _mySqlQmetaConfig.socket},
-                                    {"db", _mySqlQmetaConfig.dbName},
-                                    {"maxMsgSourceStore", std::to_string(_maxMsgSourceStore)}});
-    actualJsonConfig["qstatus"] =
-            nlohmann::json::object({{"user", _mySqlQstatusDataConfig.username},
-                                    {"passwd", "xxxxx"},
-                                    {"host", _mySqlQstatusDataConfig.hostname},
-                                    {"port", std::to_string(_mySqlQstatusDataConfig.port)},
-                                    {"unix_socket", _mySqlQstatusDataConfig.socket},
-                                    {"db", _mySqlQstatusDataConfig.dbName}});
-    actualJsonConfig["frontend"] = nlohmann::json::object({{"xrootd", _xrootdFrontendUrl}});
-    actualJsonConfig["tuning"] = nlohmann::json::object(
-            {{"interactiveChunkLimit", std::to_string(_interactiveChunkLimit)},
-             {"xrootdCBThreadsMax", std::to_string(_xrootdCBThreadsMax)},
-             {"xrootdCBThreadsInit", std::to_string(_xrootdCBThreadsInit)},
-             {"xrootdSpread", std::to_string(_xrootdSpread)},
-             {"qMetaSecsBetweenChunkCompletionUpdates",
-              std::to_string(_qMetaSecsBetweenChunkCompletionUpdates)},
-             {"queryDistributionTestVer", std::to_string(_queryDistributionTestVer)},
-             {"notifyWorkersOnQueryFinish", std::to_string(_notifyWorkersOnQueryFinish)},
-             {"notifyWorkersOnCzarRestart", std::to_string(_notifyWorkersOnCzarRestart)},
-             {"czarStatsUpdateIvalSec", std::to_string(_czarStatsUpdateIvalSec)},
-             {"czarStatsRetainPeriodSec", std::to_string(_czarStatsRetainPeriodSec)}});
-    actualJsonConfig["qdisppool"] =
-            nlohmann::json::object({{"poolSize", std::to_string(_qdispPoolSize)},
-                                    {"largestPriority", std::to_string(_qdispMaxPriority)},
-                                    {"vectRunSizes", _qdispVectRunSizes},
-                                    {"vectMinRunningSizes", _qdispVectMinRunningSizes}});
-    actualJsonConfig["replication"] = nlohmann::json::object(
-            {{"instance_id", _replicationInstanceId},
-             {"auth_key", "xxxxx"},
-             {"admin_auth_key", "xxxxx"},
-             {"registry_host", _replicationRegistryHost},
-             {"registry_port", std::to_string(_replicationRegistryPort)},
-             {"registry_heartbeat_ival_sec", std::to_string(_replicationRegistryHearbeatIvalSec)},
-             {"http_port", std::to_string(_replicationHttpPort)},
-             {"num_http_threads", std::to_string(_replicationNumHttpThreads)}});
-#endif //&&&
+
     _configValMap.populateJson(actualJsonConfig);
 
     actualJsonConfig["identity"] =
