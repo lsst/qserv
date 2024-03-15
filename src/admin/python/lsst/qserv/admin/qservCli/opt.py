59c2b66f25bfc202f2e065c9f9d4b7b115e0a30a
Igor Gaponenko <gapon@slac.stanford.edu>
2024-03-15 02:52:17 +0000

    Streamlined naming convention for the "environment values" decorators
    The new convention improves naming consistency and it's meant to improve
    readability of the code.

--- src/admin/python/lsst/qserv/admin/qservCli/opt.py
+++ src/admin/python/lsst/qserv/admin/qservCli/opt.py
@@ -251,10 +251,10 @@ class ImageName:
         tag : `str`
             The image tag
         """
-        qserv_root = qserv_root_ev.val()
+        qserv_root = envval_qserv_root.val()
         if qserv_root is None:
             raise RuntimeError("qserv root was unexpectedly None.")
-        return image_tag_ev.val_with_default(get_description(self.dockerfiles, qserv_root))
+        return envval_image_tag.val_with_default(get_description(self.dockerfiles, qserv_root))
 
     @property
     def dockerfiles(self) -> Optional[List[str]]:
@@ -279,67 +279,67 @@ class ImageName:
         raise RuntimeError(f"Invalid image type: {self.image}")
 
 
-qserv_root_ev = FlagEnvVal(
+envval_qserv_root = FlagEnvVal(
     "--qserv-root",
     "QSERV_ROOT",
     os.path.abspath(os.path.join(__file__, relative_qserv_root)),
 )
-image_tag_ev = EnvVal(env_var="QSERV_IMAGE_TAG", description="the tag of all qserv image names")
-qserv_image_ev = FlagEnvVal(
+envval_image_tag = EnvVal(env_var="QSERV_IMAGE_TAG", description="the tag of all qserv image names")
+envval_qserv_image = FlagEnvVal(
     "--qserv-image",
     "QSERV_IMAGE",
     ImageName("qserv").tagged_name,
 )
-run_base_image_ev = FlagEnvVal(
+envval_run_base_image = FlagEnvVal(
     "--run-base-image",
     "QSERV_RUN_BASE_IMAGE",
     ImageName("run-base").tagged_name,
 )
-mariadb_image_ev = FlagEnvVal(
+envval_mariadb_image = FlagEnvVal(
     "--mariadb-image",
     "QSERV_MARIADB_IMAGE",
     ImageName("mariadb").tagged_name,
 )
-build_image_ev = FlagEnvVal(
+envval_build_image = FlagEnvVal(
     "--build-image",
     "QSERV_BUILD_IMAGE",
     ImageName("build-base").tagged_name,
 )
-user_build_image_ev = FlagEnvVal(
+envval_user_build_image = FlagEnvVal(
     "--user-build-image",
     "QSERV_USER_BUILD_IMAGE",
     ImageName("build-user").tagged_name,
 )
 # qserv root default is derived by the relative path to the qserv folder from
 # the locaiton of this file.
-qserv_build_root_ev = FlagEnvVal("--qserv-build-root", "QSERV_BUILD_ROOT", "/home/{user}/code/qserv")
-project_ev = FlagEnvVal("--project", "QSERV_PROJECT", getpass.getuser())
-outdir_ev = FlagEnvVal("--outdir", "OUTDIR", "/tmp")
-dashboard_port_ev = FlagEnvVal("--dashboard-port", "QSERV_DASHBOARD_PORT", None)
-dh_user_ev = EnvVal("QSERV_DH_USER", "CI only; the dockerhub user for pushing and pulling images")
-dh_token_ev = EnvVal(
+envval_qserv_build_root = FlagEnvVal("--qserv-build-root", "QSERV_BUILD_ROOT", "/home/{user}/code/qserv")
+envval_project = FlagEnvVal("--project", "QSERV_PROJECT", getpass.getuser())
+envval_outdir = FlagEnvVal("--outdir", "OUTDIR", "/tmp")
+envval_dashboard_port = FlagEnvVal("--dashboard-port", "QSERV_DASHBOARD_PORT", None)
+envval_dh_user = EnvVal("QSERV_DH_USER", "CI only; the dockerhub user for pushing and pulling images")
+envval_dh_token = EnvVal(
     "QSERV_DH_TOKEN",
     "CI only; the dockerhub user token for pushing and pulling images",
     private=True,
 )
-ltd_user_ev = EnvVal(
+envval_ltd_user = EnvVal(
     "QSERV_LTD_USERNAME",
     "CI only; the LSST The Docs user for pushing docs."
 )
-ltd_password_ev = EnvVal(
+envval_ltd_password = EnvVal(
     "QSERV_LTD_PASSWORD",
     "CI only; the LSST The Docs password for pushing docs.",
     private=True
 )
-gh_event_name_ev = EnvVal(
+envval_gh_event_name = EnvVal(
     "QSERV_GH_EVENT_NAME",
     "CI only; The name of the event that triggered the GHA workflow."
 )
-gh_head_ref_ev = EnvVal(
+envval_gh_head_ref = EnvVal(
     "QSERV_GH_HEAD_REF",
     "CI only; The head ref or source branch of the pull request in a GHA workflow run."
 )
-gh_ref_ev = EnvVal(
+envval_gh_ref = EnvVal(
     "QSERV_GH_REF",
     "CI only; The branch or tag ref that triggered the workflow run."
 )
@@ -420,37 +420,37 @@ class OptDefault:
 itest_default = OptDefault(
     opt=["--itest-file"],
     default=None,
-    ev=qserv_root_ev,
+    ev=envval_qserv_root,
     val=lambda ev_val: os.path.join(ev_val, "src/admin/etc/integration_tests.yaml"),
 )
 itest_container_default = OptDefault(
     opt=["--itest-container"],
     default="itest",
-    ev=project_ev,
+    ev=envval_project,
     val=lambda ev_val: f"{ev_val}_itest",
 )
 itest_ref_container_default = OptDefault(
     opt=["--itest-ref-container"],
     default="itest_ref",
-    ev=project_ev,
+    ev=envval_project,
     val=lambda ev_val: f"{ev_val}_itest_ref",
 )
 test_container_default = OptDefault(
     opt=["--test-container"],
     default="test",
-    ev=project_ev,
+    ev=envval_project,
     val=lambda ev_val: f"{ev_val}_test",
 )
 compose_file_default = OptDefault(
     opt=["--file", "yaml_file"],
     default=None,
-    ev=qserv_root_ev,
+    ev=envval_qserv_root,
     val=lambda ev_val: os.path.join(ev_val, "admin/local/docker/compose/docker-compose.yml"),
 )
 build_container_default = OptDefault(
     opt=["--build-container-name"],
     default=f"build_container",
-    ev=project_ev,
+    ev=envval_project,
     val=lambda ev_val: f"{ev_val}_build",
 )
 
@@ -517,23 +517,23 @@ class DefaultValues:
 
 qserv_env_vals = FlagEnvVals(
     [
-        image_tag_ev,
-        qserv_image_ev,
-        build_image_ev,
-        user_build_image_ev,
-        run_base_image_ev,
-        mariadb_image_ev,
-        qserv_root_ev,
-        qserv_build_root_ev,
-        project_ev,
-        dashboard_port_ev,
-        dh_user_ev,
-        dh_token_ev,
-        ltd_user_ev,
-        ltd_password_ev,
-        gh_event_name_ev,
-        gh_head_ref_ev,
-        gh_ref_ev,
+        envval_image_tag,
+        envval_qserv_image,
+        envval_build_image,
+        envval_user_build_image,
+        envval_run_base_image,
+        envval_mariadb_image,
+        envval_qserv_root,
+        envval_qserv_build_root,
+        envval_project,
+        envval_dashboard_port,
+        envval_dh_user,
+        envval_dh_token,
+        envval_ltd_user,
+        envval_ltd_password,
+        envval_gh_event_name,
+        envval_gh_head_ref,
+        envval_gh_ref,
     ]
 )
 
@@ -553,24 +553,24 @@ bind_choices = ["all", "python", "bin", "lib64", "lua", "qserv", "etc"]
 
 option_qserv_image = partial(
     click.option,
-    qserv_image_ev.opt,
-    help=qserv_image_ev.help("The name of the qserv image."),
-    default=qserv_image_ev.val(),
+    envval_qserv_image.opt,
+    help=envval_qserv_image.help("The name of the qserv image."),
+    default=envval_qserv_image.val(),
 )
 
 
 option_build_image = partial(
     click.option,
-    build_image_ev.opt,
-    help=build_image_ev.help("The name of the qserv build image."),
-    default=build_image_ev.val(),
+    envval_build_image.opt,
+    help=envval_build_image.help("The name of the qserv build image."),
+    default=envval_build_image.val(),
 )
 
 option_user_build_image = partial(
     click.option,
-    user_build_image_ev.opt,
-    help=user_build_image_ev.help("The name of the qserv user build image."),
-    default=user_build_image_ev.val(),
+    envval_user_build_image.opt,
+    help=envval_user_build_image.help("The name of the qserv user build image."),
+    default=envval_user_build_image.val(),
 )
 
 
@@ -601,46 +601,46 @@ option_pull_image = partial(
 
 option_run_base_image = partial(
     click.option,
-    run_base_image_ev.opt,
-    help=run_base_image_ev.help("The name of the run base image."),
-    default=run_base_image_ev.val(),
+    envval_run_base_image.opt,
+    help=envval_run_base_image.help("The name of the run base image."),
+    default=envval_run_base_image.val(),
 )
 
 
 option_mariadb_image = partial(
     click.option,
-    mariadb_image_ev.opt,
-    help=mariadb_image_ev.help("The name of the mariadb image."),
-    default=mariadb_image_ev.val(),
+    envval_mariadb_image.opt,
+    help=envval_mariadb_image.help("The name of the mariadb image."),
+    default=envval_mariadb_image.val(),
 )
 
 
 option_qserv_root = partial(
     click.option,
-    qserv_root_ev.opt,
-    help=qserv_root_ev.help(
+    envval_qserv_root.opt,
+    help=envval_qserv_root.help(
         "Location of the qserv sources folder outside of the build container. "
         "Default location is relative to the qserv command file."
     ),
-    envvar=qserv_root_ev.env_var,
-    default=qserv_root_ev.default,
+    envvar=envval_qserv_root.env_var,
+    default=envval_qserv_root.default,
     required=True,
 )
 
 
 option_qserv_build_root = partial(
     click.option,
-    qserv_build_root_ev.opt,
-    help=qserv_build_root_ev.help("Location of the qserv sources folder inside the build container."),
-    default=qserv_build_root_ev.default,
+    envval_qserv_build_root.opt,
+    help=envval_qserv_build_root.help("Location of the qserv sources folder inside the build container."),
+    default=envval_qserv_build_root.default,
 )
 
 option_outdir = partial(
     click.option,
-    outdir_ev.opt,
-    help=outdir_ev.help("Location of the folder that will contain unzipped and partitioned integration test datasets"),
-    envvar=outdir_ev.env_var,
-    default=outdir_ev.default,
+    envval_outdir.opt,
+    help=envval_outdir.help("Location of the folder that will contain unzipped and partitioned integration test datasets"),
+    envvar=envval_outdir.env_var,
+    default=envval_outdir.default,
 )
 
 
@@ -653,9 +653,9 @@ option_qserv_group = partial(
 
 option_dashboard_port = partial(
     click.option,
-    dashboard_port_ev.opt,
-    help=dashboard_port_ev.help("The host port to use for the qserv dashboard."),
-    default=dashboard_port_ev.val(),
+    envval_dashboard_port.opt,
+    help=envval_dashboard_port.help("The host port to use for the qserv dashboard."),
+    default=envval_dashboard_port.val(),
 )
 
 
@@ -688,9 +688,9 @@ option_jobs = partial(
 
 option_project = partial(
     click.option,
-    project_ev.opt,
-    help=project_ev.help("The project name for the qserv docker-compose instance."),
-    default=project_ev.val(),
+    envval_project.opt,
+    help=envval_project.help("The project name for the qserv docker-compose instance."),
+    default=envval_project.val(),
     callback=lambda ctx, par, val: None if val == "None" else val,
 )
 
