7de5888015be677e8afa0fd3b8b680c9e8e7aeb7
John Gates <jgates@slac.stanford.edu>
2024-03-19 13:58:16 -0700

    Changed initial czar id number.

--- src/xrdsvc/SsiService.cc
+++ src/xrdsvc/SsiService.cc
@@ -29,6 +29,7 @@
 #include <cassert>
 #include <chrono>
 #include <iostream>
+#include <ranges>
 #include <string>
 #include <stdexcept>
 #include <stdlib.h>
@@ -179,9 +180,9 @@ SsiService::SsiService(XrdSsiLogger* log) {
     StreamBuffer::setMaxTotalBytes(bufferMaxTotalBytes);
 
     // Set thread pool size.
-    unsigned int poolSize =
-            std::max(wsched::BlendScheduler::getMinPoolSize(),
-                     std::max(workerConfig->getThreadPoolSize(), thread::hardware_concurrency()));
+    unsigned int poolSize = ranges::max({wsched::BlendScheduler::getMinPoolSize(),
+                                         workerConfig->getThreadPoolSize(), thread::hardware_concurrency()});
+
     unsigned int maxPoolThreads = max(workerConfig->getMaxPoolThreads(), poolSize);
 
     // poolSize should be greater than either GroupScheduler::maxThreads or ScanScheduler::maxThreads
