432c3442c1574c203e6adb36d3c5f6babe30d3e6
John Gates <jgates@slac.stanford.edu>
2024-05-16 11:22:18 -0700

    Czar and workers can send http messages to each other.

--- src/rproc/InfileMerger.h
+++ src/rproc/InfileMerger.h
@@ -108,6 +108,9 @@ public:
     /// Merge the result data collected over Http.
     bool mergeHttp(std::shared_ptr<qdisp::UberJob> const& uberJob, proto::ResponseData const& responseData);
 
+    /// Merge the result data collected over Http.
+    bool mergeHttp(std::shared_ptr<qdisp::UberJob> const& uberJob, proto::ResponseData const& responseData);
+
     /// Indicate the merge for the job is complete.
     void mergeCompleteFor(int jobId);
 
