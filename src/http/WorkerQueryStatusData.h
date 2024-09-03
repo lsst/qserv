216e0b0734719aab39d05b7dcf4ab31912d37730
John Gates <jgates@slac.stanford.edu>
2024-09-03 11:52:38 -0700

    Added cancellation code and for queries, uberjobs, and czar restart.

--- src/http/WorkerQueryStatusData.h
+++ src/http/WorkerQueryStatusData.h
@@ -35,17 +35,14 @@
 #include "global/clock_defs.h"
 #include "global/intTypes.h"
 
-
 // This header declarations
 namespace lsst::qserv::http {
 
-/// &&& doc
+/// This class just contains the czar id and network contact information.
 class CzarContactInfo {
 public:
     using Ptr = std::shared_ptr<CzarContactInfo>;
-    std::string cName(const char* fnc) const {
-        return std::string("CzarContactInfo") + fnc;
-    }
+    std::string cName(const char* fnc) const { return std::string("CzarContactInfo") + fnc; }
 
     CzarContactInfo() = delete;
     CzarContactInfo(CzarContactInfo const&) = default;
@@ -53,43 +50,39 @@ public:
 
     /// &&& doc
     bool compare(CzarContactInfo const& other) {
-        return (czName == other.czName && czId == other.czId && czPort == other.czPort && czHostName == other.czHostName);
+        return (czName == other.czName && czId == other.czId && czPort == other.czPort &&
+                czHostName == other.czHostName);
     }
 
-    static Ptr create(std::string const& czName_, CzarIdType czId_, int czPort_, std::string const& czHostName_) {
-        return Ptr(new CzarContactInfo(czName_, czId_, czPort_, czHostName_));
+    static Ptr create(std::string const& czName_, CzarIdType czId_, int czPort_,
+                      std::string const& czHostName_, uint64_t czStartupTime_) {
+        return Ptr(new CzarContactInfo(czName_, czId_, czPort_, czHostName_, czStartupTime_));
     }
 
-    static Ptr createJson(nlohmann::json const& czarJson);
+    static Ptr createFromJson(nlohmann::json const& czarJson);
 
-    std::string const czName;  ///< czar "name"
-    CzarIdType const czId;  ///< czar "id"
-    int const czPort;     ///< czar "management-port"
-    std::string const czHostName; ///< czar "management-host-name"
+    std::string const czName;      ///< czar "name"
+    CzarIdType const czId;         ///< czar "id"
+    int const czPort;              ///< czar "management-port"
+    std::string const czHostName;  ///< czar "management-host-name"
+    uint64_t const czStartupTime;  ///< czar startup time
 
     /// &&& doc
     nlohmann::json serializeJson() const;
 
-    /// &&& doc
-    //&&&bool parse(nlohmann::json const& czarJson);
-
     std::string dump() const;
-    /* &&&
-    auto& jsWCzar = jsWorkerR["czar"];
-    jsWCzar["name"] = czarConfig->name();
-    jsWCzar["id"]= czarConfig->id();
-    jsWCzar["management-port"] = czarConfig->replicationHttpPort();
-    jsWCzar["management-host-name"] = util::get_current_host_fqdn();
-    */
+
 private:
-    CzarContactInfo(std::string const& czName_, CzarIdType czId_, int czPort_, std::string const& czHostName_)
-        : czName(czName_), czId(czId_), czPort(czPort_), czHostName(czHostName_) {}
+    CzarContactInfo(std::string const& czName_, CzarIdType czId_, int czPort_, std::string const& czHostName_,
+                    uint64_t czStartupTime_)
+            : czName(czName_),
+              czId(czId_),
+              czPort(czPort_),
+              czHostName(czHostName_),
+              czStartupTime(czStartupTime_) {}
 };
 
-
-/// &&& doc This class just contains the worker id and network communication
-///     information, but it may be desirable to store connections to the
-///     worker here as well.
+/// This class just contains the worker id and network communication information.
 class WorkerContactInfo {
 public:
     using Ptr = std::shared_ptr<WorkerContactInfo>;
@@ -97,39 +90,62 @@ public:
     using WCMap = std::unordered_map<std::string, Ptr>;
     using WCMapPtr = std::shared_ptr<WCMap>;
 
-    static Ptr create(std::string const& wId_, std::string const& wHost_,
-            std::string const& wManagementHost_, int wPort_, TIMEPOINT updateTime_) {
-        return Ptr(new WorkerContactInfo(wId_, wHost_,
-             wManagementHost_, wPort_, updateTime_));
+    static Ptr create(std::string const& wId_, std::string const& wHost_, std::string const& wManagementHost_,
+                      int wPort_, TIMEPOINT updateTime_) {
+        return Ptr(new WorkerContactInfo(wId_, wHost_, wManagementHost_, wPort_, updateTime_));
     }
 
-    /// &&& doc
-    static Ptr createJson(nlohmann::json const& workerJson, TIMEPOINT updateTime);
+    /// &&& doc Used to create WorkerQueryStatusData object from a registry json message.
+    static Ptr createFromJsonRegistry(std::string const& wId_, nlohmann::json const& regJson);
+
+    /// &&& doc Used to create WorkerQueryStatusData object from a worker json message.
+    static Ptr createFromJsonWorker(nlohmann::json const& workerJson, TIMEPOINT updateTime);
 
     /// &&& doc
     nlohmann::json serializeJson() const;
 
     std::string cName(const char* fn) { return std::string("WorkerContactInfo::") + fn; }
 
-    /// &&& make private
-    WorkerContactInfo(std::string const& wId_, std::string const& wHost_,
-            std::string const& wManagementHost_, int wPort_, TIMEPOINT updateTime_)
-    : wId(wId_),
-      wHost(wHost_),
-      wManagementHost(wManagementHost_),
-      wPort(wPort_) {
-        regUpdateTime(updateTime_);
+    std::string const wId;  ///< key, this is the one thing that cannot change.
+
+    std::string getWHost() const {
+        std::lock_guard<std::mutex> lg(_rMtx);
+        return _wHost;
     }
-    std::string const wId;              ///< key
-    std::string const wHost;            ///< "host-addr" entry.
-    std::string const wManagementHost;  ///< "management-host-name" entry.
-    int const wPort;                    ///< "management-port" entry.
 
+    std::string getWManagementHost() const {
+        std::lock_guard<std::mutex> lg(_rMtx);
+        return _wManagementHost;
+    }
 
-    /// Return true if all members, aside from updateTime, are equal.
+    int getWPort() const {
+        std::lock_guard<std::mutex> lg(_rMtx);
+        return _wPort;
+    }
+
+    /// &&doc
+    void changeBaseInfo(WorkerContactInfo const& other) {
+        auto [oWId, oWHost, oWManagementHost, oWPort] = other.getAll();
+        std::lock_guard<std::mutex> lg(_rMtx);
+        _wHost = oWHost;
+        _wManagementHost = oWManagementHost;
+        _wPort = oWPort;
+    }
+
+    /// @return wId - workerId
+    /// @return _wHost - worker host
+    /// @return _wManagementHost - management host
+    /// @return _wPort - worker port
+    std::tuple<std::string, std::string, std::string, int> getAll() const {
+        std::lock_guard<std::mutex> lg(_rMtx);
+        return {wId, _wHost, _wManagementHost, _wPort};
+    }
+
+    /// Return true if communication related items are the same.
     bool isSameContactInfo(WorkerContactInfo const& other) const {
-        return (wId == other.wId && wHost == other.wHost && wManagementHost == other.wManagementHost &&
-                wPort == other.wPort);
+        auto [oWId, oWHost, oWManagementHost, oWPort] = other.getAll();
+        std::lock_guard<std::mutex> lg(_rMtx);
+        return (wId == oWId && _wHost == oWHost && _wManagementHost == oWManagementHost && _wPort == oWPort);
     }
 
     void regUpdateTime(TIMEPOINT updateTime) {
@@ -148,85 +164,206 @@ public:
         return _regUpdate;
     }
 
+    /* &&&
+    /// Sets _wStartupTime to startupTime, but only if _wStartupTime was 0.
+    /// @returns true if _wStartupTime was set.
+    bool setWStartupTime(uint64_t startupTime) { //&&& del if not used
+        std::lock_guard<std::mutex> lg(_rMtx);
+        if (_wStartupTime == 0) {
+            _wStartupTime = startupTime;
+            return true;
+        }
+        return false;
+    }
+    */
+
+    /// @return true if startupTime equals _wStartupTime or _wStartupTime was never set,
+    ///   if _wStartupTime was never set, it is set to startupTime.
+    /// @return false indicates the worker was restarted and all associated jobs need
+    ///   re-assignment.
+    bool checkWStartupTime(uint64_t startupTime) {
+        std::lock_guard<std::mutex> lg(_rMtx);
+        if (_wStartupTime == startupTime) {
+            return true;
+        }
+        if (_wStartupTime == 0) {
+            _wStartupTime = startupTime;
+            return true;
+        }
+        _wStartupTime = startupTime;
+        return false;
+    }
+
+    uint64_t getWStartupTime() const {
+        std::lock_guard<std::mutex> lg(_rMtx);
+        return _wStartupTime;
+    }
+
     std::string dump() const;
 
 private:
+    WorkerContactInfo(std::string const& wId_, std::string const& wHost_, std::string const& wManagementHost_,
+                      int wPort_, TIMEPOINT updateTime_)
+            : wId(wId_), _wHost(wHost_), _wManagementHost(wManagementHost_), _wPort(wPort_) {
+        regUpdateTime(updateTime_);
+    }
+
+    // _rMtx must be locked before calling
+    std::string _dump() const;
+
+    // _rMtx must be locked before calling
+    nlohmann::json _serializeJson() const;
+
+    std::string _wHost;            ///< "host-addr" entry.
+    std::string _wManagementHost;  ///< "management-host-name" entry.
+    int _wPort;                    ///< "management-port" entry.
+
     /// Last time the registry heard from this worker. The ActiveWorker class
     /// will use this to determine the worker's state.
     /// &&& Store in seconds since epoch to make atomic?
     TIMEPOINT _regUpdate;
 
-    mutable std::mutex _rMtx; ///< protects _regUpdate
+    /// "w-startup-time", it's value is set to zero until the real value is
+    /// received from the worker. Once it is non-zero, any change indicates
+    /// the worker was restarted and all UberJobs that were assigned there
+    /// need to be unassigned. On the worker, this should always be set from
+    /// foreman()->getStartupTime();
+    uint64_t _wStartupTime = 0;
+
+    mutable std::mutex _rMtx;  ///< protects _regUpdate
 };
 
-
-/// &&& doc
-class WorkerQueryStatusData  {
+/// This classes purpose is to be a structure to store and transfer information
+/// about which queries have been completed or cancelled on the worker. This
+/// class contains the functions that encode and decode the data they contain
+/// to and from a json format.
+class WorkerQueryStatusData {
 public:
     using Ptr = std::shared_ptr<WorkerQueryStatusData>;
-
-    /* &&&
-    enum State {
-        ALIVE = 0,
-        QUESTIONABLE,
-        DEAD
-    };
-    */
 
     WorkerQueryStatusData() = delete;
     WorkerQueryStatusData(WorkerQueryStatusData const&) = delete;
     WorkerQueryStatusData& operator=(WorkerQueryStatusData const&) = delete;
 
-    std::string cName(const char* fName) {
-        return std::string("WorkerQueryStatusData::") + fName + " " + ((_wInfo == nullptr) ? "?" : _wInfo->wId);
+    std::string cName(const char* fName) { return std::string("WorkerQueryStatusData::") + fName; }
+
+    static Ptr create(WorkerContactInfo::Ptr const& wInfo_, CzarContactInfo::Ptr const& czInfo_,
+                      std::string const& replicationInstanceId_, std::string const& replicationAuthKey_) {
+        return Ptr(new WorkerQueryStatusData(wInfo_, czInfo_, replicationInstanceId_, replicationAuthKey_));
     }
 
-    //&&&static std::string getStateStr(State st);
-
-    static Ptr create(WorkerContactInfo::Ptr const& wInfo, CzarContactInfo::Ptr const& czInfo,
-            std::string const& replicationInstanceId, std::string const& replicationAuthKey) {
-        return Ptr(new WorkerQueryStatusData(wInfo, czInfo, replicationInstanceId, replicationAuthKey));
-    }
-
-    /// &&& doc
-    static Ptr createJson(nlohmann::json const& czarJson,
-            std::string const& replicationInstanceId, std::string const& replicationAuthKey, TIMEPOINT updateTm);
-
+    /// &&& doc Used to create WorkerQueryStatusData object from a worker json message.
+    static Ptr createFromJson(nlohmann::json const& czarJson, std::string const& replicationInstanceId_,
+                              std::string const& replicationAuthKey_, TIMEPOINT updateTm);
 
     ~WorkerQueryStatusData() = default;
 
-    WorkerContactInfo::Ptr getWInfo() const { return _wInfo; }
+    void setWInfo(WorkerContactInfo::Ptr const& wInfo_) {
+        std::lock_guard<std::mutex> lgI(_infoMtx);
+        if (_wInfo == nullptr) {
+            _wInfo = wInfo_;
+            return;
+        }
+        if (wInfo_ != nullptr) {
+            // This only change host and port values of _wInfo.
+            _wInfo->changeBaseInfo(*wInfo_);
+        }
+    }
+
+    WorkerContactInfo::Ptr getWInfo() const {
+        std::lock_guard<std::mutex> lgI(_infoMtx);
+        return _wInfo;
+    }
+    CzarContactInfo::Ptr getCzInfo() const { return _czInfo; }
+
+    /// doc &&&
+    void addDeadUberJob(QueryId qId, UberJobId ujId, TIMEPOINT tm);
 
     /// &&& doc
     void addDeadUberJobs(QueryId qId, std::vector<UberJobId> ujIds, TIMEPOINT tm);
 
-    std::string dump() const;
-
-//&&&private:
-    WorkerQueryStatusData(WorkerContactInfo::Ptr const& wInfo, CzarContactInfo::Ptr const& czInfo,
-            std::string const& replicationInstanceId, std::string const& replicationAuthKey)
-        : _wInfo(wInfo), _czInfo(czInfo),
-          _replicationInstanceId(replicationInstanceId), _replicationAuthKey(replicationAuthKey) {}
-
-    std::map<QueryId, TIMEPOINT> _qIdDoneKeepFiles;  ///< &&& doc - limit reached
-    std::map<QueryId, TIMEPOINT> _qIdDoneDeleteFiles;  ///< &&& doc -cancelled/finished
-    std::map<QueryId, std::map<UberJobId, TIMEPOINT>> _qIdDeadUberJobs; ///< &&& doc
-
-    /// &&& TODO:UJ Worth the effort to inform worker of killed UberJobs?
-    //std::map<QueryId, std::set<UberJobId>> _killedUberJobs;
-
-    WorkerContactInfo::Ptr _wInfo; ///< &&& doc
-    CzarContactInfo::Ptr _czInfo; //< &&& doc
-
-    std::string const _replicationInstanceId; ///< &&& doc
-    std::string const _replicationAuthKey;  ///< &&& doc
+    /// &&& doc
+    void addToDoneDeleteFiles(QueryId qId);
 
     /// &&& doc
-    std::shared_ptr<nlohmann::json> serializeJson(double timeoutAliveSecs, double timeoutDeadSecs, double maxLifetime);
+    void addToDoneKeepFiles(QueryId qId);
+
+    /// &&& doc
+    void removeDeadUberJobsFor(QueryId qId);
+
+    void setCzarCancelAfterRestart(CzarIdType czId, QueryId lastQId) {
+        std::lock_guard<std::mutex> mapLg(mapMtx);
+        czarCancelAfterRestart = true;
+        czarCancelAfterRestartCzId = czId;
+        czarCancelAfterRestartQId = lastQId;
+    }
+
+    bool isCzarRestart() const { return czarCancelAfterRestart; }
+    CzarIdType getCzarRestartCzarId() const { return czarCancelAfterRestartCzId; }
+    QueryId getCzarRestartQueryId() const { return czarCancelAfterRestartQId; }
+
+    /// Create a json object held by a shared pointer to use as a message.
+    /// Old objects in this instance will be removed after being added to the
+    /// json message.
+    std::shared_ptr<nlohmann::json> serializeJson(double maxLifetime);
+
+    /// Add contents of qIdDoneKeepFiles, _qIdDoneDeleteFiles, and _qIdDeadUberJobs to `jsWR`,
+    /// and remove map elements that have an age (tmMark - element.touchTime) greater
+    /// than maxLifetime.
+    void addListsToJson(nlohmann::json& jsWR, TIMEPOINT tmMark, double maxLifetime);
 
     /// &&& doc
     /// @throws std::invalid_argument
-    bool _parseLists(nlohmann::json const& jsWorkerReq, TIMEPOINT updateTm); // &&& delete after basic testing
+    void parseLists(nlohmann::json const& jsWR, TIMEPOINT updateTm);
+
+    /// &&& doc
+    //&&&nlohmann::json serializeResponseJson();
+    nlohmann::json serializeResponseJson(uint64_t workerStartupTime);
+
+    /// &&& doc
+    //&&&bool handleResponseJson(nlohmann::json const& jsResp);
+    std::pair<bool, bool> handleResponseJson(nlohmann::json const& jsResp);
+
+    /// &&& doc
+    ///&&&void handleCzarRestart();
+
+    /// &&& doc
+    static void parseListsInto(nlohmann::json const& jsWR, TIMEPOINT updateTm,
+                               std::map<QueryId, TIMEPOINT>& doneKeepF,
+                               std::map<QueryId, TIMEPOINT>& doneDeleteF,
+                               std::map<QueryId, std::map<UberJobId, TIMEPOINT>>& deadUberJobs);
+
+    std::string dump() const;
+
+    // Making these private requires member functions to be written
+    // that cause issues with linking. All of the workarounds are ugly.
+    std::map<QueryId, TIMEPOINT> qIdDoneKeepFiles;                      ///< &&& doc - limit reached
+    std::map<QueryId, TIMEPOINT> qIdDoneDeleteFiles;                    ///< &&& doc -cancelled/finished
+    std::map<QueryId, std::map<UberJobId, TIMEPOINT>> qIdDeadUberJobs;  ///< &&& doc
+    std::atomic<bool> czarCancelAfterRestart = false;
+    CzarIdType czarCancelAfterRestartCzId = 0;
+    QueryId czarCancelAfterRestartQId = 0;
+    /// Protects _qIdDoneKeepFiles, _qIdDoneDeleteFiles, _qIdDeadUberJobs,
+    /// and czarCancelAfter variables.
+    mutable std::mutex mapMtx;
+
+private:
+    WorkerQueryStatusData(WorkerContactInfo::Ptr const& wInfo_, CzarContactInfo::Ptr const& czInfo_,
+                          std::string const& replicationInstanceId_, std::string const& replicationAuthKey_)
+            : _wInfo(wInfo_),
+              _czInfo(czInfo_),
+              _replicationInstanceId(replicationInstanceId_),
+              _replicationAuthKey(replicationAuthKey_) {}
+
+    WorkerContactInfo::Ptr _wInfo;       ///< &&& doc
+    CzarContactInfo::Ptr const _czInfo;  //< &&& doc
+    mutable std::mutex _infoMtx;         ///< protects wInfo
+
+    std::string const _replicationInstanceId;  ///< &&& doc
+    std::string const _replicationAuthKey;     ///< &&& doc
+
+    /// _infoMtx must be locked before calling.
+    std::string _dump() const;
 };
 
 }  // namespace lsst::qserv::http
--- src/wpublish/QueryStatistics.h
+++ src/wpublish/QueryStatistics.h
@@ -41,6 +41,7 @@
 #include "global/intTypes.h"
 #include "wbase/Task.h"
 #include "wsched/SchedulerBase.h"
+#include "util/InstanceCount.h"  //&&&
 
 namespace lsst::qserv::wbase {
 class UserQueryInfo;
@@ -95,6 +96,7 @@ public:
     void addTaskTransmit(double timeSeconds, int64_t bytesTransmitted, int64_t rowsTransmitted,
                          double bufferFillSecs);
 
+    //&&&void touch(TIMEPOINT const now);
     void addTask(TIMEPOINT const now);
     void addTaskRunning(TIMEPOINT const now);
     bool addTaskCompleted(TIMEPOINT const now, double const taskDuration);
