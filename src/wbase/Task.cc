903265528c2c7c8dfbbc73ec87dc83d7a8ead589
John Gates <jgates@slac.stanford.edu>
2024-12-18 09:37:50 -0800

    Added worker exe, memory/disk hybrid for transfering csv files, fixed tests.

--- src/wbase/Task.cc
+++ src/wbase/Task.cc
@@ -202,7 +202,7 @@ std::vector<Task::Ptr> Task::createTasksFromUberJobMsg(
 
     std::string workerId = ujMsg->getWorkerId();
     auto jobSubQueryTempMap = ujMsg->getJobSubQueryTempMap();
-    auto jobDbTablesMap = ujMsg->getJobDbTablesMap();
+    auto jobDbTablesMap = ujMsg->getJobDbTableMap();
     auto jobMsgVect = ujMsg->getJobMsgVect();
 
     for (auto const& jobMsg : *jobMsgVect) {
@@ -272,81 +272,72 @@ std::vector<Task::Ptr> Task::createTasksFromUberJobMsg(
 std::vector<Task::Ptr> Task::createTasksForUnitTest(
         std::shared_ptr<UberJobData> const& ujData, nlohmann::json const& jsJobs,
         std::shared_ptr<wbase::FileChannelShared> const& sendChannel, int maxTableSizeMb,
-        std::shared_ptr<wdb::ChunkResourceMgr> const& chunkResourceMgr) {
+        std::shared_ptr<wdb::ChunkResourceMgr> const& chunkResourceMgr,
+        std::shared_ptr<wpublish::QueriesAndChunks> const& queriesAndChunks) {
+    vector<Task::Ptr> vect;
     QueryId qId = ujData->getQueryId();
-    UberJobId ujId = ujData->getUberJobId();
     CzarIdType czId = ujData->getCzarId();
     string funcN(__func__);
     funcN += " QID=" + to_string(qId) + " czId=" + to_string(czId);
+    protojson::JobSubQueryTempMap::Ptr jobSubQueryTempMap{protojson::JobSubQueryTempMap::create()};
+    protojson::JobDbTableMap::Ptr jobDbTablesMap{protojson::JobDbTableMap::create()};
+    protojson::JobMsg::VectPtr jobMsgVect{new protojson::JobMsg::Vect()};
+    for (auto const& jsUjJob : jsJobs) {
+        protojson::JobMsg::Ptr jobMsgPtr =
+                protojson::JobMsg::createFromJson(jsUjJob, jobSubQueryTempMap, jobDbTablesMap);
+        jobMsgVect->push_back(jobMsgPtr);
+    }
 
-    vector<Task::Ptr> vect;
-    for (auto const& job : jsJobs) {
-        json const& jsJobDesc = job["jobdesc"];
-        http::RequestBodyJSON rbJobDesc(jsJobDesc);
-        // See qproc::TaskMsgFactory::makeMsgJson for message construction.
-        auto const jdCzarId = rbJobDesc.required<qmeta::CzarId>("czarId");
-        auto const jdQueryId = rbJobDesc.required<QueryId>("queryId");
-        if (jdQueryId != qId) {
-            throw TaskException(ERR_LOC, string("ujId=") + to_string(ujId) + " qId=" + to_string(qId) +
-                                                 " QueryId mismatch Job qId=" + to_string(jdQueryId));
-        }
-        auto const jdJobId = rbJobDesc.required<int>("jobId");
-        auto const jdAttemptCount = rbJobDesc.required<int>("attemptCount");
-        auto const jdQuerySpecDb = rbJobDesc.required<string>("querySpecDb");
-        auto const jdMaxTableSizeMb = rbJobDesc.required<int>("maxTableSize");
-        auto const jdChunkId = rbJobDesc.required<int>("chunkId");
-        LOGS(_log, LOG_LVL_TRACE,
-             funcN << " jd cid=" << jdCzarId << " jdQId=" << jdQueryId << " jdJobId=" << jdJobId
-                   << " jdAtt=" << jdAttemptCount << " jdQDb=" << jdQuerySpecDb
-                   << " maxTblSz=" << jdMaxTableSizeMb << " chunkId=" << jdChunkId);
+    wpublish::QueryStatistics::Ptr queryStats = queriesAndChunks->addQueryId(qId, czId);
+    UserQueryInfo::Ptr userQueryInfo = queryStats->getUserQueryInfo();
 
-        auto const jdQueryFragments = rbJobDesc.required<json>("queryFragments");
+    for (auto const& jobMsg : *jobMsgVect) {
+        JobId jobId = jobMsg->getJobId();
+        int attemptCount = jobMsg->getAttemptCount();
+        std::string chunkQuerySpecDb = jobMsg->getChunkQuerySpecDb();
+        int chunkId = jobMsg->getChunkId();
+
+        std::vector<int> chunkScanTableIndexes = jobMsg->getChunkScanTableIndexes();
+        auto jobFragments = jobMsg->getJobFragments();
         int fragmentNumber = 0;
-        for (auto const& frag : jdQueryFragments) {
-            vector<string> fragSubQueries;
-            vector<int> fragSubchunkIds;
-            vector<TaskDbTbl> fragSubTables;
-            LOGS(_log, LOG_LVL_DEBUG, funcN << " frag=" << frag);
-            http::RequestBodyJSON rbFrag(frag);
-            auto const& jsQueries = rbFrag.required<json>("queries");
-            // TODO:UJ move to uberjob???, these should be the same for all jobs
-            for (auto const& subQ : jsQueries) {
-                http::RequestBodyJSON rbSubQ(subQ);
-                auto const subQuery = rbSubQ.required<string>("subQuery");
-                LOGS(_log, LOG_LVL_DEBUG, funcN << " subQuery=" << subQuery);
-                fragSubQueries.push_back(subQuery);
-            }
-            auto const& resultTable = rbFrag.required<string>("resultTable");
-            auto const& jsSubIds = rbFrag.required<json>("subchunkIds");
-            for (auto const& scId : jsSubIds) {
-                fragSubchunkIds.push_back(scId);
-            }
-            auto const& jsSubTables = rbFrag.required<json>("subchunkTables");
 
-            for (auto const& scDbTable : jsSubTables) {  // TODO:UJ are these the same for all jobs?
-                http::RequestBodyJSON rbScDbTable(scDbTable);
-                string scDb = rbScDbTable.required<string>("scDb");
-                string scTable = rbScDbTable.required<string>("scTable");
+        for (auto const& fMsg : *jobFragments) {
+            // These need to be constructed for the fragment
+            vector<string> fragSubQueries;
+            vector<TaskDbTbl> fragSubTables;
+            vector<int> fragSubchunkIds;
+
+            vector<int> fsqIndexes = fMsg->getJobSubQueryTempIndexes();
+            for (int fsqIndex : fsqIndexes) {
+                string fsqStr = jobSubQueryTempMap->getSubQueryTemp(fsqIndex);
+                fragSubQueries.push_back(fsqStr);
+            }
+
+            vector<int> dbTblIndexes = fMsg->getJobDbTablesIndexes();
+            for (int dbTblIndex : dbTblIndexes) {
+                auto [scDb, scTable] = jobDbTablesMap->getDbTable(dbTblIndex);
                 TaskDbTbl scDbTbl(scDb, scTable);
                 fragSubTables.push_back(scDbTbl);
             }
 
+            fragSubchunkIds = fMsg->getSubchunkIds();
+
             for (string const& fragSubQ : fragSubQueries) {
-                LOGS(_log, LOG_LVL_DEBUG, "ignoring " << fragSubQ);
+                size_t templateId = userQueryInfo->addTemplate(fragSubQ);
                 if (fragSubchunkIds.empty()) {
                     bool const noSubchunks = false;
                     int const subchunkId = -1;
-                    auto task = Task::Ptr(new Task(ujData, jdJobId, jdAttemptCount, jdChunkId, fragmentNumber,
-                                                   0, noSubchunks, subchunkId, jdQuerySpecDb, fragSubTables,
-                                                   fragSubchunkIds, sendChannel, nullptr));
+                    auto task = Task::Ptr(new Task(ujData, jobId, attemptCount, chunkId, fragmentNumber,
+                                                   templateId, noSubchunks, subchunkId, chunkQuerySpecDb,
+                                                   fragSubTables, fragSubchunkIds, sendChannel, queryStats));
                     vect.push_back(task);
                 } else {
                     for (auto subchunkId : fragSubchunkIds) {
                         bool const hasSubchunks = true;
                         auto task =
-                                Task::Ptr(new Task(ujData, jdJobId, jdAttemptCount, jdChunkId, fragmentNumber,
-                                                   0, hasSubchunks, subchunkId, jdQuerySpecDb, fragSubTables,
-                                                   fragSubchunkIds, sendChannel, nullptr));
+                                Task::Ptr(new Task(ujData, jobId, attemptCount, chunkId, fragmentNumber,
+                                                   templateId, hasSubchunks, subchunkId, chunkQuerySpecDb,
+                                                   fragSubTables, fragSubchunkIds, sendChannel, queryStats));
                         vect.push_back(task);
                     }
                 }
@@ -588,10 +579,19 @@ nlohmann::json Task::getJson() const {
 
 int64_t Task::getMaxTableSize() const { return _ujData->getMaxTableSizeBytes(); }
 
-ostream& operator<<(ostream& os, Task const& t) {
+ostream& Task::dump(ostream& os) const {
     os << "Task: "
-       << "msg: " << t.getIdStr() << " chunk=" << t._chunkId << " db=" << t._db << " " << t.getQueryString();
-
+       << "msg: " << getIdStr() << " chunk=" << _chunkId << " seq=" << _tSeq << " db=" << _db << " "
+       << getQueryString();
+    if (_ujData == nullptr) {
+        os << " ujData=null";
+        return os;
+    }
+    if (_ujData->getScanInfo() == nullptr) {
+        os << " scanInfo=null";
+        return os;
+    }
+    _ujData->getScanInfo()->dump(os);
     return os;
 }
 
