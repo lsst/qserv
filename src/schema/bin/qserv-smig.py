c6f12f8796816491bdee4fd550855bde8b6ffb9b
Fritz Mueller <fritzm@fritzm.org>
2025-02-14 00:06:55 +0000

    ruff format

--- src/schema/bin/qserv-smig.py
+++ src/schema/bin/qserv-smig.py
@@ -20,9 +20,7 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 
-"""Application which implements migration process for qserv databases.
-"""
-
+"""Application which implements migration process for qserv databases."""
 
 import argparse
 import os
@@ -33,37 +31,60 @@ from lsst.qserv.schema import smig
 _def_scripts = os.path.join(os.environ.get("QSERV_DIR", ""), "share/qserv/schema")
 
 
-
 def main():
-
     parser = argparse.ArgumentParser(description="Qserv database schema migration.")
 
-    parser.add_argument("-v", "--verbose", default=0, action="count",
-                        help="Use one -v for INFO logging, two for DEBUG.")
+    parser.add_argument(
+        "-v", "--verbose", default=0, action="count", help="Use one -v for INFO logging, two for DEBUG."
+    )
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-m", "--do-migrate", default=False, action="store_true",
-                       help="Do migration, without this option script prints various info "
-                       "and exits.")
-    group.add_argument("--check", default=False, action="store_true",
-                       help="Check that migration is needed, script returns 0 if schema is "
-                       "up-to-date, 1 otherwise.")
-    parser.add_argument("-n", "--final", default=None, action="store", type=int,
-                        metavar="VERSION",
-                        help="Stop migration at given version, by default update to "
-                        "latest version.")
-    parser.add_argument("--scripts", default=_def_scripts, action="store",
-                        metavar="PATH",
-                        help="Location for migration scripts, def: %(default)s.")
+    group.add_argument(
+        "-m",
+        "--do-migrate",
+        default=False,
+        action="store_true",
+        help="Do migration, without this option script prints various info and exits.",
+    )
+    group.add_argument(
+        "--check",
+        default=False,
+        action="store_true",
+        help="Check that migration is needed, script returns 0 if schema is up-to-date, 1 otherwise.",
+    )
+    parser.add_argument(
+        "-n",
+        "--final",
+        default=None,
+        action="store",
+        type=int,
+        metavar="VERSION",
+        help="Stop migration at given version, by default update to latest version.",
+    )
+    parser.add_argument(
+        "--scripts",
+        default=_def_scripts,
+        action="store",
+        metavar="PATH",
+        help="Location for migration scripts, def: %(default)s.",
+    )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-c", "--connection", metavar="CONNECTION",
-                       help="Connection string in format mysql://user:pass@host:port/database.")
-    group.add_argument("-f", "--config-file", metavar="PATH",
-                       help="Name of configuration file in INI format with connection parameters.")
-    parser.add_argument("-s", "--config-section", metavar="NAME",
-                        help="Name of configuration section in configuration file.")
+    group.add_argument(
+        "-c",
+        "--connection",
+        metavar="CONNECTION",
+        help="Connection string in format mysql://user:pass@host:port/database.",
+    )
+    group.add_argument(
+        "-f",
+        "--config-file",
+        metavar="PATH",
+        help="Name of configuration file in INI format with connection parameters.",
+    )
+    parser.add_argument(
+        "-s", "--config-section", metavar="NAME", help="Name of configuration section in configuration file."
+    )
 
-    parser.add_argument("module",
-                        help="Name of Qserv module for which to update schema, e.g. qmeta.")
+    parser.add_argument("module", help="Name of Qserv module for which to update schema, e.g. qmeta.")
 
     args = parser.parse_args()
     smig(**args)
