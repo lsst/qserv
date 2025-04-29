80b594d31a4eee77450b4421d3c826da2c617be2
John Gates <jgates@slac.stanford.edu>
2025-04-29 08:25:32 -0700

    Added JobErrorMsg.

--- src/mysql/LocalInfile.cc
+++ src/mysql/LocalInfile.cc
@@ -132,8 +132,6 @@ int LocalInfile::getError(char* buf, unsigned int bufLen) {
     return 0;
 }
 
-LocalInfile::Mgr::~Mgr() { LOGS(_log, LOG_LVL_TRACE, "LocalInfile::Mgr::~Mgr()"); }
-
 void LocalInfile::Mgr::attach(MYSQL* mysql) {
     mysql_set_local_infile_handler(mysql, local_infile_init, local_infile_read, local_infile_end,
                                    local_infile_error, this);
@@ -156,7 +154,11 @@ std::string LocalInfile::Mgr::prepareSrc(std::shared_ptr<CsvBuffer> const& csvBu
 int LocalInfile::Mgr::local_infile_init(void** ptr, const char* filename, void* userdata) {
     assert(userdata);
     LocalInfile::Mgr* m = static_cast<LocalInfile::Mgr*>(userdata);
+<<<<<<< HEAD
     auto csvBuffer = m->get(std::string(filename));
+=======
+    auto csvBuffer = m->getCsv(std::string(filename));
+>>>>>>> 24f5d37ea (Added JobErrorMsg.)
     assert(csvBuffer);
     LocalInfile* lf = new LocalInfile(filename, csvBuffer);
     *ptr = lf;
@@ -190,10 +192,17 @@ void LocalInfile::Mgr::setBuffer(std::string const& filename, std::shared_ptr<Cs
     }
 }
 
+<<<<<<< HEAD
 std::shared_ptr<CsvBuffer> LocalInfile::Mgr::get(std::string const& filename) {
     std::lock_guard<std::mutex> lock(_mapMutex);
     CsvBufferMap::iterator i = _map.find(filename);
     if (i == _map.end()) {
+=======
+std::shared_ptr<CsvBuffer> LocalInfile::Mgr::getCsv(std::string const& filename) {
+    std::lock_guard<std::mutex> lock(_mapMutex);
+    auto i = _mapCsv.find(filename);
+    if (i == _mapCsv.end()) {
+>>>>>>> 24f5d37ea (Added JobErrorMsg.)
         return std::shared_ptr<CsvBuffer>();
     }
     return i->second;
@@ -209,7 +218,11 @@ std::string LocalInfile::Mgr::_nextFilename() {
 
 bool LocalInfile::Mgr::_set(std::string const& filename, std::shared_ptr<CsvBuffer> const& csvBuffer) {
     std::lock_guard<std::mutex> lock(_mapMutex);
+<<<<<<< HEAD
     auto res = _map.insert(std::pair<std::string, std::shared_ptr<CsvBuffer>>(filename, csvBuffer));
+=======
+    auto res = _mapCsv.insert(std::pair<std::string, std::shared_ptr<CsvBuffer>>(filename, csvBuffer));
+>>>>>>> 24f5d37ea (Added JobErrorMsg.)
     return res.second;
 }
 
