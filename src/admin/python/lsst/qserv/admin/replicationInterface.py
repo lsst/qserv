46a81b3abae587fbf812f7cb27f92c7f68aa7a44
Igor Gaponenko <gapon@slac.stanford.edu>
2025-06-11 17:39:44 -0700

    Bumped the version number of the REST API to 42

--- src/admin/python/lsst/qserv/admin/replicationInterface.py
+++ src/admin/python/lsst/qserv/admin/replicationInterface.py
@@ -41,7 +41,7 @@ headers = {"Content-Type": "application/json"}
 
 chunk_info_file = "chunk_info.json"
 
-repl_api_version = 41
+repl_api_version = 42
 
 _log = logging.getLogger(__name__)
 
--- src/www/qserv/js/Common.js
+++ src/www/qserv/js/Common.js
@@ -6,7 +6,7 @@ function(sqlFormatter,
          _) {
 
     class Common {
-        static RestAPIVersion = 41;
+        static RestAPIVersion = 42;
         static query2text(query, expanded) {
             if (expanded) {
                 if (query.length > Common._max_expanded_length) {
