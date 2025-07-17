77c3f3f864a4f7ed42e7f1b6f3256bd7a8ef54f9
Igor Gaponenko <gapon@slac.stanford.edu>
2025-07-16 19:14:35 -0700

    Incremented the version number of the Qserv API to 46

--- src/www/qserv/js/Common.js
+++ src/www/qserv/js/Common.js
@@ -6,7 +6,7 @@ function(sqlFormatter,
          _) {
 
     class Common {
-        static RestAPIVersion = 45;
+        static RestAPIVersion = 46;
         static query2text(query, expanded) {
             if (expanded) {
                 if (query.length > Common._max_expanded_length) {
