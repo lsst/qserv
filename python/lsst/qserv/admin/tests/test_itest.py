62f875c14a59cbe06b4092a3ccf154cbce8371fa
Fritz Mueller <fritzm@fritzm.org>
2026-02-01 08:20:18 +0000

    Remove qserv watcher

--- python/lsst/qserv/admin/tests/test_itest.py
+++ python/lsst/qserv/admin/tests/test_itest.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Unit tests for test_watcher."""
+"""Unit tests for itest."""
 
 import unittest
 
