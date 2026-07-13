bec73bc4b2a04731df2c9e98184b2c98442d4d84
Matthew Malensek <malensek@stanford.edu>
2026-07-13 13:11:39 -0700

    Add spatial join and area restrictor tests
    These tests exercise spatial joins via scisql_s2PtInCircle and area
    restrictors via scisql_angSep.
    
    They also include test coverage for director-child and child-child
    spatial joins, as well as near-neighbor integration tests.

---
+++ data/case01/queries/1051.2_nn.sql
@@ -0,0 +1,14 @@
+-- Find near-neighbor objects in a given region
+--
+-- Variant of 1051_nn.sql that expresses the area restrictor using
+-- scisql_angSep(...) <= r instead of qserv_areaspec_box(...)
+
+-- pragma sortresult
+SELECT o1.objectId AS objId1,
+       o2.objectId AS objId2,
+       scisql_angSep(o1.ra_PS, o1.decl_PS, o2.ra_PS, o2.decl_PS) AS distance
+  FROM Object o1,
+       Object o2
+ WHERE scisql_angSep(o1.ra_PS, o1.decl_PS, 0.1, 0.5) <= 0.55
+   AND scisql_angSep(o1.ra_PS, o1.decl_PS, o2.ra_PS, o2.decl_PS) < 0.016
+   AND o1.objectId <> o2.objectId
