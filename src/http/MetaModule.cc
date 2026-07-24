33ffbeb1ef29fa264903dd6bbf02e881e2fa5a97
Igor Gaponenko <gapon@slac.stanford.edu>
2026-07-24 12:52:36 -0700

    Upgraded version number of the REST API to 58

--- src/http/MetaModule.cc
+++ src/http/MetaModule.cc
@@ -36,7 +36,7 @@ lsst::qserv::http::AuthContext const authContext;
 
 namespace lsst::qserv::http {
 
-unsigned int const MetaModule::version = 57;
+unsigned int const MetaModule::version = 58;
 
 void MetaModule::process(string const& context, nlohmann::json const& info,
                          shared_ptr<qhttp::Request> const& req, shared_ptr<qhttp::Response> const& resp,
