19972680a440f7943fdc4a1311c0afbea92f4caf
Igor Gaponenko <gapon@slac.stanford.edu>
2026-07-27 20:37:48 -0700

    Migrated Qserv Registry REST services to report code 404

--- src/replica/registry/RegistryServices.h
+++ src/replica/registry/RegistryServices.h
@@ -40,52 +40,83 @@ class RegistryServices {
 public:
     /**
      * Merge (complete or partial) worker definition into the worker entry.
-     * @param name A unique identifier of the worker.
-     * @param workerInfo A payload to be merged.
-     * @throws std::invalid_argument If the worker name is empty or if the worker
-     *   definition is not a valid JSON object.
+     * @param id A unique identifier of the worker.
+     * @param desc A payload to be merged.
+     * @throws std::invalid_argument If the worker id is empty or if the worker
+     *   descriptor is not a valid JSON object.
      */
-    void updateWorker(std::string const& name, nlohmann::json const& workerInfo);
+    void updateWorker(std::string const& id, nlohmann::json const& desc);
 
     /**
-     * Remove (if exists) a worker entry.
-     * @param name A unique identifier of the worker.
-     * @throws std::invalid_argument If the worker name is empty.
+     * Remove an existing worker entry.
+     * @param id A unique identifier of the worker.
+     * @throws std::invalid_argument If the worker id is empty.
+     * @throws http::ErrorNotFound404 If the worker id is not found in the collection.
      */
-    void removeWorker(std::string const& name);
+    void removeWorker(std::string const& id);
 
     /**
      * Add or update (if exists) the complete definition into the Czar entry.
-     * @param name A unique identifier of the czar.
-     * @param czarInfo A payload to be added/updated.
-     * @throws std::invalid_argument If the definition of the czar is not a valid JSON object.
+     * @param id A unique identifier of the czar.
+     * @param desc A payload to be added/updated.
+     * @throws std::invalid_argument If the czar id is empty or if the czar
+     *   definition is not a valid JSON object.
      */
-    void updateCzar(std::string const& name, nlohmann::json const& czarInfo);
+    void updateCzar(std::string const& id, nlohmann::json const& desc);
 
     /**
-     * Remove (if exists) a Czar entry.
-     * @param name A unique identifier of the czar.
+     * Remove an existing Czar entry.
+     * @param id A unique identifier of the czar.
+     * @throws std::invalid_argument If the czar id is empty.
+     * @throws http::ErrorNotFound404 If the czar id is not found in the collection.
      */
-    void removeCzar(std::string const& name);
+    void removeCzar(std::string const& id);
 
     /**
-     * Add or update (if exists) the complete definition into the Controller entry.
-     * @param name A unique identifier of the controller.
-     * @param controllerInfo A payload to be added/updated.
-     * @throws std::invalid_argument If the definition of the controller is not a valid JSON object.
+     * Add or update (if exists) the complete definition into the controller entry.
+     * @param id A unique identifier of the controller.
+     * @param desc A payload to be added/updated.
+     * @throws std::invalid_argument If the controller id is empty or if the controller
+     *   definition is not a valid JSON object.
      */
-    void updateController(std::string const& name, nlohmann::json const& controllerInfo);
+    void updateController(std::string const& id, nlohmann::json const& desc);
 
     /**
-     * Remove (if exists) a Controller entry.
-     * @param name A unique identifier of the controller.
+     * Remove an existing controller entry.
+     * @param id A unique identifier of the controller.
+     * @throws std::invalid_argument If the controller id is empty.
+     * @throws http::ErrorNotFound404 If the controller id is not found in the collection.
      */
-    void removeController(std::string const& name);
+    void removeController(std::string const& id);
 
     /// @return nlohmann::json The whole collection of services.
     nlohmann::json toJson() const;
 
 private:
+    /**
+     * Insert or update (if exists) a service entry in the collection. If the specified entry already exists
+     * in the specified service type category, the function will insert new attributes or replace existing
+     * ones of the service entry.
+     * @param func A name of the calling function (for logging purposes).
+     * @param serviceType A type of the service (worker, czar, controller).
+     * @param serviceId A unique identifier of the service.
+     * @param desc A payload to be merged into the service entry.
+     * @throws std::invalid_argument If the service id is empty or if the service descriptor is not a valid
+     * JSON object.
+     */
+    void _update(std::string const& func, std::string const& serviceType, std::string const& serviceId,
+                 nlohmann::json const& desc);
+
+    /**
+     * Remove an existing service entry from the collection.
+     * @param func A name of the calling function (for logging purposes).
+     * @param serviceType A type of the service (worker, czar, controller).
+     * @param serviceId A unique identifier of the service.
+     * @throws std::invalid_argument If the service id is empty.
+     * @throws http::ErrorNotFound404 If the service id is not found in the collection.
+     */
+    void _remove(std::string const& func, std::string const& serviceType, std::string const& serviceId);
+
     /// This mutex is needed for implementing synchronized operations over
     /// the collection.
     mutable replica::Mutex _mtx;
